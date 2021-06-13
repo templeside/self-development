5fc1e8e4df5b5a74df37201d6f122d3783d9b585
templeside <47994862+templeside@users.noreply.github.com>
2021-06-13 13:35:48 +0900

    Update two-sum.java
    Runtime: 11 ms, faster than 25.01% of Java online submissions for Two Sum.
    Memory Usage: 43.7 MB, less than 5.45% of Java online submissions for Two Sum.

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,11 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++){
-            for(int j= i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target)
-                    return new int[]{i,j};
-            }
+        // <number, index>
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i< nums.length;i++){
+            map.put(nums[i],i);
         }
-        return new int[2];
+        
+        for(int i=0;i<nums.length;i++){
+            int num = nums[i];
+            int offset = target-num;
+            if(map.containsKey(offset) && map.get(offset) !=i)
+                return new int[]{i,map.get(offset)};
+        }
+        return null;
     }
 }
