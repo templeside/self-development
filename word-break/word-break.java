16fa344d156c2dd4acb080bfe071665338160341
templeside <47994862+templeside@users.noreply.github.com>
2021-06-15 20:05:48 +0900

    Time: 6 ms (63.67%), Space: 39.8 MB (16.67%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> dict = new HashSet(wordDict);
-        boolean[]dp = new boolean[s.length()+1];
-        dp[0] = true;
+        int len = s.length();
+        HashSet<String> dict = new HashSet<String>();
+        boolean[] isValid = new boolean[len+1];
+        isValid[0] = true;
         
-        for(int i=1;i<s.length()+1;i++){
+        for(String word: wordDict){
+            dict.add(word);
+        }
+        
+        for(int i=1;i<=len;i++){
             for(int j=0;j<i;j++){
-                if(dp[j] && dict.contains(s.substring(j,i))){
-                    dp[i] = true;
-                    break;
+                if(isValid[j] && dict.contains(s.substring(j,i))){
+                    isValid[i] = true;
                 }
             }
         }
-        return dp[s.length()];
+        return isValid[len];
     }
 }
