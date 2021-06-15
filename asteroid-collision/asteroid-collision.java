2b77a0505361896f754725652e31e1c5248758de
templeside <47994862+templeside@users.noreply.github.com>
2021-06-15 12:11:23 +0900

    Time: 8 ms (19.10%), Space: 45.3 MB (6.07%) - LeetHub

--- asteroid-collision/asteroid-collision.java
+++ asteroid-collision/asteroid-collision.java
@@ -1,66 +1,21 @@
-
 class Solution {
-    /*
-    The data structure used here is Stack.
-    Since, we only iterate through the array once, the time complexity is O(N).
-    The maximum space occupied by the Stack can be N, thus space complexity is O(N)
-    */
     public int[] asteroidCollision(int[] asteroids) {
-        /* Processing the collisions */
-        
-        // Stack for storing asteroids that survive the collision
-        Deque<Integer> st = new ArrayDeque<>();
-        
-        // Initializing the stack
-        st.push(asteroids[0]);
-        
-        /* len - for storing length of asteroid array
-           and later storing size of stack
-           and also serves as index */
-        // val - for storing value(size) of the i-th asteroid
-        int len = asteroids.length, val;
-        
-        // iteration through asteroid array
-        for(int i=1;i<len;i++){
-            val = asteroids[i];
-            // if asteroids[i]<0 we need to process further
-            // otherwise we can simply push it to the stack
-            if(val<0){
-                /* destroying all asteroids of the stack whose
-                   absolute value (size) is less than val */
-                while(st.size()!=0&&st.peek()>0&&st.peek()<-1*val){
-                    st.pop();
-                }
-                /* if there is any element remaining in stack
-                 and that element is positive, we'll need to check
-                 whether it has same size has val asteroid*/
-                if(st.size()!=0&&st.peek()>0){
-                    /* if top of the stack has an absolute value(size) 
-                       less than val means both will be destroyed */
-                    if(st.peek()==-1*val){ st.pop(); }
-                } else{
-                    // otherwise push val to stack
-                    st.push(val);
-                }
-            } else{
-                st.push(val);
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            int asteroid = asteroids[i];
+            if(asteroid>0){             //양수 일 때만 넣는다.
+                stack.push(asteroid);
+            }else{
+                // 음수 일 때, stack.
+                while(!stack.isEmpty() && stack.peek() >0 && stack.peek()< -asteroid) 
+                    stack.pop();
+                if(stack.isEmpty()|| stack.peek()<0) stack.push(asteroid);
+                else if(stack.peek() == -asteroid) stack.pop(); //[10],5,-5
             }
         }
-        
-        /* Creating the result array */
-        
-        len = st.size();
-        int[] result = new int[len];
-        
-        /* The stack contains value in reverse order
-           that is, last index of result is top of the
-           stack. Thus, this loop stores values in result
-           in reverse order*/
-        while(st.size()!=0){
-            result[--len]=st.pop();
-        }
-        
+        int[] result = new int[stack.size()];
+        for(int i=result.length-1; i>-1;i--)
+            result[i] = stack.pop();
         return result;
     }
 }
-
