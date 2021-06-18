2a9dfaab857fe32e35aa09a03ebce3361b997b97
templeside <47994862+templeside@users.noreply.github.com>
2021-06-19 00:05:06 +0900

    Time: 19 ms (73.81%), Space: 39.1 MB (83.90%) - LeetHub

---
+++ find-the-celebrity/find-the-celebrity.java
@@ -0,0 +1,22 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int candidate = 0;
+        for(int i=0;i< n;i++){
+            if(knows(candidate,i)) 
+                candidate = i;
+        }
+        if(isHeCelebrity(candidate, n))return candidate;
+        return -1;
+    }
+    private boolean isHeCelebrity(int candidate,int n){
+        for(int j=0;j<n;j++){
+            if(candidate == j) continue;
+            if(knows(candidate, j) || !knows(j,candidate))
+                return false;
+        }
+        return true;
+    }
+}
