51a30c73465fb1bf1b2032b99ea43a50f17b0669
templeside <47994862+templeside@users.noreply.github.com>
2021-07-24 20:17:51 +0900

    Time: 19 ms (77.27%), Space: 39.2 MB (72.60%) - LeetHub

--- find-the-celebrity/find-the-celebrity.java
+++ find-the-celebrity/find-the-celebrity.java
@@ -1,21 +1,36 @@
-/* The knows API is defined in the parent class Relation.
-      boolean knows(int a, int b); */
-
 public class Solution extends Relation {
-    public int findCelebrity(int n) {
-        int candidate = 0;
-        for(int i=0;i< n;i++){
-            if(knows(candidate,i)) 
-                candidate = i;
+    
+    private int numberOfPeople;
+    private Map<Pair<Integer, Integer>, Boolean> cache = new HashMap<>(); 
+    
+    @Override
+    public boolean knows(int a, int b) {
+        if (!cache.containsKey(new Pair(a, b))) {
+            cache.put(new Pair(a, b), super.knows(a, b));
         }
-        if(isHeCelebrity(candidate, n))return candidate;
+        return cache.get(new Pair(a, b));
+    }
+    
+    public int findCelebrity(int n) {
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
+            }
+        }
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
+        }
         return -1;
     }
-    private boolean isHeCelebrity(int candidate,int n){
-        for(int j=0;j<n;j++){
-            if(candidate == j) continue;
-            if(knows(candidate, j) || !knows(j,candidate))
+    
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
                 return false;
+            }
         }
         return true;
     }
