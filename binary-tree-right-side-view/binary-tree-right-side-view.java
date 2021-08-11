989a2c3a3ca9999708e76231572369b0a1040713
templeside <47994862+templeside@users.noreply.github.com>
2021-08-11 16:38:50 +0900

    Time: 1 ms (75.26%), Space: 39.4 MB (5.80%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.java
+++ binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,33 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root ==null)return new LinkedList<Integer>();
-        //bfs
-        List<Integer> rightSide = new LinkedList<Integer>();
-        //arraydequeue 는 referencing을 하지 않는다(LinkedList는 한다). linkedlist로 구성하면, levels에 모든 nodes들이 추가 됨.
-        ArrayDeque<ArrayDeque<TreeNode>> levels = new ArrayDeque<>();   //total db
-        ArrayDeque<TreeNode> currLevel = new ArrayDeque<>();            //curr level to check right side
-        ArrayDeque<TreeNode> nextLevel;                                 //next level adding with curr level nodes' children
-
-        currLevel.add(root);
-        levels.add(currLevel);
-        TreeNode node = null;
-
-        while(!levels.isEmpty()){
-            currLevel = levels.poll();          // curr level from total db
-            nextLevel = new ArrayDeque<>();     // making the next level
+        if(root == null) return new ArrayList<Integer>();// base case
+        
+        List<Integer> returnList= new ArrayList<Integer>();
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque<>();
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque<>();
+        
+        nextLevel.add(root);
+        returnList.add(root.val);
+        
+        while(!nextLevel.isEmpty()){
+            currLevel = nextLevel;
+            nextLevel = new ArrayDeque<>();
+            
             while(!currLevel.isEmpty()){
-                node = currLevel.poll();
-                if(node.left !=null)            //adding to next level
-                    nextLevel.add(node.left);
-                if(node.right !=null)           //adding to next level
-                    nextLevel.add(node.right);
+                TreeNode curr =currLevel.poll();
+                if(curr.left !=null)
+                    nextLevel.add(curr.left);
+                if(curr.right !=null)
+                    nextLevel.add(curr.right);
+            }
+            
+            if(!nextLevel.isEmpty()){
+                int rightVal = nextLevel.peekLast().val;
+                returnList.add(rightVal);                
             }
 
-            rightSide.add(node.val);
-            if(!nextLevel.isEmpty())
-                levels.add(nextLevel);
         }
-        return rightSide;
+        
+        return returnList;
+        
     }
 }
-
