549f3a74ca64291338d78254c7b8cbe08ccb032f
templeside <47994862+templeside@users.noreply.github.com>
2021-08-14 14:10:34 +0900

    Time: 20 ms (10.19%), Space: 53.4 MB (6.04%) - LeetHub

--- smallest-common-region/smallest-common-region.java
+++ smallest-common-region/smallest-common-region.java
@@ -1,10 +1,14 @@
 class Solution {
     public String findSmallestRegion(List<List<String>> regions, String region1, String region2) {
+        // db화 만들기
         Map<String, String> parents = new HashMap<>();
         Set<String> ancestryHistory = new HashSet<>();
+
+        // add the values to db.
         for (List<String> region : regions)
             for (int i = 1; i < region.size(); ++i)
                 parents.put(region.get(i), region.get(0));
+
         while (region1 != null) {
             ancestryHistory.add(region1);
             region1 = parents.get(region1);
@@ -14,3 +18,4 @@ class Solution {
         return region2;
     }
 }
+
