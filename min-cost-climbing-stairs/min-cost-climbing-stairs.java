618ce91a3b5e4e1062a3f770bd86ab0ab83c8690
templeside <47994862+templeside@users.noreply.github.com>
2021-09-09 22:32:52 +0900

    Time: 3 ms (6.54%), Space: 39 MB (31.46%) - LeetHub

---
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+    public int minCostClimbingStairs(int[] cost) {
+        return minimumCost(cost.length, cost);
+    }
+    
+    private int minimumCost(int i, int[]cost){
+        if(i<=1)        //base case
+            return 0;
+        
+        if(memo.containsKey(i))
+            return memo.get(i);
+    
+        int downOne = cost[i-1] + minimumCost(i-1,cost);
+        int downTwo = cost[i-2] + minimumCost(i-2, cost);
+        memo.put(i, Math.min(downOne, downTwo));
+        return memo.get(i);
+    }
+}
