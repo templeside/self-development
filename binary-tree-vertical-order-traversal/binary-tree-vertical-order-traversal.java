272bd874e26e1dca7a7cd4981094d6ab530a3b08
templeside <47994862+templeside@users.noreply.github.com>
2021-09-16 02:09:50 +0900

    Time: 13 ms (5.30%), Space: 40.8 MB (6.90%) - LeetHub

--- binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -4,43 +4,74 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  public List<List<Integer>> verticalOrder(TreeNode root) {
-    List<List<Integer>> output = new ArrayList();
-    if (root == null) {
-      return output;
-    }
-
-    Map<Integer, ArrayList> columnTable = new HashMap();
-    Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
-    int column = 0;
-    queue.offer(new Pair(root, column));
-
-    while (!queue.isEmpty()) {
-      Pair<TreeNode, Integer> p = queue.poll();
-      root = p.getKey();
-      column = p.getValue();
-
-      if (root != null) {
-        if (!columnTable.containsKey(column)) {
-          columnTable.put(column, new ArrayList<Integer>());
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        if(root ==null)return new ArrayList<>();
+        // <Index, list<Integer>>
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int index = 100;
+        bfs(root, index, map);
+        
+        List<List<Integer>> order = new ArrayList<>();
+        System.out.println(map.keySet());
+        
+        Set<Integer> keyset = map.keySet();
+        Integer[] keys = keyset.toArray(new Integer[map.size()]);
+        Arrays.sort(keys);
+        for(int key: keys){
+        // for(int key: map.keySet()){
+            order.add(map.get(key));
         }
-        columnTable.get(column).add(root.val);
-
-        queue.offer(new Pair(root.left, column - 1));
-        queue.offer(new Pair(root.right, column + 1));
-      }
+        return order;
     }
-
-    List<Integer> sortedKeys = new ArrayList<Integer>(columnTable.keySet());
-    Collections.sort(sortedKeys);
-    for(int k : sortedKeys) {
-      output.add(columnTable.get(k));
+    
+    public class Node{
+        public int index;
+        public TreeNode node;
+        
+        public Node(int index, TreeNode node){
+            this.index = index;
+            this.node = node;
+        }
     }
-
-    return output;
-  }
+    
+    private void bfs(TreeNode root, int index, HashMap<Integer, List<Integer>> map){
+        if(root == null)return;   //base case
+        // {index, node}
+        LinkedList<Node> q = new LinkedList<Node>();
+        LinkedList<Node> nextQ = new LinkedList<Node>();
+        
+        nextQ.add(new Node(index, root));
+        while(!nextQ.isEmpty()){
+            q = nextQ;
+            nextQ = new LinkedList<Node>();
+            
+            while(!q.isEmpty()){
+                Node currNode = q.poll();
+                int currIndex = currNode.index;
+                TreeNode currRoot = currNode.node;
+                
+                // hasmap에 넣기
+                if(!map.containsKey(currIndex))
+                    map.put(currIndex, new LinkedList<Integer>());
+                map.get(currIndex).add(currRoot.val);                
+                
+                if(currRoot.left !=null)
+                    nextQ.add(new Node(currIndex-1, currRoot.left));
+                
+                if(currRoot.right !=null)
+                    nextQ.add(new Node(currIndex+1, currRoot.right));
+            }
+        }
+        return;
+    }
 }
