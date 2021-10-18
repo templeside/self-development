61bf09596cc81f525e51882dbae63ffdca99a055
templeside <47994862+templeside@users.noreply.github.com>
2021-10-19 05:35:21 +0900

    Time: 7 ms (52.83%), Space: 42 MB (50.76%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,14 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
-        // last list 빼올 수 있는거: linkedList
-        LinkedList<int[]> result = new LinkedList<>();
-        for(int[] interval: intervals){
-            if(result.isEmpty() || result.getLast()[1]<interval[0])
-                result.add(interval);
-            else
-                result.getLast()[1] = Math.max(result.getLast()[1],interval[1]);
+        
+        System.out.println("");
+        
+        ArrayList<int[]> ans = new ArrayList<>();
+        
+        for(int[] interval : intervals){            
+            if( ans.size() !=0 && ans.get(ans.size()-1)[1] >=interval[0]){
+                // int[]
+                int[] prev = ans.get(ans.size()-1);       
+                
+                if(prev[1]>= interval[1])
+                    continue;
+                else
+                    prev[1] = interval[1];
+                
+            }else{
+                ans.add(interval);
+            }
         }
-        return result.toArray(new int[result.size()][2]);
+        
+        int[][] returnArr = new int[ans.size()][2];
+        for(int i=0; i< ans.size();i++){
+            returnArr[i] = ans.get(i);
+        }
+        
+        return returnArr;
     }
 }
