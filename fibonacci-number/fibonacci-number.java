ddf6a0decef318d9b3affda16505eceadfcbe8d2
templeside <47994862+templeside@users.noreply.github.com>
2021-10-26 08:28:25 +0900

    Time: 0 ms (100.00%), Space: 35.7 MB (78.10%) - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,7 +1,22 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n ==0 || n==1)
+            return n;
+        int[] arr = new int[n+1];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        return dynamic(arr, n);
     }
+    
+    private int dynamic(int[] arr, int n){
+        if(n <=0 || arr[n] !=0)
+            return arr[n];
+        
+        int sum = dynamic(arr, n-1) + dynamic(arr, n-2);
+        arr[n] = sum;
+        return sum;
+    }
+    
 }
