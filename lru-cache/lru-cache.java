76014ea18f10f606c1a8e4a4e4436390d24e4390
templeside <47994862+templeside@users.noreply.github.com>
2021-11-09 02:23:59 -0600

    Time: 45 ms (98.04%), Space: 109.5 MB (84.31%) - LeetHub

--- lru-cache/lru-cache.java
+++ lru-cache/lru-cache.java
@@ -1,86 +1,22 @@
-class LRUCache {
-    class ListNode{
-        int key;
-        int value;
-        ListNode prev;
-        ListNode next;
-        public ListNode(){
-            key = 0;
-            value = 0;
-            prev = null;
-            next = null;
-        }
-    }
-            //<key, listNode>
-    HashMap<Integer, ListNode> map;
-    private int size, capacity;
-    private ListNode head, tail;
+class LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
+    
     public LRUCache(int capacity) {
-        map = new HashMap<>();
-        size = 0;
+        super(capacity, 0.75F, true);
         this.capacity = capacity;
-        
-        head = new ListNode();
-        tail = new ListNode();
-        
-        head.next = tail;
-        tail.prev = head;
     }
-    
+
     public int get(int key) {
-        if(!map.containsKey(key))return -1;
-        
-        ListNode node = map.get(key);
-        this.moveToHead(node);
-        
-        return node.value;
+        return super.getOrDefault(key, -1);
     }
-    
+
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-            map.get(key).value = value;
-            this.moveToHead(map.get(key));
-        }else{
-            ListNode newNode = new ListNode();
-            newNode.key = key;
-            newNode.value = value;
-            
-            map.put(key, newNode);
-            this.addNode(newNode);
-            size ++;
-            
-            if(size>capacity){
-                ListNode removed = removeLate();
-                map.remove(removed.key);
-                size --;
-            }
-        }
+        super.put(key, value);
     }
-    private void moveToHead(ListNode node){
-        this.removeNode(node);
-        this.addNode(node);
-    }
-    
-    private void removeNode(ListNode node){
-        ListNode prev = node.prev;
-        ListNode next = node.next;
-        
-        prev.next = next;
-        next.prev = prev;
-    }
-    
-    private void addNode(ListNode node){
-        node.next = head.next;
-        node.prev = head;
-        
-        head.next.prev = node;
-        head.next = node;
-    }
-    private ListNode removeLate(){
-        ListNode removed = tail.prev;
-        removed.prev.next= tail;
-        tail.prev = removed.prev;
-        return removed;
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity; 
     }
 }
 
