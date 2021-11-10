c57de1d76aa67754b0e232e2ca97010c29b86bc7
templeside <47994862+templeside@users.noreply.github.com>
2021-11-10 13:53:50 -0600

    Time: 5 ms (69.54%), Space: 39.5 MB (87.36%) - LeetHub

--- asteroid-collision/asteroid-collision.java
+++ asteroid-collision/asteroid-collision.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<asteroids.length;i++){
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i=0; i<asteroids.length;i++){
             int asteroid = asteroids[i];
-            if(asteroid>0){             //양수 일 때만 넣는다.
+            if(asteroid>0)
                 stack.push(asteroid);
-            }else{
-                // 음수 일 때, stack.
-                while(!stack.isEmpty() && stack.peek() >0 && stack.peek()< -asteroid) 
+            else{
+                while(!stack.isEmpty() && stack.peek()>0 && stack.peek() <-asteroid)
                     stack.pop();
-                if(stack.isEmpty()|| stack.peek()<0) stack.push(asteroid);
-                else if(stack.peek() == -asteroid) stack.pop(); //[10],5,-5
+                if(stack.isEmpty() || stack.peek() <0)
+                    stack.push(asteroid);
+                else if(stack.peek() == -asteroid)
+                    stack.pop();
             }
+            
         }
         int[] result = new int[stack.size()];
-        for(int i=result.length-1; i>-1;i--)
+        for(int i=result.length-1; i>=0;i--){
             result[i] = stack.pop();
+        }
         return result;
     }
 }
