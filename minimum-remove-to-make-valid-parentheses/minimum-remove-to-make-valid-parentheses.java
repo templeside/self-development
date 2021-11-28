b4d5086732d2855fdf9df3ccfaeaf04a7dfc0cd2
templeside <47994862+templeside@users.noreply.github.com>
2021-11-27 20:24:20 -0600

    Time: 30 ms (28.11%), Space: 52.2 MB (6.97%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,30 +1,26 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        int openCount = 0;
-        StringBuilder sb = new StringBuilder();
         
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         for(int i=0; i< s.length();i++){
-            char c= s.charAt(i);
-            if(c== '('){
-                sb.append(c);
-                openCount++;
-            }else if(c ==')'){
-                if(openCount >0){
-                    openCount --;
-                    sb.append(c);
-                }
-            }else
-                sb.append(c);
+            if(s.charAt(i) =='(')
+                stack.push(i);
+            else if(s.charAt(i) ==')'){
+                if(stack.isEmpty())
+                    indexesToRemove.add(i);
+                else
+                    stack.pop();
+            }
         }
         
-        StringBuilder result = new StringBuilder();
-        for(int i= sb.length()-1 ;i>=0; i--){
-            if(sb.charAt(i) == '(' && openCount -- >0 )
-                continue;
-            result.append(sb.charAt(i));
+        while(!stack.isEmpty())
+            indexesToRemove.add(stack.pop());
+        StringBuilder sb= new StringBuilder();
+        for(int i=0; i< s.length();i++){
+            if(!indexesToRemove.contains(i))
+                sb.append(s.charAt(i));
         }
-        return result.reverse().toString();
-            
+        return sb.toString();
     }
 }
-
