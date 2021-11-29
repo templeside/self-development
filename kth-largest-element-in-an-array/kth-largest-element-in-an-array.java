cdeffcbb6843eb342d072ae7f1cbdb6292f42130
templeside <47994862+templeside@users.noreply.github.com>
2021-11-29 04:21:28 -0600

    Time: 1 ms (98.48%), Space: 39.5 MB (54.72%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,25 +1,6 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Stack<Integer> db = new Stack<Integer>();
-        Stack<Integer> tempDb = new Stack<Integer>();
-        for(int n: nums){
-            if(db.isEmpty() || db.peek() <= n)
-                db.push(n);
-            else{
-                while(!db.isEmpty() && n < db.peek()){     //move to tempDB
-                    tempDb.push(db.pop());
-                }
-                db.push(n);
-                
-                while(!tempDb.isEmpty())
-                    db.push(tempDb.pop());
-            }
-        }
-        while(k>1){
-            db.pop();
-            k--;
-        }
-            
-        return db.peek();
+        Arrays.sort(nums);
+        return nums[nums.length-k];
     }
 }
