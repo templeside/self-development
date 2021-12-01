dba9dd7be1603d1ac2f333a934e1c87339faf1be
templeside <47994862+templeside@users.noreply.github.com>
2021-11-30 23:48:44 -0600

    Time: 60 ms (29.83%), Space: 43 MB (92.72%) - LeetHub

--- accounts-merge/accounts-merge.java
+++ accounts-merge/accounts-merge.java
@@ -1,34 +1,42 @@
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> acts) {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+            // <any email, owner name>
         Map<String, String> owner = new HashMap<>();
+            // <any meail, parent email>
         Map<String, String> parents = new HashMap<>();
+            // <parentEmail, union>
         Map<String, TreeSet<String>> unions = new HashMap<>();
-        for (List<String> a : acts) {
-            for (int i = 1; i < a.size(); i++) {
-                parents.put(a.get(i), a.get(i));
-                owner.put(a.get(i), a.get(0));
+        
+        for(List<String> account: accounts){
+            for(int i=1; i< account.size();i++){
+                owner.put( account.get(i), account.get(0));
+                parents.put( account.get(i), account.get(i));
             }
         }
-        for (List<String> a : acts) {
-            String p = find(a.get(1), parents);
-            for (int i = 2; i < a.size(); i++)
-                parents.put(find(a.get(i), parents), p);
+        
+        for(List<String> account: accounts){
+            String p = findParent(account.get(1),parents);
+            for(int i=2; i< account.size();i++)
+                parents.put(findParent(account.get(i), parents),p);
         }
-        for(List<String> a : acts) {
-            String p = find(a.get(1), parents);
-            if (!unions.containsKey(p)) unions.put(p, new TreeSet<>());
-            for (int i = 1; i < a.size(); i++)
-                unions.get(p).add(a.get(i));
+        
+        for(List<String> account: accounts){
+            String p = findParent(account.get(1), parents);
+            if(!unions.containsKey(p)) unions.put(p, new TreeSet<>());
+            for(int i=1; i<account.size();i++)
+                unions.get(p).add(account.get(i));
         }
         List<List<String>> res = new ArrayList<>();
-        for (String p : unions.keySet()) {
+        for(String p: unions.keySet()){
             List<String> emails = new ArrayList(unions.get(p));
-            emails.add(0, owner.get(p));
+            emails.add(0,owner.get(p));
             res.add(emails);
         }
         return res;
+        
     }
-    private String find(String s, Map<String, String> p) {
-        return p.get(s) == s ? s : find(p.get(s), p);
+    
+    public String findParent(String s, Map<String, String> p){
+        return p.get(s)==s? s: findParent(p.get(s),p);
     }
 }
