153d52b3a44eb4804482823069a0172bb497d264
templeside <47994862+templeside@users.noreply.github.com>
2021-12-03 23:29:18 -0600

    Time: 19 ms (9.89%), Space: 38.7 MB (37.08%) - LeetHub

--- insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
+++ insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
@@ -1,36 +1,63 @@
-class Solution {
-  public Node insert(Node head, int insertVal) {
-    if (head == null) {
-      Node newNode = new Node(insertVal, null);
-      newNode.next = newNode;
-      return newNode;
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
     }
 
-    Node prev = head;
-    Node curr = head.next;
-    boolean toInsert = false;
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
 
-    do {
-      if (prev.val <= insertVal && insertVal <= curr.val) {
-        // Case 1).
-        toInsert = true;
-      } else if (prev.val > curr.val) {
-        // Case 2).
-        if (insertVal >= prev.val || insertVal <= curr.val)
-          toInsert = true;
-      }
-
-      if (toInsert) {
-        prev.next = new Node(insertVal, curr);
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        // 1. when head ==null
+        
+        if(head ==null){
+            Node newNode = new Node(insertVal);
+            newNode.next = newNode;
+            return newNode;
+        }
+        
+        // 2. when head[1,2,3,4] inserval out of range[5 or -1]
+        // 3. whe nhead[1,2,3,4] inserval is 3.5 
+        // prevNode, currNode
+        
+        Node prevNode = head;
+        Node currNode = head.next;
+        boolean inserted = false;
+        
+        do{
+            System.out.println(prevNode.val +" "+ currNode.val);
+            if(prevNode.val<= insertVal && insertVal<= currNode.val){
+                Node newNode = new Node(insertVal);
+                newNode.next = currNode;
+                prevNode.next = newNode;
+                inserted = true;
+                return head;
+            }else if(prevNode.val> currNode.val){
+                if( prevNode.val <= insertVal || insertVal <=currNode.val ){
+                    Node newNode = new Node(insertVal);
+                    newNode.next = currNode;
+                    prevNode.next = newNode;
+                    inserted = true;
+                    return head;
+                }
+                }
+            
+            prevNode = currNode;
+            currNode = currNode.next;
+        }while(prevNode != head);
+        
+        prevNode.next = new Node(insertVal, currNode);
         return head;
-      }
-
-      prev = curr;
-      curr = curr.next;
-    } while (prev != head);
-
-    // Case 3).
-    prev.next = new Node(insertVal, curr);
-    return head;
-  }
+    }
 }
