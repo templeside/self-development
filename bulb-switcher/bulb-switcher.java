34e86ccdd8b188ce6fba0059585bef7f686190c7
templeside <47994862+templeside@users.noreply.github.com>
2021-12-05 04:50:16 -0600

    Time: 0 ms (100.00%), Space: 35.6 MB (78.66%) - LeetHub

---
+++ bulb-switcher/bulb-switcher.java
@@ -0,0 +1,7 @@
+// Only when i has perfect square root, its number of divisors is ODD, because it contains duplicated divisor.
+class Solution{
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+
+    }
+}
