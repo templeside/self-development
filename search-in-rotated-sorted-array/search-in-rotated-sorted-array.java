2b85a9dbde8807fba77dc7ab604ca1f5c0116d3c
templeside <47994862+templeside@users.noreply.github.com>
2021-12-30 13:18:08 -0600

    Time: 1 ms (19.30%), Space: 39.7 MB (7.61%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,34 +1,39 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        int returnIndex = -1;
+    public int find_rotation(int[] nums){
+        int left = 0, right = nums.length-1;
+        int saved = -1;
         
         while(left<=right){
             int mid = left+(right-left)/2;
-            if(nums[mid] == target){
-                returnIndex = mid;
-                return returnIndex;
-            } 
             
-            else if(nums[left]<=nums[mid]){
-                if(nums[left] <=target && target< nums[mid]){  //in between in normal order
-                    right = mid-1;
-                    continue;
-                }else{              //if(not in that range)
-                    left= mid+1;
-                    continue;
-                }
-            }else{
-                if(target>=nums[left]|| target<nums[mid]){
-                    right = mid-1;
-                    continue;
-                }else{
-                    left = mid+1;
-                    continue;
-                }
+            if(nums[mid]<= nums[nums.length-1]){
+                saved = mid;
+                right = mid-1;
             }
+            else
+                left = mid+1;
+        }        
+        return saved;
+    }
+    
+    public int search(int[] nums, int target) {
+        int smallest_idx = find_rotation(nums);
+        
+        if(smallest_idx ==0) return search(nums, 0,nums.length-1,target);
+        else if(nums[smallest_idx]<= target && target<=nums[nums.length-1]) return search(nums, smallest_idx,nums.length-1,target);
+        else return search(nums, 0, smallest_idx-1,target);
+    }
+    public int search(int[] nums, int left, int right,int target){
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid]>target)
+                right =mid-1;
+            else
+                left = mid+1;
+
+
         }
-        return returnIndex;
+        return -1;
     }
 }
