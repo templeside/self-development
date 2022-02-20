9641cbf1372e391dca97e1fa37673e268b1ed9d1
templeside <47994862+templeside@users.noreply.github.com>
2022-02-20 14:26:02 -0600

    Time: 7 ms (44.69%), Space: 44.4 MB (50.56%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -4,7 +4,8 @@ class Solution {
         List<List<Integer>> returnVal = new ArrayList<>();
         //so, in the paramter, there should be currsum, target, currVal, returnVal
         // backtrack(0, currVal, currIdx, returnVal, target);
-        backtrack(0, new ArrayList<>(), -1, returnVal, candidates, target);
+        // backtrack(0, new ArrayList<>(), -1, returnVal, candidates, target);
+        backtrack(0, new ArrayList<>(), 0, returnVal, candidates, target);
         return returnVal;
     }
     public void backtrack(int currSum, List<Integer> currVal, int currIdx, List<List<Integer>> returnVal, int[] candidates, int target){
@@ -17,7 +18,7 @@ class Solution {
             returnVal.add(new ArrayList(currVal));
         
         for(int i= currIdx; i< candidates.length;i++){
-            if(i == -1)continue;
+            // if(i == -1)continue;
             currSum += candidates[i];
             currVal.add(candidates[i]);
             backtrack(currSum, currVal, i, returnVal, candidates, target);
