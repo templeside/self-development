c2d1eb4ace918b966376569e7c27ecb1dd134acd
templeside <47994862+templeside@users.noreply.github.com>
2022-03-29 18:08:18 -0500

    Time: 165 ms (57.07%), Space: 68.8 MB (29.02%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,36 +1,16 @@
-class Solution {
-    // can use by brute force, using two for loop. 
-    // but can use better way by crossing out the prime number and its multiplication.
-    /**
-        creating the boolean[]  isPrime = boolean[n];
-        
-        1 2 3 4 5 6 7 8 9 10
-    2         ^   ^   ^    ^
-    3                   ^ 
-    5                                   25
-    7                                   49
-    
-    time complexity = O(N * sqrt(N))
-    */
-    
-    
+public class Solution {
     public int countPrimes(int n) {
-        
         boolean[] notPrime = new boolean[n];
-        
-        for(int i=2; i<= (int)Math.sqrt(n);i++){
-            if(!notPrime[i]){
-                for(int j= i*i; j<n; j+=i){
-                    notPrime[j] = true;
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
                 }
             }
         }
         
-        int count =0;
-        for(int i=2; i<n;i++){
-            if(!notPrime[i])
-                count++;
-        }
         return count;
     }
 }
