f4a47c56ee7a44013905f0d6df2ffc34d2dc95f6
templeside <47994862+templeside@users.noreply.github.com>
2022-05-22 11:32:50 -0500

    Time: 1 ms (95.66%), Space: 39.3 MB (94.38%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,21 +1,44 @@
 class Solution {
-     public int getNext(int n) {
-        int totalSum = 0;
-        while (n > 0) {
-            int d = n % 10;
-            n = n / 10;
-            totalSum += d * d;
-        }
-        return totalSum;
-    }
+    
+    /**
+    n= 19 82 68 100 1
+slow          ^
+fast                ^
 
-    public boolean isHappy(int n) {
-        int slowRunner = n;
-        int fastRunner = getNext(n);
-        while (fastRunner != 1 && slowRunner != fastRunner) {
-            slowRunner = getNext(slowRunner);
-            fastRunner = getNext(getNext(fastRunner));
+    4 16 37 58 89 145 42 20
+      ^
+         ^
+    
+    37 = 9+49 = 58
+    58 = 25 + 64 = 89
+    89 = 64 + 81 = 145
+    145 = 1 + 16 + 25 = 42
+    42 = 16+4 = 20
+    20 = 4
+    
+    **/
+    public int getNext(int n){
+        int sum = 0;
+        
+        while(n>0){
+            int digit = n%10;
+            sum += digit *digit;
+            n=n/10;
         }
-        return fastRunner == 1;
+        
+        return sum;
+    }
+    
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        
+        while(true){
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+            if(slow == fast)
+                break;
+        }
+        return slow == 1;
     }
 }
