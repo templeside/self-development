a732630cc3cd31d65f5023d360238e8165e871f7
templeside <47994862+templeside@users.noreply.github.com>
2022-05-22 11:21:20 -0500

    Time: 3 ms (94.71%), Space: 52.6 MB (87.95%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,31 +1,39 @@
 class Solution {
+    /**          0 1 2 3 4 5 6 7 8
+    height(n) = [1 8 6 2 5 4 8 3 7]
+left                 ^
+right                        ^
+distance = 49
+maxSum=0;
+1. if(leftVal< rightVal)
+    maxSum reinitialize
+    left++
     
-    /**
-height= 1 8 6 2 5 4 8 3 7
-index   0 1 2 3 4 5 6 7 8
-left      ^
-right               ^
-    8-1 = 7 * 7 
+2. else
+    maxSum reinitialize
+    right++
     
+output = max( min(max(left),max(right)) *right-left)
     **/
+    
     public int maxArea(int[] height) {
-        int left=  0;
+        int left = 0;
         int right = height.length-1;
+        int maxSum = 0;
         
-        int maxArea = 0;
-        
-        while(left< right){
-            int currArea = (right-left) * Math.min(height[left], height[right]);
-            maxArea = Math.max(maxArea, currArea);
-            
-            int rightVal = height[right];
+        while(left<right){
             int leftVal = height[left];
+            int rightVal = height[right];
             
-            if(leftVal< rightVal){
+            if(leftVal<rightVal){
+                maxSum = Math.max(maxSum, Math.min(leftVal, rightVal) * (right-left));
                 left++;
-            }else
+            }else{
+                maxSum = Math.max(maxSum, Math.min(leftVal, rightVal) * (right-left));
                 right--;
+            }
         }
-        return maxArea;
+        
+        return maxSum;
     }
 }
