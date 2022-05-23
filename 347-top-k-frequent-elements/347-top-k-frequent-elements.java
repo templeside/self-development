d148a1535fc10dda90c88eafa55665e56c85e052
templeside <47994862+templeside@users.noreply.github.com>
2022-05-23 15:57:03 -0500

    Time: 13 ms (74.56%), Space: 51 MB (18.73%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,21 +1,55 @@
 class Solution {
+    /**
+    so I am planning on using the count as an index and trying to get the values as a 
+    through the hash map, I am counting up the numbers.
+    
+    ex)
+    Input: nums = [1,1,1,2,2,3], k = 2
+    1-3
+    2-2
+    3-1
+    
+    I can list out the maps as a int array<count, value>
+    which means 
+    3-1
+    2-2
+    1-3
+    
+    1,2
+    Output: [1,2]
+
+    **/
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> counter = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        List<Integer>[] bucket = new List[nums.length];
+        
         for(int num: nums){
-            counter.put(num, counter.getOrDefault(num,0)+1);
+            map.put(num, map.getOrDefault(num,0)+1);
         }
         
-        PriorityQueue<Integer> queue = new PriorityQueue((a,b)-> counter.get(b) - counter.get(a));
-        for(int key: counter.keySet()){
-            // System.out.println(new int[]{key, (int)counter.get(key)});
-            queue.add(key);
+        for(int key : map.keySet()){
+            int idx = map.get(key)-1;
+            if(bucket[idx] ==null)
+                bucket[idx] = new ArrayList<>();
+            bucket[idx].add(key);
         }
         
-        int[] returnVal = new int[k];
-        for(int i=0;i<k;i++){
-            int val = queue.poll();
-            returnVal[i] = val;
+        
+        // for(int i=0; i<bucket.length;i++)
+            // System.out.println(bucket[i]);
+        
+        int answerIdx = 0;
+        
+        int[] answer = new int[k];
+        for(int i= bucket.length-1; i>=0 && answerIdx<k; i--){
+            if(bucket[i] !=null){
+                for(int num: bucket[i]){
+                    answer[answerIdx] = num;
+                    answerIdx++;    
+                }
+                
+            }
         }
-        return returnVal;
+        return answer;
     }
 }
