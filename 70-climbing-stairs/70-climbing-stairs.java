b9d6f78243ba774a579001b5f29d4e3d4dd2e019
templeside <47994862+templeside@users.noreply.github.com>
2022-05-24 16:37:42 -0500

    Time: 0 ms (100.00%), Space: 38.6 MB (98.98%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+    if n== 2
+              ^
+f(1)  f(2)   f(3)   
+  1   1*f(1) 1*f(2)
+      2*f(0) 2*f(1)
+      2       3
+    f(i)=f(i-1) + 2*f(i-2)
+    **/
+    public int climbStairs(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(n==2)return 2;
+        int[] memo = new int[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 2;
+        
+        for(int i=3;i<=n;i++){
+            memo[i]=  memo[i-1]+ memo[i-2];
+        }
+        return memo[n];
+    }
+}
