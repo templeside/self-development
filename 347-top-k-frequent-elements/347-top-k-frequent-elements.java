e13f0585f0eced5798cab8fad12243462838b645
templeside <47994862+templeside@users.noreply.github.com>
2022-05-24 22:24:29 -0500

    Time: 14 ms (69.27%), Space: 50.1 MB (62.34%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,55 +1,33 @@
 class Solution {
     /**
-    so I am planning on using the count as an index and trying to get the values as a 
-    through the hash map, I am counting up the numbers.
+    nums = [1,1,1,2,2,3], k = 2
     
-    ex)
-    Input: nums = [1,1,1,2,2,3], k = 2
-    1-3
-    2-2
-    3-1
+    1 - 3
+    2 - 2 
+    3 - 1
     
-    I can list out the maps as a int array<count, value>
-    which means 
-    3-1
-    2-2
-    1-3
+    [1,2]
+    priorityQueue - O(Nlog(N))
     
-    1,2
-    Output: [1,2]
-
     **/
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        List<Integer>[] bucket = new List[nums.length];
+                // <element, count>
+        HashMap<Integer, Integer> counter = new HashMap<>();
         
         for(int num: nums){
-            map.put(num, map.getOrDefault(num,0)+1);
+            counter.put(num, counter.getOrDefault(num,0)+1);
         }
         
-        for(int key : map.keySet()){
-            int idx = map.get(key)-1;
-            if(bucket[idx] ==null)
-                bucket[idx] = new ArrayList<>();
-            bucket[idx].add(key);
+        PriorityQueue<Map.Entry<Integer,Integer>> queue = new PriorityQueue<>((a,b)-> b.getValue()- a.getValue());
+        
+        for(Map.Entry<Integer, Integer> count: counter.entrySet()){
+            queue.add(count);
         }
         
-        
-        // for(int i=0; i<bucket.length;i++)
-            // System.out.println(bucket[i]);
-        
-        int answerIdx = 0;
-        
-        int[] answer = new int[k];
-        for(int i= bucket.length-1; i>=0 && answerIdx<k; i--){
-            if(bucket[i] !=null){
-                for(int num: bucket[i]){
-                    answer[answerIdx] = num;
-                    answerIdx++;    
-                }
-                
-            }
+        int[] returnVal = new int[k];
+        for(int i=0; i<k; i++){
+            returnVal[i] = queue.poll().getKey();
         }
-        return answer;
+        return returnVal;
     }
 }
