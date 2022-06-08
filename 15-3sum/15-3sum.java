1aa40a31eee548805266d3b0502a7ab6f867dc24
templeside <47994862+templeside@users.noreply.github.com>
2022-06-08 16:48:07 -0500

    Time: 27 ms (75.16%), Space: 59.3 MB (39.53%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,52 +1,27 @@
 class Solution {
-    
-    /**
-    Input: nums = [-1,0,1,2,-1,-4]
-    pivot           ^
-    left              ^
-    right                       ^
-    
-    trying to find a sum = nums[pivot]+nums[left]+nums[right] == 0;
-    
-    ending
-    pivot                 ^   
-    left                     ^
-    right                       ^
-    Output: [[-1,-1,2],[-1,0,1]]
-        
-    in this case, I am going to use a pivot and two pointers.
-    
-    no duplicates - checker.
-    how to find duplicates? 
-        for two pointers, move to next pointer avoiding duplicates
-        for pivot, move to next pointer 
-    
-    Time complexity: O(N^2) two for loop(pivot, twosum)
-    */
-    
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int pivot = 0; pivot< nums.length-2;pivot++){
-            if(pivot ==0 || nums[pivot] != nums[pivot-1]){
+        List<List<Integer>> ans = new LinkedList<>();
+        
+        for(int pivot = 0; pivot< nums.length-2; pivot++){
+            if(pivot ==0 || (pivot>0 && nums[pivot] != nums[pivot-1])){     //같지 않을 때.
                 int left = pivot+1;
                 int right = nums.length-1;
+                int target = 0-nums[pivot];
                 
-                while(left < right){
-                    int sum = nums[pivot] + nums[left] + nums[right];
+                while(left<right){
+                    int sum = nums[left] + nums[right];
+                    if(sum == target){
+                        ans.add(Arrays.asList(nums[pivot], nums[left], nums[right]));
+                        while(left< right && nums[left] == nums[left +1])left ++;
+                        while(left<right && nums[right] == nums[right -1]) right --;
+                        left ++; 
+                        right --;
+                    }else if(sum> target) right --;
+                    else left ++;
                     
-                    if(sum == 0){
-                        ans.add(List.of(nums[pivot], nums[left], nums[right]));
-                        while(left<right &&nums[left] == nums[left+1])left++;
-                        while(left< right &&nums[right] == nums[right-1]) right--;
-                        left++;
-                        right--;
-                    }
-                    else if(sum>0)right --;
-                    else left++;
                 }
             }
-            
         }
         return ans;
     }
