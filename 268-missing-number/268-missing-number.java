245801f8a99643c9cb359c05533c1ad20ee63f03
templeside <47994862+templeside@users.noreply.github.com>
2022-06-14 15:50:56 -0500

    Time: 2 ms (30.57%), Space: 50.4 MB (74.78%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,9 +1,30 @@
 class Solution {
+    /**
+    index and value should be same.
+    nums = [3,0,1]
+    idx     0 1 2
+    sorted=[0,1,3]
+    
+    the idx 2 != 3, so the value should return 2.
+    if the sorted is perfact match with nums, that means the n is not able to match, so return the number n.
+    nums[i] == nums[nums[i]];
+    */
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0; i< nums.length;i++){
-            if(nums[i] !=i)
-                return i;
+        int i=0; 
+        while(i<nums.length){
+            if(nums[i]< nums.length && nums[i] != nums[nums[i]]){
+                swap(nums, i, nums[i]);
+            }else
+                i++;
+        }
+        
+        for(i=0; i< nums.length; i++){
+            if(nums[i]== nums.length || nums[i] != nums[nums[i]]) return i;
         }
         return nums.length;
     }
