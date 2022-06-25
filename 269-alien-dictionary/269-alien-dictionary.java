17cd2bd892a82b305f5af7a20158e68476052b5f
templeside <47994862+templeside@users.noreply.github.com>
2022-06-25 17:54:18 -0500

    Time: 8 ms (38.95%), Space: 43.1 MB (24.20%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.java
+++ 269-alien-dictionary/269-alien-dictionary.java
@@ -1,62 +1,59 @@
 class Solution {
     public String alienOrder(String[] words) {
-    if (words == null || words.length == 0)
-      return "";
+        if(words ==null || words.length ==0)return "";
+        
+        // a. initialize the graph
+        HashMap<Character, Integer> inDegree = new HashMap<>(); // count of incoming edges for every vertex.
+        HashMap<Character, List<Character>> graph = new HashMap<>();    // adjacency list graph.
+        for(String word: words){
+            for(char character : word.toCharArray()){
+                inDegree.put(character,0);
+                graph.put(character, new ArrayList<Character>());
+            }
+        }
+        
+        // b. build the graph
+        for(int i=0; i< words.length-1; i++){
+            String w1=words[i], w2=words[i+1];  // find  the ording of characters.
+            // Check that word2 is not a prefix of w1.
+            if (w1.length() > w2.length() && w1.startsWith(w2)) {
+                return "";
+            }
 
-    // a. Initialize the graph
-    HashMap<Character, Integer> inDegree = new HashMap<>(); // count of incoming edges for every vertex
-    HashMap<Character, List<Character>> graph = new HashMap<>(); // adjacency list graph
-    for (String word : words) {
-      for (char character : word.toCharArray()) {
-        inDegree.put(character, 0);
-        graph.put(character, new ArrayList<Character>());
-      }
-    }
-
-    // b. Build the graph
-    for (int i = 0; i < words.length - 1; i++) {
-      String w1 = words[i], w2 = words[i + 1]; // find ordering of characters from adjacent words
-        // Check that word2 is not a prefix of word1.
-        if (w1.length() > w2.length() && w1.startsWith(w2)) {
+            for(int j=0; j<Math.min(w1.length(), w2.length());j++){
+                char parent = w1.charAt(j), child= w2.charAt(j);
+                if(parent !=child){                             //if two characters are different, 
+                    inDegree.put(child, inDegree.get(child)+1); //put the child into it's parent list
+                    graph.get(parent).add(child);               //increment child's inDegree
+                break;                                          //only checking the first different character between the two words
+                }
+            }
+        }
+        
+        // 여기 부터는 기존 topological sort와 같음.
+        // c. find all sources i.e., all verticies with 0 inDegrees
+        Queue<Character> sources = new LinkedList<>();
+        for(Map.Entry<Character, Integer> entry: inDegree.entrySet()){
+            if(entry.getValue()==0)
+                sources.add(entry.getKey());
+        }
+        
+        // d. for each source, add it to the sortedOrder and subtract one from all of its schildren's inDegrees if a hild's inDegree becomes zero, add it to the sources queue.
+        StringBuilder sortedOrder = new StringBuilder();
+        
+        while(!sources.isEmpty()){
+            Character vertex = sources.poll();
+            sortedOrder.append(vertex);
+            List<Character> children = graph.get(vertex);
+            for(Character child: children){
+                inDegree.put(child, inDegree.get(child)-1);
+                if(inDegree.get(child) ==0)
+                    sources.add(child);
+            }
+        }
+        
+        if(sortedOrder.length() !=inDegree.size())
             return "";
-        }
-
-      for (int j = 0; j < Math.min(w1.length(), w2.length()); j++) {
-        char parent = w1.charAt(j), child = w2.charAt(j);
-        if (parent != child) { // if the two characters are different
-          graph.get(parent).add(child); // put the child into it's parent's list
-          inDegree.put(child, inDegree.get(child) + 1); // increment child's inDegree
-          break; // only the first different character between the two words will help us find the order
-        }
-      }
-    }
-
-    // c. Find all sources i.e., all vertices with 0 in-degrees
-    Queue<Character> sources = new LinkedList<>();
-    for (Map.Entry<Character, Integer> entry : inDegree.entrySet()) {
-      if (entry.getValue() == 0)
-        sources.add(entry.getKey());
-    }
-
-    // d. For each source, add it to the sortedOrder and subtract one from all of its children's in-degrees
-    // if a child's in-degree becomes zero, add it to the sources queue
-    StringBuilder sortedOrder = new StringBuilder();
-    while (!sources.isEmpty()) {
-      Character vertex = sources.poll();
-      sortedOrder.append(vertex);
-      List<Character> children = graph.get(vertex); // get the node's children to decrement their in-degrees
-      for (Character child : children) {
-        inDegree.put(child, inDegree.get(child) - 1);
-        if (inDegree.get(child) == 0)
-          sources.add(child);
-      }
-    }
-
-    // if sortedOrder doesn't contain all characters, there is a cyclic dependency between characters, therefore, we
-    // will not be able to find the correct ordering of the characters
-    if (sortedOrder.length() != inDegree.size())
-      return "";
-
-    return sortedOrder.toString();
+        return sortedOrder.toString();
     }
 }
