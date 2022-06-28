1d59422731fc398662f06bc7b556338da0ef263f
templeside <47994862+templeside@users.noreply.github.com>
2022-06-28 11:44:57 -0500

    Time: 67 ms (75.66%), Space: 75.1 MB (7.29%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int startIdx=0, endIdx=s.length()-1;
+        Integer[][] dp = new Integer[s.length()][s.length()];
+        return recursion(dp, s, startIdx, endIdx);
+    }
+    
+    public int recursion(Integer[][] dp, String s, int startIdx, int endIdx){
+        if(startIdx > endIdx)return 0;
+        else if(startIdx ==endIdx) return 1;
+        
+        if(dp[startIdx][endIdx] !=null)return dp[startIdx][endIdx];// dp 이미 있을 때
+        
+        if(s.charAt(startIdx) == s.charAt(endIdx)){
+            dp[startIdx][endIdx] = 2+ recursion(dp, s, startIdx+1, endIdx-1);
+        }else{
+            dp[startIdx][endIdx] = Math.max(recursion(dp, s, startIdx+1, endIdx), recursion(dp, s, startIdx, endIdx-1));
+        }
+        
+        return dp[startIdx][endIdx];
+    }
+}
