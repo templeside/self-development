5045a4dff748931234e014df71ca94c5ca26439c
templeside <47994862+templeside@users.noreply.github.com>
2022-07-03 15:39:45 -0500

    Time: 28 ms (76.04%), Space: 58.5 MB (73.36%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,65 +1,64 @@
 class Solution {
-    /**
-    for(int i=0; i< nums.length; i++){
-        for(int j=i+1; j< nums.length; j++){
-            for(int k = j+1; nums.length; j++){
-                nums[i]+nums[j]+nums[k] == 0 
-                returnArr.add (i,j,k);
-            }
-        }
-    }
-    time complexity: O(N^3). 
-    0. sorted
-    1. iterate through the nums, nums[i] = a.
-    2. find out -a in the second loop. two sum.
+    /*
+    for(int i=0; i< nums.length-2; i++){
+        for(int j=i+1; j< nums.length-1; j++){
+            for(int k=j+1; k< nums.length; k++){
+    }}}
+    time complexity: o(N^3)
     
+    two sum
+    pivot,(for loop)
+        two sum ( two pointer)
 
-    
+algorithm:
+    1. sort
+    2. set my pivot, left, right idx.
+    3. compare through, and iterate it.
+        pivot = -target. find the target with the left and right index.
+
 Input: nums = [-4,-1,-1,0,1,2]
-pivot                   ^            +1
-left[pivot+1]             ^
-right[length-1]           ^        -1,2
+pivot                   ^
+left                      ^
+right                     ^
+target 0
+sum(left+right) 3
 
--1,-1,2 -1,0,1
-
+returnVal[[-1,-1,2],[-1,0,1]]
 Output: [[-1,-1,2],[-1,0,1]]
 
-edge cases = duplicated values, increment it.
+edge case: 
+    1. pivot[i] != pivot[i-1].
+    2. figure out my left and right.        
+    
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
 
-time complexity: O(N^2)
-    **/
+    */
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums.length < 3) return new ArrayList<>();
-        
         List<List<Integer>> returnVal = new ArrayList<>();
-        Arrays.sort(nums);
         
-        for(int pivotIdx = 0; pivotIdx < nums.length-2; pivotIdx++){
-            int target = 0- nums[pivotIdx];
-            
-            int left = pivotIdx+1;
-            int right = nums.length-1;
-            
-            while(left< right && (pivotIdx == 0 || nums[pivotIdx] != nums[pivotIdx-1])){
-                int sum = nums[left]+nums[right];
-                if(sum == target){
-                    List<Integer> temp = (List.of(nums[pivotIdx], nums[left], nums[right]));
-
-                    
-                    returnVal.add(temp);
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
-                    left++;
-                    right--;
-                }
-                else if(sum> target){
-                    right--;
-                }
-                else{
-                    left++;
+        Arrays.sort(nums);// O(N log N)
+        for(int pivot = 0; pivot< nums.length-2; pivot++){
+            if(pivot ==0 || nums[pivot] != nums[pivot-1]){
+                int left=pivot+1, right = nums.length-1;
+                int target = -nums[pivot];
+                
+                while(left < right){
+                    int sum  = nums[left]+nums[right];
+                    if(target == sum){
+                        returnVal.add(List.of(nums[pivot], nums[left], nums[right]));
+                        while(left<right && nums[left] == nums[left+1]) left ++;
+                        while(left< right && nums[right] == nums[right -1]) right --;
+                        left++;
+                        right--;
+                    }
+                    else if(target < sum){
+                        right --;
+                    }
+                    else
+                        left ++ ;
                 }
             }
-            
         }
         return returnVal;
     }
