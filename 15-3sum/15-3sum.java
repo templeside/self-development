14fca0732759e9b44c55c1f551d40dac8a1e7382
templeside <47994862+templeside@users.noreply.github.com>
2022-07-03 14:40:36 -0500

    Time: 32 ms (61.05%), Space: 58.3 MB (75.54%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,28 +1,66 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new LinkedList<>();
-        
-        for(int pivot = 0; pivot< nums.length-2; pivot++){
-            if(pivot ==0 || (pivot>0 && nums[pivot] != nums[pivot-1])){     //같지 않을 때.
-                int left = pivot+1;
-                int right = nums.length-1;
-                int target = 0-nums[pivot];
-                
-                while(left<right){
-                    int sum = nums[left] + nums[right];
-                    if(sum == target){
-                        ans.add(Arrays.asList(nums[pivot], nums[left], nums[right]));
-                        while(left< right && nums[left] == nums[left +1])left ++;
-                        while(left<right && nums[right] == nums[right -1]) right --;
-                        left ++; 
-                        right --;
-                    }else if(sum> target) right --;
-                    else left ++;
-                    
-                }
+    /**
+    for(int i=0; i< nums.length; i++){
+        for(int j=i+1; j< nums.length; j++){
+            for(int k = j+1; nums.length; j++){
+                nums[i]+nums[j]+nums[k] == 0 
+                returnArr.add (i,j,k);
             }
         }
-        return ans;
+    }
+    time complexity: O(N^3). 
+    0. sorted
+    1. iterate through the nums, nums[i] = a.
+    2. find out -a in the second loop. two sum.
+    
+
+    
+Input: nums = [-4,-1,-1,0,1,2]
+pivot                   ^            +1
+left[pivot+1]             ^
+right[length-1]           ^        -1,2
+
+-1,-1,2 -1,0,1
+
+Output: [[-1,-1,2],[-1,0,1]]
+
+edge cases = duplicated values, increment it.
+
+time complexity: O(N^2)
+    **/
+    public List<List<Integer>> threeSum(int[] nums) {
+        if(nums.length < 3) return new ArrayList<>();
+        
+        List<List<Integer>> returnVal = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int pivotIdx = 0; pivotIdx < nums.length-2; pivotIdx++){
+            int target = 0- nums[pivotIdx];
+            
+            int left = pivotIdx+1;
+            int right = nums.length-1;
+            
+            while(left< right && (pivotIdx == 0 || nums[pivotIdx] != nums[pivotIdx-1])){
+                int sum = nums[left]+nums[right];
+                if(sum == target){
+                    List<Integer> temp = (List.of(nums[pivotIdx], nums[left], nums[right]));
+
+                    
+                    returnVal.add(temp);
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                }
+                else if(sum> target){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+            
+        }
+        return returnVal;
     }
 }
