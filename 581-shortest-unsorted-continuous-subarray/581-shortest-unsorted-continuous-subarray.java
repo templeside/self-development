37d6adf9d5dcba9fe849ae5df7b62105b72418fa
templeside <47994862+templeside@users.noreply.github.com>
2022-07-04 13:22:40 -0500

    Time: 1 ms (100.00%), Space: 53.7 MB (72.47%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,27 +1,39 @@
 class Solution {
+    /**
+Input: nums = [2,6,4,8,10,9,15]
+left             ^
+right                   ^
+[2,15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+
+        [2,1]
+left     ^
+right      ^
+max 
+min
+
+    **/
     public int findUnsortedSubarray(int[] nums) {
-        int left = 0, n= nums.length, right = n-1;
-        while(left < n-1 && nums[left] <=nums[left+1])
+        int left = 0, right = nums.length-1;
+        while(left< nums.length-1 && nums[left] <= nums[left+1])
             left++;
-        if(left==n-1)return 0;
+        if(left == nums.length -1) return 0;
         
-        while(right>0 && nums[right]>= nums[right-1])
+        while(right>0 && nums[right-1] <= nums[right])
             right--;
         
-        int min=nums[left];
-        int max=nums[left];
-        for(int i=left; i<=right; i++ ){
-            min = Math.min(min, nums[i]);
+        int max = nums[left];
+        int min = nums[left];
+        for(int i=left; i<= right; i++){
             max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
         }
         
-        while(left>0 && nums[left-1] > min){
+        while(left>0 && min< nums[left-1])
             left--;
-        }
-        
-        while(right <n-1 && nums[right+1] < max){
+        while(right<nums.length-1 && max> nums[right+1])
             right++;
-        }
         
         return right-left+1;
     }
