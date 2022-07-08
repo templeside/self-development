40224a430cddc77c3c283353c73cee79acd99063
templeside <47994862+templeside@users.noreply.github.com>
2022-07-07 21:52:18 -0500

    Time: 5 ms (53.67%), Space: 127.6 MB (46.22%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -8,8 +8,8 @@ class Solution {
         int i=0;
         while( i< nums.length){
             int origIdx = nums[i]-1;
-            if( nums[i]>0 && nums[i]<=nums.length && nums[i] != nums[nums[i]-1])
-                swap(nums,i,nums[i]-1);
+            if( nums[i]>0 && nums[i]<=nums.length && nums[i] != nums[origIdx])
+                swap(nums,i,origIdx);
             else
                 i++;
         }
