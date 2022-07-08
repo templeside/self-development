a4e9347baa27d67279b28753ee863b0cb606e815
templeside <47994862+templeside@users.noreply.github.com>
2022-07-08 17:39:22 -0500

    Time: 2 ms (84.76%), Space: 55.8 MB (40.53%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,36 +1,50 @@
 class Solution {
     /**
-brute force way - timecomplexity: O(N logN)
+    다 square 해버려서 sor 해버리면 된다.
+    O(N log N).
+    However, I can do better because it is already sorted.
+    which means, if I know the 0, then I can do a better solution because
+    
+    negative values are becoming positive if squaring.
+    
+    what I will do is using two pointers, I can do better solution.
+    
 Input: nums = [-4,-1,0,3,10]
-newNums = [16,1,0,9,100]
-sort(newNums) = [0,1,9,16,100]
+                ^         ^
+Input: nums = [-4,-1,0,3,10]
+                ^      ^
+Input: nums = [-4,-1,0,3,10]
+                     ^ 
+                     ^
+[0, 1,9,16,100]
+1. while having negative numbers, compare with postivie numbers.
+2. after no negative numbers, fill out the variables in the array.
 
-    [16,1,0,9,100]
-left^
-right               ^
-output = [0, 1, 9, 16, 100]
+Output: [0,1,9,16,100]
 
-    [16,1,0,9,100]
-left        ^
-right     ^
-output = [0,1,9,16,100]
-
-O(N) 
+    ex)
+    
     **/
     public int[] sortedSquares(int[] nums) {
-        int[] newNum = new int[nums.length];
-        int left=0, right=nums.length-1;
+        int left = 0, right = nums.length-1;
         
-        for(int newIdx= nums.length-1; newIdx>=0 ; newIdx--){
+        int[] returnVal = new int[nums.length];
+        for(int i= nums.length-1; i>=0 ;i--){
+            int negative = nums[left];
+            int positive = nums[right];
             
-            if(nums[left] <0 && nums[left]*nums[left] > nums[right]*nums[right]){
-                newNum[newIdx] = nums[left]*nums[left];
-                left ++;
-            }else{
-                newNum[newIdx] = nums[right]*nums[right];
-                right --;
+            if(negative <0){
+                
+                //compare
+                if(negative * negative >= positive*positive){
+                    returnVal[i] = negative * negative;
+                    left++;
+                    continue;
+                }
             }
+            returnVal[i] = positive*positive;
+            right--;
         }
-        return newNum;
+        return returnVal;
     }
 }
