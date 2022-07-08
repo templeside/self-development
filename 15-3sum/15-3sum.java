2558e8fc08efd83943b7a5176fcb8383691f5b23
templeside <47994862+templeside@users.noreply.github.com>
2022-07-08 16:45:31 -0500

    Time: 40 ms (41.80%), Space: 59.2 MB (48.33%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,39 +1,37 @@
 class Solution {
+    /**
+    3sum -sorted, and compare - left right, two pointer, - to check is there a target number 0.
+    **/
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> returnVal = new ArrayList<>();
-        int target = 0;
-        int n = nums.length;
-        for(int i=0; i<n-2; i++ ){
-            if(i>0 &&nums[i] ==nums[i-1])continue;
-            
-            int left=  i+1;
-            int right = n-1;
-            while(left< right){
-                int sum = nums[left]+ nums[right]+nums[i];
-                if(sum == 0){
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[left]);
-                    temp.add(nums[right]);
-                    returnVal.add(temp);
-                    
-                    
-                    
-                    while(left<right &&nums[left] == nums[left+1]) left++;
-                    while(left<right && nums[right] == nums[right-1]) right--;
-                    
-                    left ++;
-                    right --;
-                }else if(sum> 0){
-                    // while(left<right && nums[right] == nums[right-1]) right--;
-                    right --;
-                }else{
-                    // while(left<right &&nums[left] == nums[left+1]) left++;
-                    left++;
+        
+        for(int i=0; i<nums.length-2; i++){
+            if(i ==0 || nums[i] !=nums[i-1]){
+                int j = i+1;
+                int k = nums.length-1;
+
+                while(j< k){
+                    int sum = nums[i]+nums[j]+nums[k];
+
+                    if(sum ==0 ){
+                        returnVal.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while(j<k && nums[j] == nums[j+1]) j++;
+                        while(j<k && nums[k] == nums[k-1]) k--;
+                        j++;
+                        k--;
+                    }
+                    else if(sum>0){
+                        while(j<k && nums[k] == nums[k-1]) k--;
+                        k--;
+                    }else{
+                        while(j<k && nums[j] == nums[j+1]) j++;
+                        j++;
+                    }
                 }
             }
         }
+        
         return returnVal;
     }
 }
