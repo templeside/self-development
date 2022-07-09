06e8ac0b1ae3ad4af95ea8c28f860d22355b9c9c
templeside <47994862+templeside@users.noreply.github.com>
2022-07-08 20:40:43 -0500

    Time: 7 ms (45.65%), Space: 40.5 MB (95.27%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,15 +1,31 @@
 class Solution {
+/**
+palindrome should be 
+find the palindrome index, and compare with the i+1, i-1.
+
+Input: s = "abccccdd"
+one odd value, even numbers 
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+
+**/
     public int longestPalindrome(String s) {
-        int[] count = new int[128];
-        for(char c: s.toCharArray())
-            count[c] ++;
-        
-        int ans = 0;
-        for(int v: count){
-            ans += v/2*2;
-            if(ans %2 ==0 && v%2 ==1)
-                ans++;
+        HashMap<Character, Integer> counter = new HashMap<>();
+        for(char c: s.toCharArray()){
+            counter.put(c, counter.getOrDefault(c, 0)+1);
         }
-        return ans;
+        
+        boolean isOdd = false;
+        int count = 0;
+        for(char key : counter.keySet()){
+            int val = counter.get(key);
+            count += (val/2)*2;
+            
+            if(val%2 ==1 && !isOdd)
+                isOdd=true;
+        }
+        
+        return isOdd? count+1: count;
     }
 }
