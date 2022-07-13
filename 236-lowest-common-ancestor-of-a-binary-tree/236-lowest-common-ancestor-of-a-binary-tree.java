fc1c744e621122b259f3cee129f031b8e465cf4c
templeside <47994862+templeside@users.noreply.github.com>
2022-07-13 16:53:11 -0500

    Time: 8 ms (78.60%), Space: 47.9 MB (21.54%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,36 +8,36 @@
  * }
  */
 class Solution {
-/**
-lca 
-1. p is on lca
-2. q is on lca
-
-non lca
-3. left and right lca
-4. only found left
-5. only found right
-
-base case null
-return the ancestor.
-            3
-       (5)       1
-    6    2     0  8   
-        7  4
-
-5,4
-**/
+    /**
+    이걸 어떻게 표현 할 건데????????
+    미친 또라이 쉐퀴들아
+    개새꺄
+    죽여버리고 시퍼
+    ㅎㅎ
+    when curr is LCA
+    1. left & right is p and q
+    2. one is under the another
+    
+    when curr is not LCA
+    3. no p or q
+    4. in path
+    5. in subtree.
+    
+    base case when null
+    
+    return p or q or lowest common ancestor.
+    */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root ==null)return null;
+        if(root ==null)return null; // 
         
-        if(root == p || root ==q)return root;
+        if(root == p || root == q)return root;// #2
         
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p,q);
         
-        if(left !=null && right !=null)return root;
-        if(left !=null)return left;
-        if(right !=null)return right;
+        if(left !=null&& right !=null) return root; // #1
+        if(left !=null) return left;    //#4, #5
+        if(right !=null) return right;  //#4, #5
         
         return null;
     }
