43452bd4e9da95889534fd403cf160efe18ea3d2
templeside <47994862+templeside@users.noreply.github.com>
2022-07-17 00:13:03 -0500

    Time: 33 ms (21.41%), Space: 70 MB (55.67%) - LeetHub

--- 320-generalized-abbreviation/320-generalized-abbreviation.java
+++ 320-generalized-abbreviation/320-generalized-abbreviation.java
@@ -1,22 +1,56 @@
 class Solution {
+    /**
+    backtrack condition:
+        abbreviated
+        not abbreviated.
+        
+        
+            w           |1
+            wo|w1       |1o     |2
+       wor|1or|w1o|w2   |1or|1o1|2r|3
+word|wor1 |...
+   
+    basecase- when reaches to the end of the index.
+    
+    currabbrev & currString
+    currabbrev - count of abbreviation by.
+    currString - representation of the return string.
+    
+    backtrack( currentabbrev+1, currstring)
+    backtrack( currentabbrev, currstring+1)
+    
+    if currentabbrev >0, then do something.
+    if currentabbrev <0 not in, do something.
+    **/
     public List<String> generateAbbreviations(String word) {
-        List<String> result = new ArrayList<>();
-        dfs(word, new StringBuilder(), 0,0,result);
-        return result;
+        int currIdx = 0;
+        int currAbbrev = 0;
+        String currString="";
+        List<String> returnVal = new ArrayList<>();
+        
+        backtrack(currIdx, currAbbrev, currString, word, returnVal);
+        return returnVal;
     }
     
-    public void dfs(String word, StringBuilder abWord, int start, int count, List<String> result){
-        if(start ==word.length()){
-            if(count !=0)
-                abWord.append(count);
-            result.add(abWord.toString());
+    public void backtrack(int currIdx, int currAbbrev, String currString, String word, List<String> returnVal){
+        if(currIdx == word.length()){
+            if(currAbbrev >0){
+                currString += Integer.toString(currAbbrev);
+            }
+            returnVal.add(currString);
+            return;
         }
-        else{
-            dfs(word, new StringBuilder(abWord), start+1, count+1, result);
-            
-            if(count !=0)
-                abWord.append(count);
-            dfs(word, new StringBuilder(abWord).append(word.charAt(start)), start+1, 0, result);
+        
+        //do without abbreviation
+        if(currAbbrev>0){
+            String newString = currString + Integer.toString(currAbbrev)+word.charAt(currIdx);
+            int newAbbrev=0;
+            backtrack(currIdx+1, newAbbrev, newString, word, returnVal);
+        }else{
+            backtrack(currIdx+1, currAbbrev, currString+word.charAt(currIdx), word, returnVal);
         }
+        
+        //with abbreviation
+        backtrack(currIdx+1, currAbbrev+1, currString, word, returnVal);
     }
 }
