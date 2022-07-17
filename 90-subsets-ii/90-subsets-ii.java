6ac1e68283be67dfde03aaac711dff565e88f21f
templeside <47994862+templeside@users.noreply.github.com>
2022-07-16 22:06:50 -0500

    Time: 2 ms (85.11%), Space: 44.5 MB (17.69%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,28 +1,22 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res= new ArrayList<>();
-        for(int digit=0; digit<=nums.length; digit++){
-            dfs(nums, res, new ArrayList<>(), digit, 0);
-        }
-        return res;
+        List<List<Integer>> returnVal = new ArrayList<>();
+        List<Integer> currPath = new ArrayList<>();
+        int currIdx =0;
+        
+        backtrack(currIdx, currPath, returnVal, nums);
+        return returnVal;
     }
     
-    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> currentSubset, int digit, int start){
-        // 1. base case
-        if(currentSubset.size()==digit){
-            res.add(new ArrayList<>(currentSubset));
-            return;
-        }
-        // 2. combinations
-        for(int i=start; i<nums.length; i++){
-            // ignore if current number is duplicate
-            if(i!=start&& nums[i]==nums[i-1])    //starting index가 아닌 다른 values들과 duplication인지 확인.                
-                continue;
-           
-            currentSubset.add(nums[i]);// add
-            dfs(nums, res, currentSubset, digit, i+1); // backtrack
-            currentSubset.remove(currentSubset.size()-1); // remove
+    public void backtrack(int currIdx,List<Integer> currPath,List<List<Integer>> returnVal,int[] nums){
+        returnVal.add(new ArrayList<>(currPath));
+        
+        for(int i=currIdx; i<nums.length; i++){
+            if(i>currIdx && nums[i]==nums[i-1])continue;    //복사 멈춰!
+            currPath.add(nums[i]);
+            backtrack(i+1, currPath, returnVal, nums);
+            currPath.remove(currPath.size()-1);
         }
     }
 }
