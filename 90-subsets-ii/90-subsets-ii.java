5fe03bf53de18d4e697232e2ee00f954e3559f2e
templeside <47994862+templeside@users.noreply.github.com>
2022-07-16 19:44:19 -0500

    Time: 2 ms (85.11%), Space: 44 MB (64.02%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,25 +1,28 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        
-        List<List<Integer>> subsets = new ArrayList<>();
-        
-        subsets.add(new ArrayList<>());
-        int startIdx =0, endIdx=0;
-        
-        for(int i=0; i< nums.length; i++){
-            startIdx=0;
-            
-            if(i>0 && nums[i] == nums[i-1])
-                startIdx = endIdx+1;
-            endIdx = subsets.size()-1;
-            
-            for(int j= startIdx; j<= endIdx; j++){
-                List<Integer> subset = new ArrayList<>(subsets.get(j));
-                subset.add(nums[i]);
-                subsets.add(subset);
-            }
+        List<List<Integer>> res= new ArrayList<>();
+        for(int digit=0; digit<=nums.length; digit++){
+            dfs(nums, res, new ArrayList<>(), digit, 0);
         }
-        return subsets;
+        return res;
+    }
+    
+    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> currentSubset, int digit, int start){
+        // 1. base case
+        if(currentSubset.size()==digit){
+            res.add(new ArrayList<>(currentSubset));
+            return;
+        }
+        // 2. combinations
+        for(int i=start; i<nums.length; i++){
+            // ignore if current number is duplicate
+            if(i!=start&& nums[i]==nums[i-1])    //starting index가 아닌 다른 values들과 duplication인지 확인.                
+                continue;
+           
+            currentSubset.add(nums[i]);// add
+            dfs(nums, res, currentSubset, digit, i+1); // backtrack
+            currentSubset.remove(currentSubset.size()-1); // remove
+        }
     }
 }
