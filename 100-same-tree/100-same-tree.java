79cb3abc4037c7814aad97b09effa2425ff76735
templeside <47994862+templeside@users.noreply.github.com>
2022-07-18 15:03:12 -0500

    Time: 0 ms (100.00%), Space: 41.7 MB (33.52%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,11 +14,14 @@
  * }
  */
 class Solution {
+    /**
+    check the current node it is same by recursing.
+    **/
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null)return true;
-        else if(p ==null || q==null)return false;
+        if(p== null && q == null)return true;
+        if(p==null || q==null)return false;
+        if(p.val !=q.val)return false;
         
-        if(p.val != q.val)return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
