7e7b892583d464a18e7873cc0f5f30bb356579ab
templeside <47994862+templeside@users.noreply.github.com>
2022-07-18 16:25:01 -0500

    Time: 1 ms (99.67%), Space: 48.4 MB (18.24%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -13,32 +13,24 @@
  *     }
  * }
  */
-/**
-dfs(root)
-reutrns max sum path left or right.
-
-1.iterate through each path
-2. find the left and right path.
-3. left+right+ curr.val -> compare with maxSum and switch to the maxSum.
-4. return the max path length including curr.val
-**/
 class Solution {
-    public int max;
+    public static Integer max;
     public int maxPathSum(TreeNode root) {
         max = Integer.MIN_VALUE;
-        dfs(root);
+        
+        backtrack(root);
         return max;
     }
     
-    public int dfs(TreeNode root){
+    public int backtrack(TreeNode root){
         if(root ==null)return 0;
         
-        int left= Math.max(dfs(root.left),0);
-        int right = Math.max(dfs(root.right),0);
+        int left = Math.max(backtrack(root.left),0);
+        int right = Math.max(backtrack(root.right),0);
         
-        int currSum = left+right+ root.val;
-        max = Math.max(max, currSum);
+        int totalSum = left+right+ root.val;
+        max = Math.max(max, totalSum);
         
-        return Math.max(left, right)+root.val;
+        return Math.max(left,right)+root.val;
     }
 }
