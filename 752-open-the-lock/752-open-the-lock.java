e1dcd320b9d61aed43a543f1a4128c7731d87619
templeside <47994862+templeside@users.noreply.github.com>
2022-07-21 21:38:53 -0500

    Time: 229 ms (58.74%), Space: 50.1 MB (88.75%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.java
+++ 752-open-the-lock/752-open-the-lock.java
@@ -1,37 +1,59 @@
 class Solution {
+    /**
+    starting from '0000' heading to the target.
+    
+    deadends are obstacles.
+    
+    do the bfs with the lock.
+        get the children by adding 
+    
+    if could not found, return -1.
+    **/
     public int openLock(String[] deadends, String target) {
-        Set<String> dead = new HashSet();
-        for (String d: deadends) dead.add(d);
-
+        if(target.equals("0000"))return 0;
+        
+        Set<String> deadend = new HashSet<>();
+        for(String dead: deadends){
+            if(dead.equals("0000"))return -1;
+            deadend.add(dead);
+            
+        }
+            
+        
         Queue<String> queue = new LinkedList();
-        queue.offer("0000");
-        queue.offer(null);
-
-        Set<String> seen = new HashSet();
-        seen.add("0000");
-
-        int depth = 0;
-        while (!queue.isEmpty()) {
-            String node = queue.poll();
-            if (node == null) {
-                depth++;
-                if (queue.peek() != null)
-                    queue.offer(null);
-            } else if (node.equals(target)) {
-                return depth;
-            } else if (!dead.contains(node)) {
-                for (int i = 0; i < 4; ++i) {
-                    for (int d = -1; d <= 1; d += 2) {
-                        int y = ((node.charAt(i) - '0') + d + 10) % 10;
-                        String nei = node.substring(0, i) + ("" + y) + node.substring(i+1);
-                        if (!seen.contains(nei)) {
-                            seen.add(nei);
-                            queue.offer(nei);
-                        }
-                    }
+        queue.add("0000");
+        int count =0;
+        
+        while(queue.size()>0){
+            count ++;
+            int n= queue.size();
+            
+            for(int i=0; i< n;i++){
+                String currString = queue.poll();
+                
+                List<String> adjacents = getValidAdjacents(currString, deadend);
+                for(String adjacent: adjacents){
+                    if(adjacent.equals(target))return count;
+                    queue.add(adjacent);
+                    deadend.add(adjacent);
                 }
             }
         }
         return -1;
+    }
+    
+    public List<String> getValidAdjacents(String currString, Set<String> deadend){
+        List<String> adjacents = new LinkedList<>();
+        
+        for(int i=0; i< 4; i++){
+            for(int d= -1; d<=1; d+=2){
+                int y = ((currString.charAt(i)-'0')+d+10)%10;
+                String adjacent = currString.substring(0,i)+(""+y)+currString.substring(i+1);
+                if(!deadend.contains(adjacent)){
+                    adjacents.add(adjacent);
+                }
+            }
+        }
+        return adjacents;
     }
 }
