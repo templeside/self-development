ae3af0472e90dd0eaa8e33e65e3f1c5fc08d2b7e
templeside <47994862+templeside@users.noreply.github.com>
2022-07-27 20:31:43 -0500

    Time: 4 ms (82.76%), Space: 74.5 MB (52.51%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,13 +1,5 @@
 class Solution {
-    /**
-    need to find the max area I could get. starting from the most outward would be great to have.
-    
-    having two pointers, trying to find a max value I could get.
-    
-    maxVal = 0;
-    
-    shifting the smaller value. as much as I could.
-    
+    /**    
                  0 1 2 3 4 5 6 7 8
 Input: height = [1,8,6,2,5,4,8,3,7]
 left               ^
@@ -17,18 +9,18 @@ Output: 49
 
     **/
     public int maxArea(int[] height) {
-        int left = 0, right = height.length-1;
+        int leftIdx = 0, rightIdx = height.length-1;
         int maxArea = 0;
-        while(left<right){
-            int leftVal = height[left];
-            int rightVal = height[right];
+        while(leftIdx<rightIdx){
+            int leftVal = height[leftIdx];
+            int rightVal = height[rightIdx];
             
             if(leftVal> rightVal){
-                maxArea = Math.max(maxArea, rightVal * (right-left));
-                right --;
+                maxArea = Math.max(maxArea, rightVal * (rightIdx-leftIdx));
+                rightIdx --;
             }else{
-                maxArea = Math.max(maxArea, leftVal * (right-left));
-                left++;
+                maxArea = Math.max(maxArea, leftVal * (rightIdx-leftIdx));
+                leftIdx++;
             }
         }
         return maxArea;
