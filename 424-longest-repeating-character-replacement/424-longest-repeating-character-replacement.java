0f7eae01b76f39472fabbcf3bbd13d329c9821d0
templeside <47994862+templeside@users.noreply.github.com>
2022-07-30 12:33:53 -0500

    Time: 41 ms (25.53%), Space: 45 MB (27.33%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,43 +1,51 @@
 class Solution {
     /**
-    intuition I thought I could use backtracking, iterating through all of each characters and decrementing the k until reaches to 0. 
-    In this case, Time complexity: O(2^N)
+    having the two pointer, start, end point to track on the character.
     
-    however, the s is string and we should only know the longest repeating character, we can use. 
+    having the slidingWindow  - having the updating the k.
     
-            ABAB, K=2
-windowStart ^
-windowEnd     ^
+    hashmap으로 업데이트 maxRepeatedCount 업데이트 하면 된다.
+    그러면 maxSubstring 찾는 거는 ???? Math.max(windowEnd-windowStart+1) 하면 된다.
     
-    returnVal = 2
-    k=1
+    그리고 k는????? shrink condition: len(current window) - maxRepeatedCount < k.
     
-    first we need to count the maximum count and check it is valid with K replacables.
-    if the currMax is not valid, move window start position to make it valid.
+    keep updating max repeated count를 미리미리 해 주는 것!
     
-    then update the returnVal comparing with current max.
-    
-    windowEnd - windowStart +1 = current window
+    "AABABBA" k=1
+start^
+end      ^
+
+map={A- 3, B-2}
+
+windowLength = 4
+longestSubstring = 4
+maxRepeatedCount = 3
+
+
     **/
     public int characterReplacement(String s, int k) {
-        int windowStart = 0;
-        int currMax =0;
-        int returnVal = 0;
-        HashMap<Character, Integer> counter = new HashMap<>();
+        int startIdx = 0;
+        int longestSubstring = 0;
+        int maxRepeatedCount = 0;
         
-        for(int windowEnd =0; windowEnd< s.length(); windowEnd++){
-            char endChar = s.charAt(windowEnd);
-            counter.put(endChar, counter.getOrDefault(endChar, 0)+1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int endIdx = 0; endIdx< s.length(); endIdx++){
+            char endChar = s.charAt(endIdx);
+            map.put(endChar, map.getOrDefault(endChar,0)+1);
+
+            int windowLength = endIdx-startIdx+1;
             
-            currMax = Math.max(currMax, counter.get(endChar));
-            
-            while(windowEnd-windowStart+1 -currMax>k){
-                char startChar = s.charAt(windowStart);
-                counter.put(startChar, counter.get(startChar)-1);
-                windowStart ++;
+            maxRepeatedCount = Math.max(maxRepeatedCount, map.get(endChar));
+            while(windowLength - maxRepeatedCount>k){
+                char startChar = s.charAt(startIdx);
+                map.put(startChar, map.get(startChar)-1);
+                startIdx++;
+                windowLength = endIdx-startIdx+1;
             }
-            returnVal = Math.max(returnVal, windowEnd-windowStart+1);
+            
+            longestSubstring = Math.max(windowLength, longestSubstring);
         }
-        return returnVal;
+        return longestSubstring;
     }
 }
