b030a670314c34fa0673b8eb9d56f5d3e7f2a231
templeside <47994862+templeside@users.noreply.github.com>
2022-07-31 17:48:18 -0500

    Time: 24 ms (46.18%), Space: 68.4 MB (80.21%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,34 +1,35 @@
-public class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
         
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
+        ArrayDeque<int[]> bfsIterator = new ArrayDeque<>();
+        
+        for(int r=0; r< m; r++){
+            for(int c=0; c< n; c++){
+                if(mat[r][c] ==0){
+                    bfsIterator.add(new int[]{r,c});
                 }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
+                else{
+                    mat[r][c] = Integer.MAX_VALUE;
                 }
             }
         }
+        int[] rDir = new int[]{0,0,1,-1};
+        int[] cDir = new int[]{1,-1,0,0};
         
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-                if (r < 0 || r >= m || c < 0 || c >= n || 
-                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+        while(bfsIterator.size()>0){
+            int[] coordinate = bfsIterator.poll();
+            for(int i=0; i< 4; i++){
+                int newR = coordinate[0] +rDir[i];
+                int newC = coordinate[1] +cDir[i];
+                
+                if(newR>=0 && newR< m && newC>= 0 && newC<n && mat[coordinate[0]][coordinate[1]]+1< mat[newR][newC]){
+                    bfsIterator.add(new int[]{newR,newC});
+                    mat[newR][newC] = mat[coordinate[0]][coordinate[1]]+1;
+                }
             }
         }
-        
-        return matrix;
+        return mat;
     }
 }
