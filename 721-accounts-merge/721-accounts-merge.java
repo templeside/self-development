9f6d4c6c4ad46f1871a257e68263dafd70818aca
templeside <47994862+templeside@users.noreply.github.com>
2022-08-04 20:32:25 -0500

    Time: 43 ms (80.24%), Space: 67.5 MB (24.80%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.java
+++ 721-accounts-merge/721-accounts-merge.java
@@ -12,18 +12,21 @@ class Solution {
     3. move on to next iteration.
     4. after that, make the returnValue.
     이 방법도 좋긴 좋은데, 만약 두개 이상이 연결되어 있다면은 불가능한 방법이다.
-    
+    아니다 가능한 방법이고, 첫번째 solution이 내가 말한 것을 하는 것.
     
     time complexity: O(N). N is the length of accounts.
     **/
     HashSet<String> visited = new HashSet<>();
     Map<String, List<String>> adjacent = new HashMap<String, List<String>>();
+    
     private void DFS(List<String> mergedAccount, String email){
         visited.add(email);
         
+        // Add the email vector that contains the current component's emails
         mergedAccount.add(email);
         if(!adjacent.containsKey(email))
             return;
+        
         for(String neighbor: adjacent.get(email)){
             if(!visited.contains(neighbor))
                 DFS(mergedAccount, neighbor);
@@ -31,8 +34,6 @@ class Solution {
     }
     
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int accountListSize = accounts.size();
-        
         for(List<String> account: accounts){
             int accountSize = account.size();
             
@@ -40,6 +41,8 @@ class Solution {
             // adding edge between first email to all other emails in the account.
             // kinda vertex.
             // accountFirstEamil가지고 connecting을 하려고 하네. 그래야 연결이 편하니까.
+            // 그렇다면 accountFirstEmaiil이 아닌 다른 email로 connected 되면 어떻게 하는 거지???
+            // adjacent라고 하는 global variable로 adjacent를 accountFirstEmail로 다 모아넣기를 한다.
             String accountFirstEmail = account.get(1);
             for(int j=2; j< accountSize; j++){
                 String accountEmail = account.get(j);
@@ -60,6 +63,8 @@ class Solution {
             String accountName= account.get(0);
             String accountFirstEmail = account.get(1);
             
+            // If email is visited, then it's a part of different component
+            // Hence perform DFS only if email is not visited yet
             if(! visited.contains(accountFirstEmail)){
                 List<String> mergedAccount = new ArrayList<>();
                 // Adding account name at the 0th index
