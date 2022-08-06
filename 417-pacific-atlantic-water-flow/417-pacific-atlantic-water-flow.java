dada8db384f654e4812c93dff3726325c6c6a0ec
templeside <47994862+templeside@users.noreply.github.com>
2022-08-05 23:16:55 -0500

    Time: 15 ms (39.30%), Space: 54.4 MB (77.81%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,77 +1,66 @@
 class Solution {
     /**
-    starting from the ocean borderlines
-    trying to spread out the grids that oceans can be visited.
+    find all of the pacific Ocen, atlantic Ocean locations.
+    find duplicated places that are available.
     
-    height[currIdx] <= height[childIdx]
-    because we are going to do in reverse order.
+    1. initiate pacific ocean matrix, atlantic ocean matrix.
+    2. from the pacific ocean matrix, find out as much as it could.
+    3. from the atlantic ocean matrix, find out as much as it could.
+    4. compare atlantic ocean and pacific ocean. 
+    5. returnthe pacificAtlantic coordinates.
     **/
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int maxR= heights.length;
-        int maxC = heights[0].length;
-        int [][] pacific = new int[maxR][maxC]; // 0 - not visited, 1 - canvisit, -1 - cannotvisit.
-        int [][] atlantic = new int[maxR][maxC];
-        List<List<Integer>> returnVal =new ArrayList<>();
+        int rMax = heights.length, cMax = heights[0].length;
+        List<List<Integer>> returnVal = new ArrayList<>();
         
-        ArrayDeque<int[]> pacificQue = new ArrayDeque<>();
-        ArrayDeque<int[]> atlanticQue = new ArrayDeque<>();
-        
-        for(int r=0; r<maxR;r++){
-            pacific[r][0] = 1;
-            atlantic[r][maxC-1] = 1;
-            
-            pacificQue.add(new int[]{r,0});
-            atlanticQue.add(new int[]{r,maxC-1});
+
+        Queue<int[]> pacificIterator = new LinkedList<>();
+        Queue<int[]> atlanticIterator = new LinkedList<>();
+        for(int r= 0; r<rMax; r++){
+            pacificIterator.add(new int[]{r,0});
+            atlanticIterator.add(new int[]{r, cMax-1});
         }
-        for(int c=0; c<maxC;c++){
-            pacific[0][c] = 1;
-            atlantic[maxR-1][c] = 1;
-            
-            pacificQue.add(new int[]{0,c});
-            atlanticQue.add(new int[]{maxR-1,c});
+        for(int c=0; c< cMax; c++){
+            pacificIterator.add(new int[]{0,c});
+            atlanticIterator.add(new int[]{rMax-1, c});
         }
+
+        int[][] pacific = bfs(heights, pacificIterator);
+        int[][] atlantic = bfs(heights, atlanticIterator);
         
-        bfs(pacificQue, pacific,heights);
-        bfs(atlanticQue, atlantic, heights);
-        
-        for(int r=0; r< maxR; r++){
-            for(int c=0; c <maxC;c++){
-                if(pacific[r][c] ==1 && atlantic[r][c]==1){
+        for(int r=0; r< rMax; r++){
+            for(int c=0; c< cMax; c++){
+                if(pacific[r][c]==1 && atlantic[r][c]==1)
                     returnVal.add(List.of(r,c));
-                }
             }
         }
         return returnVal;
     }
     
-    public void bfs(ArrayDeque<int[]> que, int[][] ocean, int[][] heights){
-        while(que.size()>0){
-            int n= que.size();
-            for(int i=0; i<n;i++){
-                int[] coordinate = que.poll();
-                List<int[]> adjacent = getAdjacent(coordinate, ocean, heights);
-                for(int[] newCoordinate : adjacent){
-                    que.add(newCoordinate);
-                    ocean[newCoordinate[0]][newCoordinate[1]] = 1;
+    // 0 - not visited, -1 - notValid, 1- valid.
+    public int[][] bfs(int[][] heights, Queue<int[]> iterator){
+        int[]rDir = new int[]{0,0,1,-1};
+        int[] cDir = new int[]{1,-1, 0,0};
+        int rMax = heights.length;
+        int cMax = heights[0].length;
+        int[][] map = new int[rMax][cMax];
+        
+        while(iterator.size()>0){
+            int[] coordinate =iterator.poll();
+            int r = coordinate[0];
+            int c=  coordinate[1];
+            map[r][c] = 1;
+            
+            //get the adjacencies.
+            for(int i=0; i<4; i++){
+                int newR = r+ rDir[i];
+                int newC = c+ cDir[i];
+                if(newR>=0 && newC>=0 && newR< rMax && newC< cMax && map[newR][newC] ==0 && heights[r][c] <= heights[newR][newC]){
+                    map[newR][newC] = 1;
+                    iterator.add(new int[]{newR,newC});
                 }
             }
         }
-    }
-    public List<int[]> getAdjacent(int[] coordinate, int[][] ocean, int[][] heights){
-        int[] xdir = new int[]{0,0,1,-1};
-        int[] ydir=  new int[]{1,-1,0,0};
-        List<int[]> adjacents = new ArrayList();
-        int maxR = heights.length;
-        int maxC = heights[0].length;
-        
-        for(int i=0; i<4; i++){
-            int newR = coordinate[0]+ xdir[i];
-            int newC = coordinate[1]+ ydir[i];
-            
-            if(newR>=0 && newR< maxR && newC>=0 && newC< maxC && ocean[newR][newC] ==0 && heights[coordinate[0]][coordinate[1]] <= heights[newR][newC]){
-                adjacents.add(new int[]{newR, newC});
-            }
-        }
-        return adjacents;
+        return map;
     }
 }
