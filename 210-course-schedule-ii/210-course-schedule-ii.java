fc6ee460df7b4d822de6fa9a5930bd81b2db84bc
templeside <47994862+templeside@users.noreply.github.com>
2022-08-06 12:46:13 -0500

    Time: 17 ms (24.77%), Space: 51.2 MB (15.71%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,44 +1,41 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer> sortedOrder = new ArrayList<>();
-        
-        HashMap<Integer, Integer> incomingCounter = new HashMap<>();
-        HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+        HashMap<Integer, List<Integer>> adjList = new HashMap<>();  // courses looking for child courses
+        HashMap<Integer, Integer> numPrerequisite = new HashMap<>();
         for(int i=0; i< numCourses; i++){
-            incomingCounter.put(i, 0);
-            adjList.put(i, new ArrayList<Integer>());
+            adjList.put(i, new ArrayList<>());
+            numPrerequisite.put(i, 0);
         }
-        
         for(int[] prerequisite: prerequisites){
-            int prereq = prerequisite[0];
-            int totake = prerequisite[1];
+            int prereq=prerequisite[1];
+            int currCourse = prerequisite[0];
+            adjList.get(prereq).add(currCourse);    // currCourse is child course. prereq -> currCourse
+            numPrerequisite.put(currCourse, numPrerequisite.get(currCourse)+1);
+        }
+        
+        List<Integer> courseOrder = new LinkedList<>();
+        Queue<Integer> bfsIterator = new LinkedList<>();
+        for(Map.Entry<Integer,Integer> entry: numPrerequisite.entrySet()){
+            if(entry.getValue() == 0)
+                bfsIterator.add(entry.getKey());
+        }
+        
+        while(bfsIterator.size()>0){
+            int currCourse = bfsIterator.poll();
+            courseOrder.add(currCourse);
             
-            incomingCounter.put(totake, incomingCounter.get(totake)+1);
-            adjList.get(prereq).add(totake);
-        }
-        
-        Queue<Integer> sources= new LinkedList<>();
-        for(Map.Entry<Integer, Integer> entry: incomingCounter.entrySet()){
-            if(entry.getValue()==0)
-                sources.add(entry.getKey());
-        }
-        
-        while(!sources.isEmpty()){
-            int vertex = sources.poll();
-            sortedOrder.add(vertex);
-            List<Integer> children = adjList.get(vertex);
-            for(int child: children){
-                incomingCounter.put(child, incomingCounter.get(child)-1);
-                if(incomingCounter.get(child)==0)
-                    sources.add(child);
+            for(int childCourse: adjList.get(currCourse)){
+                numPrerequisite.put(childCourse, numPrerequisite.get(childCourse)-1);
+                if(numPrerequisite.get(childCourse)==0)
+                    bfsIterator.add(childCourse);
             }
         }
-        System.out.println(sortedOrder);
-        if(sortedOrder.size()!= numCourses)
-            return new int[]{};
-        int[] returnval = new int[sortedOrder.size()];
-        for(int i=0; i<sortedOrder.size();i++)
-            returnval[numCourses-1-i] = sortedOrder.get(i);
-        return returnval;
+        if(courseOrder.size() != numCourses)return new int[]{};
+        
+        int[] returnOrder = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            returnOrder[i] = courseOrder.get(i);
+        }
+        return returnOrder;
     }
 }
