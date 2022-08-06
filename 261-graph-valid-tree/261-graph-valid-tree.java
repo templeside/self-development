2c6e06dfb8ca317a6866c8030106cada1871a289
templeside <47994862+templeside@users.noreply.github.com>
2022-08-06 12:08:35 -0500

    Time: 6 ms (38.87%), Space: 42.7 MB (90.58%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,53 +1,63 @@
 class Solution {
-    /**
-    그래프에서 tree로 될려면 어떠한 특성이 있어야 하는가를 물어보고싶은 것이다.
-    
-    picking from the vertex, find the visited node.
-    if it is not a parent node, then the node itself is visited, then it is false. 
-    graph -> tree.
-    graph - tree is almost same. 
-    
-    picking on vertex, and that point is going to be a tree of the structure.
-    
-    set the parent hashmap.
-    if adjacencies except the hashmap is visited, then the graph is not valid, and returning that is false.
-    
-    1. first of all, those are edges and consist of int array, we will transform this structure to HashMap structure for easier usage.
-    
-    **/
+/**
+first intuition, I can have the meaning of the typical things. 그게 무엇이냐하면 in every nodes, the graph should be a valid tree - that means there should not be any cycle. soit should have only one parent, and other is children.
+
+so, it only should iterate except parent node. 
+
+having the parent node, just having the focusness, I would just have the DFS iteration for checking is there a duplication and making a cycle.
+
+so, sudo will going to be:
+0. implement an adjacency - because it is graph.
+1. draw the graph
+2. from the node 0, search for the DFS(currNode, parent, hashSet<Integer> visited)
+    a. add to visited
+    b. iterate the adjacencies
+        if parent - skip
+        if not parent - but visited - return false;
+3. return visited.size() == n
+**/
     public boolean validTree(int n, int[][] edges) {
-        HashMap<Integer, List<Integer>> adjacencyList= new HashMap<>();
+        HashMap<Integer, List<Integer>> adjacencies = new HashMap<>();
         HashSet<Integer> visited = new HashSet<>();
         
-
-        for(int i=0; i< n; i++)
-            adjacencyList.put(i, new ArrayList<>());
-        for(int[] edge: edges){
-            adjacencyList.get(edge[0]).add(edge[1]);
-            adjacencyList.get(edge[1]).add(edge[0]);
+        for(int i=0; i< n; i++){
+            adjacencies.put(i, new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            adjacencies.get(edge[0]).add(edge[1]);
+            adjacencies.get(edge[1]).add(edge[0]);
         }
         
-        return dfs(0, -1, adjacencyList, visited) && visited.size() ==n;
+        boolean result = dfs(0, -1, visited, adjacencies);
+        System.out.println(visited.size());
+        if(result== false)return false;
+        
+        return visited.size() == n;
     }
+    /**
+    0-[1,2,3]
+    1-[0,4]
+    2-[0]
+    3-[0]
+    4-[1]
+    visited -[0,1]
     
-    public boolean dfs(int currNode, int parent, HashMap<Integer,List<Integer>> adjacencyList, HashSet<Integer> visited){
-        //base case?? no필요.
-        if(visited.contains(currNode))return false;
-        
+    dfs(0,-1, visited, adjacencies)
+        dfs(1,0, visited, adjacencies)
+        dfs(2,0, visited, adjacencies)
+        dfs(3,0, visited, adjacencies)
+    **/
+    public boolean dfs(int currNode, int parent, HashSet<Integer> visited, HashMap<Integer, List<Integer>> adjacencies){
+        // if(visited.contains(currNode))return false;
         visited.add(currNode);
-        
-        for(int adjacentNode: adjacencyList.get(currNode)){
-            if(adjacentNode != parent){
-
-                boolean result = dfs(adjacentNode, currNode, adjacencyList, visited);
-                
-                if(!result)
-                    return false;
-            }
-        }
+        for(int neighbor: adjacencies.get(currNode)){
+            if(neighbor == parent)continue;
+            else if(visited.contains(neighbor))return false;
             
-        // return case??   
-        
+            visited.add(neighbor);
+            boolean result =  dfs(neighbor, currNode, visited, adjacencies);
+            if(!result)return false;
+        }
         return true;
     }
 }
