a7c7fb8bf087ec279e156facf1df333c892db842
templeside <47994862+templeside@users.noreply.github.com>
2022-08-19 17:29:16 -0500

    Time: 0 ms (100.00%), Space: 41.9 MB (27.59%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,28 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 0)
-            return 0;
-
-        if (nums.length == 1)
-            return nums[0];
-
-        int max1 = rob_simple(nums, 0, nums.length - 2);
-        int max2 = rob_simple(nums, 1, nums.length - 1);
-
+        if(nums.length == 0)return 0;
+        else if(nums.length ==1)return nums[0];
+        
+        int max1 = rob_simple(nums, 0, nums.length -2);
+        int max2 = rob_simple(nums, 1, nums.length -1);
+        
         return Math.max(max1, max2);
     }
-
-    public int rob_simple(int[] nums, int start, int end) {
-        int t1 = 0;
-        int t2 = 0;
-
-        for (int i = start; i <= end; i++) {
-            int temp = t1;
-            int current = nums[i];
-            t1 = Math.max(current + t2, t1);
-            t2 = temp;
+    
+    public int rob_simple(int[] nums, int start, int end){
+        if(end-start ==0)return nums[start];
+        if(end-start == 1)return Math.max(nums[start], nums[end]);
+        
+        int[] memo = new int[nums.length];
+        memo[start] = nums[start];
+        memo[start+1] = Math.max(nums[start], nums[start+1]);
+        
+        for(int i= start+2; i<=end; i++){
+            memo[i] = Math.max(memo[i-1], memo[i-2]+nums[i]);
         }
-
-        return t1;
+        
+        return memo[end];
     }
 }
