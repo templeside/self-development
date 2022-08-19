267bb3c9aa6dd742cf7206316caf8d50931a8fa9
templeside <47994862+templeside@users.noreply.github.com>
2022-08-19 16:40:56 -0500

    Time: 0 ms (100.00%), Space: 39.8 MB (92.01%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,16 +1,28 @@
 class Solution {
+    /**
+    start from the robbing. 
+    
+    nums = [1,2,3,1]
+    output = 4 
+    because i don't want to get the two adjacent files.
+    how??? I would get the ways to get the most maximum values I could get.
+    how??? 
+    initializing the best max values 
+    values[i] = Math.max(values[i-1], values[i-2]+ nums[i])
+    values = []
+    **/
     public int rob(int[] nums) {
-        if(nums.length ==0)return 0;
-        else if(nums.length ==1)return nums[0];
-        else if(nums.length ==2)return Math.max(nums[0],nums[1]);
+        if(nums.length == 0) return 0;
+        else if(nums.length == 1)return nums[0];
+        else if(nums.length == 2)return Math.max(nums[0], nums[1]);
         
-        int[] memo = new int[nums.length];
-        memo[0] = Math.max(nums[0],0);
-        memo[1] = Math.max(memo[0],nums[1]);
+        int[] amount = new int[nums.length];
+        amount[0] = Math.max(nums[0],0);
+        amount[1] = Math.max(amount[0], nums[1]);
         
-        for(int i=2; i<memo.length;i++){
-            memo[i] = Math.max(memo[i-1], memo[i-2]+nums[i]);
+        for(int i=2; i< nums.length; i++){
+            amount[i] = Math.max(amount[i-1], amount[i-2]+nums[i]);
         }
-        return memo[memo.length-1];
+        return amount[nums.length-1];
     }
 }
