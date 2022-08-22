18ea27377760cd4ecd3d962fa3348eb0d2ec9031
templeside <47994862+templeside@users.noreply.github.com>
2022-08-22 18:44:25 -0500

    Time: 16 ms (70.65%), Space: 51.5 MB (28.67%) - LeetHub

---
+++ 494-target-sum/494-target-sum.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        int total =0;
+        for(int num: nums)
+            total+=num;
+        
+        int[][] dp = new int[nums.length][2 * total + 1];
+        dp[0][nums[0] + total] = 1;
+        dp[0][-nums[0] + total] += 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            for (int sum = -total; sum <= total; sum++) {
+                if (dp[i - 1][sum + total] > 0) {
+                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];
+                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                }
+            }
+        }
+        
+        return Math.abs(S) > total ? 0 : dp[nums.length - 1][S + total];
+    }
+}
