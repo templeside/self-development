1c0305de86aa552ff4f3f7ace426f02c78a8886c
templeside <47994862+templeside@users.noreply.github.com>
2022-08-30 12:45:55 -0500

    Time: 294 ms (29.52%), Space: 71.9 MB (23.99%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -2,21 +2,20 @@ class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
-        int[] returnIdx = new int[2];
+        int[] returnIdx=  new int[2];
         
-        for(int i=0; i<n;i++){
+        for(int i=0; i< n; i++)
             dp[i][i] = true;
-        }
         
-        for(int startIdx=n-1; startIdx>=0; startIdx--){
-            for(int endIdx=startIdx+1; endIdx<n; endIdx++){
+        for(int startIdx= n-1; startIdx>=0 ; startIdx--){
+            for(int endIdx= startIdx+1; endIdx<n; endIdx++){
                 if(s.charAt(startIdx) == s.charAt(endIdx)){
                     if(endIdx-startIdx ==1 || dp[startIdx+1][endIdx-1]){
-                        dp[startIdx][endIdx] = true;
-                        if(returnIdx[1]-returnIdx[0]< endIdx-startIdx){
-                            returnIdx[0] = startIdx;
-                            returnIdx[1] = endIdx;
-                        }
+                    dp[startIdx][endIdx] = true;
+                    if(returnIdx[1]-returnIdx[0]< endIdx-startIdx){
+                        returnIdx[0] = startIdx;
+                        returnIdx[1] = endIdx;
+                    }
                     }
                 }
             }
