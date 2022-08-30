7612193323fde30d4d041b9d6131f58268677323
templeside <47994862+templeside@users.noreply.github.com>
2022-08-30 14:36:07 -0500

    Time: 349 ms (25.84%), Space: 69.6 MB (25.80%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,25 +1,28 @@
 class Solution {
+    /**
+    bottom up things - 
+    */
     public String longestPalindrome(String s) {
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
-        int[] returnIdx=  new int[2];
+        int[] returnVal = new int[2];
         
-        for(int i=0; i< n; i++)
-            dp[i][i] = true;
+        for(int i=0; i<n; i++)
+            dp[i][i]= true;
         
-        for(int startIdx= n-1; startIdx>=0 ; startIdx--){
-            for(int endIdx= startIdx+1; endIdx<n; endIdx++){
+        for(int startIdx=n-1; startIdx>=0; startIdx--){
+            for(int endIdx = startIdx+1; endIdx<n; endIdx++){
                 if(s.charAt(startIdx) == s.charAt(endIdx)){
-                    if(endIdx-startIdx ==1 || dp[startIdx+1][endIdx-1]){
-                    dp[startIdx][endIdx] = true;
-                    if(returnIdx[1]-returnIdx[0]< endIdx-startIdx){
-                        returnIdx[0] = startIdx;
-                        returnIdx[1] = endIdx;
-                    }
+                    if(endIdx-startIdx==1 || dp[startIdx+1][endIdx-1]){
+                        dp[startIdx][endIdx] = true;
+                        if(returnVal[1]-returnVal[0]< endIdx-startIdx){
+                            returnVal[0] = startIdx;
+                            returnVal[1] = endIdx;
+                        }
                     }
                 }
             }
         }
-        return s.substring(returnIdx[0], returnIdx[1]+1);
+        return s.substring(returnVal[0], returnVal[1]+1);
     }
 }
