2b2f2b9e62c2ebbaf2cafc44bf7f8cfc7068b8e0
templeside <47994862+templeside@users.noreply.github.com>
2022-08-31 18:04:28 -0500

    Time: 49 ms (9.26%), Space: 64.9 MB (5.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,34 +1,30 @@
 class Solution {
     /**
-    abcde
-    ^
-    ace
-    ^
-    s1 abcde, s2 ace -> s1(a) == s2(a). matched.
-    s1 bcde,  s2 ce ->  s1(b) != s2(c). not matched.
-    s1 bcde,  s2 e ->   s1(b) != s2(e). not matched || s1 cde, s2 ce -> s1(c) != s2(c). not matched.
-        
-    moving to the index if the curr index is equal. otherwise, looking for other ways to do.
-    changing variables - s1Idx, s2Idx
-    dp[s1Idx][s2Idx] = maxval? nope. count should be.
-    
-    dp[i][j] = dp[i-1][j-1];        //when matching
-    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);    //when not matching.
+    subsequence including any messages.
+    option 1 - matching
+    option 2 - not matching.
+    because it is subsequence - always matching is long.
     **/
-    public int longestCommonSubsequence(String s1, String s2) {
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
+    public int longestCommonSubsequence(String text1, String text2) {
+        Integer[][] dp = new Integer[text1.length()][text2.length()];
+        return recursion(text1,text2, 0,0, dp);
+    }
+    
+    public int recursion(String text1, String text2, int idx1, int idx2, Integer[][] dp){
+        if(idx1>= text1.length() || idx2>= text2.length()) 
+            return 0;
         
-        for(int s1Idx=1; s1Idx<= s1.length(); s1Idx++){
-            for(int s2Idx=1; s2Idx<= s2.length(); s2Idx++ ){
-                if(s1.charAt(s1Idx-1)== s2.charAt(s2Idx-1))
-                    dp[s1Idx][s2Idx] = 1+dp[s1Idx-1][s2Idx-1];
-                else{
-                    int c1 = dp[s1Idx-1][s2Idx];
-                    int c2 = dp[s1Idx][s2Idx-1];
-                    dp[s1Idx][s2Idx] = Math.max(c1, c2);
-                }
-            }
+        if(dp[idx1][idx2] !=null)
+            return dp[idx1][idx2] ;
+        
+        
+        if(text1.charAt(idx1) == text2.charAt(idx2))
+            dp[idx1][idx2] = 1+recursion(text1, text2, idx1+1, idx2+1, dp);
+        else{
+            int s1 = recursion(text1, text2, idx1+1, idx2, dp);
+            int s2 = recursion(text1, text2, idx1, idx2+1, dp);
+            dp[idx1][idx2] =  Math.max(s1, s2);
         }
-        return dp[s1.length()][s2.length()];
+        return dp[idx1][idx2];
     }
 }
