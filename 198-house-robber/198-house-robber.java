e0a74970e2a5d6076efd8076aec6d6254c8b8522
templeside <47994862+templeside@users.noreply.github.com>
2022-09-09 13:03:20 -0500

    Time: 0 ms (100.00%), Space: 42 MB (14.49%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,28 +1,23 @@
 class Solution {
     /**
-    start from the robbing. 
+    robber should not have two adjacent houses.
+    there are n houses, and need to get the maximum profit.
+    cuz we need the maximum profit, we look forward to get a max profit as much as I can do from 0 to n house.
+    so we need a step by step max profit, so we need dynamic programming to use.
     
-    nums = [1,2,3,1]
-    output = 4 
-    because i don't want to get the two adjacent files.
-    how??? I would get the ways to get the most maximum values I could get.
-    how??? 
-    initializing the best max values 
-    values[i] = Math.max(values[i-1], values[i-2]+ nums[i])
-    values = []
+    dp[n] = max profit i can get.
+    dp[n] = Math.max(dp[n-1], dp[n-2]+nums[n])
     **/
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        else if(nums.length == 1)return nums[0];
-        else if(nums.length == 2)return Math.max(nums[0], nums[1]);
+        int n = nums.length;
+        int[]dp = new int[n];
+
+        if(n ==1)return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<n;i++)
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
         
-        int[] amount = new int[nums.length];
-        amount[0] = Math.max(nums[0],0);
-        amount[1] = Math.max(amount[0], nums[1]);
-        
-        for(int i=2; i< nums.length; i++){
-            amount[i] = Math.max(amount[i-1], amount[i-2]+nums[i]);
-        }
-        return amount[nums.length-1];
+        return dp[n-1];
     }
 }
