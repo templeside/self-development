b24760b8c68571c47c7198e90e3e5f9a56341cd5
templeside <47994862+templeside@users.noreply.github.com>
2022-09-08 21:58:13 -0500

    Time: 0 ms (100.00%), Space: 41.4 MB (11.59%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,25 +1,24 @@
 class Solution {
     /**
-    like a fibonacci thing, I would count I would have two cases
-    option 1: step 1
-    option 2: step 2
+    can be divided upto the things......
+    dp[n] = ways to the n stairs
+    dp[n] = dp[n-1]+ dp[n-2].
     
-    dp[i] = dp[i-1]+ dp[i-2].
-    of course 
-    dp[0] = 1
-    dp[1] = 1
+    option: 1,2
+    climb 1
+    climb 2
     
-    dp[3] = 
+    recursion(currStair, endStair, dp)
     
     **/
     public int climbStairs(int n) {
-        if(n ==1) return 1;
-        int[] dp = new int[n+1];
+        int[]dp = new int[n+1];
+        
         dp[0] = 1;
         dp[1] = 1;
         
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1]+ dp[i-2];
+            dp[i] = dp[i-1]+dp[i-2];
         }
         return dp[n];
     }
