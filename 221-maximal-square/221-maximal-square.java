cec2b09c06fd682f29ecfe400726ebcac090c382
templeside <47994862+templeside@users.noreply.github.com>
2022-09-10 16:44:46 -0500

    Time: 18 ms (17.74%), Space: 66.8 MB (20.03%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -42,32 +42,3 @@ class Solution {
     }
     
 }
-
-//     private static int maximumSizeSquareSubmatrixWithAllOnes(int[][] matrix) {
-// 4	        int maxSize = 0;
-// 5	        int r = matrix.length;
-// 6	        int c = matrix[0].length;
-// 7	         
-// 8	        int[][] table = new int[r][c];
-// 9	
-// 10	        for (int i = 0; i < r; i++) {
-// 11	            for (int j = 0; j < c; j++) {
-// 12	                 
-// 13	                if (i == 0 || j == 0) {
-// 14	                    table[i][j] = matrix[i][j];
-// 15	                    maxSize = table[i][j] > maxSize ? table[i][j] : maxSize;
-// 16	                }
-// 17	                 
-// 18	                else if (matrix[i][j] == 0) {
-// 19	                    table[i][j] = 0;
-// 20	                }
-// 21	                 
-// 22	                else {
-// 23	                    table[i][j] = min(table[i - 1][j - 1], table[i - 1][j], table[i][j - 1]) + 1;
-// 24	                    maxSize = table[i][j] > maxSize ? table[i][j] : maxSize;
-// 25	                }
-// 26	            }
-// 27	        }
-// 28	
-// 29	        return maxSize;
-// 30	    }
