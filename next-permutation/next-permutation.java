290ba5bc32e3be93240cba9abe1ed654340e89f5
templeside <47994862+templeside@users.noreply.github.com>
2022-09-18 11:59:29 -0500

    Time: 1 ms, Memory: 43.8 MB - LeetHub

--- next-permutation/next-permutation.java
+++ next-permutation/next-permutation.java
@@ -1,31 +1,40 @@
 class Solution {
+    /**
+    1,2,3
+    
+    ?? is it backtracking???
+    
+    because the permutation stars reverse when finished all the right numberse are reversed. 
+    
+    
+    **/
     public void nextPermutation(int[] nums) {
-        int i= nums.length -2;
-        while(i>=0 && nums[i+1] <= nums[i]){
-            i--;
+        int pivot = nums.length-2;
+        while(pivot>=0 && nums[pivot] >= nums[pivot+1]){
+            pivot--;
         }
-        if(i>=0){
-            int j = nums.length -1;
-            while(nums[j]<= nums[i])
-                j--;
-            
-            swap(nums, i,j);
+        if(pivot>=0){
+            int min = nums.length-1;
+            while(nums[pivot] >= nums[min])
+                min --;
+            swap(nums, min, pivot);
         }
-        reverse(nums,i+1);
+        reverse(nums, pivot+1);
     }
     
-    private void reverse(int[] nums, int start){
-        int i=start, j= nums.length-1;
-        while(i<j){
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
-    }
-    
-    private void swap(int[] nums, int i, int j){
+    public void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+    
+    public void reverse(int[] nums, int i){
+        int j=  nums.length-1;
+        
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
     }
 }
