17c78a9537dfb5d8b59a1438a75bb5da3be214c1
templeside <47994862+templeside@users.noreply.github.com>
2022-09-18 11:01:48 -0500

    Time: 8 ms, Memory: 81 MB - LeetHub

--- container-with-most-water/container-with-most-water.java
+++ container-with-most-water/container-with-most-water.java
@@ -1,20 +1,34 @@
 class Solution {
+    /*
+    first, need to update the max area of water.
+    I need to look for the weater with container.
+    
+    water = width * height.
+    width = windowEnd - windowStart
+    height = Math.min(height[windowEnd, windowStart])
+    
+    update the area
+    shifting the smaller height value index.
+    */
     public int maxArea(int[] height) {
-        int left =0, right = height.length-1;
-        int difference;
-        int output =0;
+        int maxArea = 0;
+        int n = height.length;
         
-        while(left<right){
-            // int col = Math.min(height[left], height[right]);
-            // System.out.println(col);
-            output = Math.max(Math.min(height[left], height[right])*(right-left), output);
+        int windowStart = 0;
+        int windowEnd = n-1;
+        
+        while(windowStart<windowEnd){
+            int currWidth = windowEnd-windowStart;
+            int currHeight = Math.min(height[windowEnd], height[windowStart]);
+            int currArea = currWidth * currHeight;
             
-            if(height[left]>height[right]){
-                right--;
-            }else{
-                left++;
-            }
+            maxArea = Math.max(currArea, maxArea);
+            
+            if(height[windowStart]< height[windowEnd])
+                windowStart++;
+            else
+                windowEnd--;
         }
-        return output;
+        return maxArea;
     }
 }
