6698009e38369c7e7e628825bff2a067058702ed
templeside <47994862+templeside@users.noreply.github.com>
2022-09-20 11:11:32 -0500

    Time: 8 ms (32.98%), Space: 70 MB (5.54%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,28 +1,27 @@
 class Solution {
     /*
-    jump 할 수 있는지 없는지 체크를 해야 한다.
-    brute force: 
-        iterate using for loop.
-        having boolean[] dp - checking how much can you do.
-        i've done it before :0
-        
-    nums  = [2,3,1,1,4]
-    i        ^
-    j          ^
-    dp[n] =  t t
+    brute force is to reach out to the max jump I could get.
+    how?? 
+    from beginning, using two pointer to check where is the way to go further.
+    
+    trying to remove the overlap.
+    
+    trying to get the current index step by step.
+    bottom up approach
+    nums= [2,3,1,1,4]
+    idx    ^
+    maxIdx=    ^
+    dp=   [f,f,f,f,f]
+    update which one is larger, == n-1
     */
     public boolean canJump(int[] nums) {
-        int n =nums.length;
-        boolean[] dp= new boolean[n];
-        dp[0] = true;
+        int n = nums.length;
+        int[] dp = new int[n];
+        int maxIdx = 0;
         
-        for(int i=0; i< n; i++){
-            for(int j=i+1; j<n && j<= i+nums[i]; j++){
-                if(dp[i]){
-                    dp[j]= true;
-                }
-            }
+        for(int i=0; i<n-1 && i<=maxIdx ; i++){
+            maxIdx = Math.max(maxIdx, i+ nums[i]);
         }
-        return dp[n-1];
+        return maxIdx >= n-1;
     }
 }
