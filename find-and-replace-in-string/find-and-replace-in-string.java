4c0dd719395a72aab26ce80fa08bf95a5cf27cc3
templeside <47994862+templeside@users.noreply.github.com>
2022-09-27 13:30:53 -0500

    Time: 1 ms, Memory: 41.8 MB - LeetHub

--- find-and-replace-in-string/find-and-replace-in-string.java
+++ find-and-replace-in-string/find-and-replace-in-string.java
@@ -1,41 +1,38 @@
 class Solution {
-    class Node{
-        int index;
-        String source;
-        String target;
-        public Node(int i, String s, String t){
-            index = i;
-            source = s;
-            target = t;
+    /**
+    1. Check if the substring sources[i] occurs at index indices[i] in the original string s.
+    2. If it does not occur, do nothing.
+    3. Otherwise if it does occur, replace that substring with targets[i].
+Input: s = "abcd", indices = [0, 2], sources = ["a", "cd"], targets = ["eee", "ffff"]
+Output: "eeebffff"
+Explanation:
+"a" occurs at index 0 in s, so we replace it with "eee".
+"cd" occurs at index 2 in s, so we replace it with "ffff".
+replace from left to right.
+    **/
+public String findReplaceString(String s, int[] indexes, String[] sources, String[] targets) {
+    Map<Integer, Integer> map = new HashMap<>();
+    StringBuilder sb = new StringBuilder();
+
+    for(int i = 0; i < indexes.length; i++)//map the indexes 
+        map.put(indexes[i], i);
+
+    for(int i = 0; i < s.length(); i++){
+        if(!map.containsKey(i)) {// i is not in indexes array
+            sb.append(s.charAt(i));//append current character 
+            continue;
         }
-    }
-    
-    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        PriorityQueue<Node> que = new PriorityQueue<Node>((a,b)-> a.index - b.index);
-        for(int i=0; i<indices.length;i++){
-            que.add(new Node( indices[i], sources[i], targets[i]));
+
+        int idx = map.get(i);//get the index for sources and targets array
+
+        if(!s.substring(i, i + sources[idx].length()).equals(sources[idx])){//substring in s doesn't match string in source
+            sb.append(s.charAt(i));//add current character 
+            continue;
         }
-        StringBuilder sb = new StringBuilder();
-        
-        Node node = que.poll();
-        int i=0;
-        while(node!=null && i<s.length()){
-            if(node.index == i){
-                if(node.source.equals(s.substring(i, i+node.source.length()))){
-                    i += node.source.length();
-                    sb.append(node.target);
-                    node = que.poll();
-                    continue;
-                }else{
-                    node = que.poll();
-                }
-            }
-            sb.append(s.charAt(i));
-            i++;
-        }    
-        if(i<s.length())
-            sb.append(s.substring(i,s.length()));
-        
-        return sb.toString();
+
+        i += sources[idx].length() - 1;//move i to the end of matched substring
+        sb.append(targets[idx]);//append the corresponding target string 
     }
+    return sb.toString();
+}
 }
