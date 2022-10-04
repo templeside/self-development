eeae63164b6c292224fa9f6b994173e3a2bbc676
templeside <47994862+templeside@users.noreply.github.com>
2022-10-04 12:19:19 -0500

    Time: 1 ms (95.63%), Space: 42.2 MB (56.67%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,31 +1,27 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        int sIdx = s.length()-1;
-        int tIdx = t.length()-1;
-        
-        while(sIdx>=0 || tIdx>=0){
-            sIdx = findValid(s, sIdx);
-            tIdx = findValid(t, tIdx);
-            
-            if(sIdx <0 && tIdx<0) return true;
-            else if( sIdx<0 || tIdx<0)return false;
-            else if( s.charAt(sIdx) != t.charAt(tIdx)) return false;
-            
-            sIdx--;
-            tIdx--;
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
         }
         return true;
-    }
-    
-    public int findValid(String s, int idx){
-        int countBackspace = 0;
-        while(idx>=0){
-            if(s.charAt(idx)=='#') countBackspace++;
-            else if( countBackspace>0) countBackspace--;
-            else
-                break;
-            idx--;
-        }
-        return idx;
     }
 }
