c075de1eaea41c4898a5fc74913d6f92703bb525
templeside <47994862+templeside@users.noreply.github.com>
2023-07-02 01:27:48 +0900

    Time: 0 ms (100.00%), Space: 56.6 MB (26.97%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,20 +4,41 @@ class Solution {
     with time complexity of O(n)
     
     instead, we can do in place transfer.
+    
+    rotate: ->
+    nums = [1,2,3,4,5,6,7], k = 3
+    nums = [7,1,2,3,4,5,6]
+    nums = [6,7,1,2,3,4,5]
+    nums = [5,6,7,1,2,3,4]
+    
+    takes too much time.
+    while we are looking at the steps, the last digit is incrementing in reverse order.
+    and reverse(reverse(nums)) = inorder.
+    how about reverse & reverse in two steps?
+    1. reverse all
+    2. reverse divide partially 
+
+    reversed = [7,6,5 | 4,3,2,1]
+if we reverse in two part: 
+    reversed = [5,6,7 | 1,2,3,4]
+
     */
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        List<Integer> maps = new ArrayList<>();
+        k = k%n;
+        reverse(0, n-1, nums);
+        reverse(0, k-1, nums);
+        reverse(k, n-1, nums);
         
-        for(int i=n-(k%n); i< n; i++){
-            maps.add(nums[i]);
+    }
+    //reverse (starting point, end point, nums)
+    public void reverse(int startIdx, int endIdx, int[] nums){
+        while(startIdx<endIdx){
+            int tempVal = nums[startIdx];
+            nums[startIdx] = nums[endIdx];
+            nums[endIdx]= tempVal;
+            startIdx++;
+            endIdx--;
         }
-        for(int i=0; i<n-(k%n); i++){
-            maps.add(nums[i]);
-        }
-        
-        for(int i=0; i< n; i++)
-            nums[i] = maps.get(i);
-        
     }
 }
