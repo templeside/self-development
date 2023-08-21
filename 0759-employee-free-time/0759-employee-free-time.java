a07646d1f485f71bff207c44549f5604983a7723
templeside <47994862+templeside@users.noreply.github.com>
2023-08-21 15:11:46 -0400

    Time: 9 ms (95.69%), Space: 45.6 MB (8.62%) - LeetHub

---
+++ 0759-employee-free-time/0759-employee-free-time.java
@@ -0,0 +1,39 @@
+/*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+왜냐면 it is already sorted by start time.
+먼저 interval를 만들어 놓기
+그 만들어진 schedule가지고 free time 찾기 적용하기
+*/
+
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> avails) {
+
+        List<Interval> result = new ArrayList<>();
+
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
+        avails.forEach(e -> pq.addAll(e));
+
+        Interval temp = pq.poll();
+        while(!pq.isEmpty()) {
+            if(temp.end < pq.peek().start) { // no intersect
+                result.add(new Interval(temp.end, pq.peek().start));
+                temp = pq.poll(); // becomes the next temp interval
+            }else { // intersect or sub merged
+                temp = temp.end < pq.peek().end ? pq.peek() : temp;
+                pq.poll();
+            }
+        }
+        return result;
+    }
+}
