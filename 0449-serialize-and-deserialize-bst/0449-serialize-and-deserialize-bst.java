646232cbed426e02473345f0fb56185ee41ccfac
templeside <47994862+templeside@users.noreply.github.com>
2023-09-19 10:24:06 -0500

    Time: 5 ms (85.39%), Space: 44.9 MB (33.56%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public static StringBuilder sb;
+    public String serialize(TreeNode root) {
+        sb = new StringBuilder();
+        serializeDFS(root);
+        return sb.toString();
+    }
+    public void serializeDFS(TreeNode root){
+        if(root == null){
+            sb.append("x");
+            sb.append(" ");
+        }
+        else{
+            sb.append(root.val);
+            sb.append(" ");
+            serializeDFS(root.left);
+            serializeDFS(root.right);
+        }
+    }
+    // Decodes your encoded data to tree.
+    public static Integer splittedIdx;
+    public TreeNode deserialize(String data) {
+        String[] splitted = data.split(" ");
+        splittedIdx= 0;
+        return deserializeDFS(splitted);
+    }
+    
+    public TreeNode deserializeDFS(String[] splitted){
+        // base case : when splittedIdx>= splitted.length, splitted[i] == x
+        if(splittedIdx>= splitted.length)return null;
+        
+        if(splitted[splittedIdx].equals("x")){
+            splittedIdx++;
+            return null;
+        }
+        TreeNode returnNode = new TreeNode(Integer.valueOf(splitted[splittedIdx]));
+        splittedIdx++;
+        returnNode.left = deserializeDFS(splitted);
+        returnNode.right = deserializeDFS(splitted);
+        return returnNode;
+    }}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
