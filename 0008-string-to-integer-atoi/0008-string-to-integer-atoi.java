48a01b1144ea6554bc2a4835653fdf9d7928c0a5
templeside <47994862+templeside@users.noreply.github.com>
2023-11-19 15:36:57 -0600

    Time: 1 ms (100.00%), Space: 41.1 MB (73.81%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,39 +1,47 @@
 class Solution {
-    public int myAtoi(String str) {
-        int index = 0, sign = 1, total = 0;
+    /*
+    "32 "-> 32
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+
+    */
+    
+    public int myAtoi(String s) {
+        int integer = 0;
+        int sIdx = 0;
+        int sign = 1;
         
-        // Check if empty string
-        if(str.length() == 0)
+        //Read in and ignore any leading whitespace.
+        while(sIdx< s.length() && s.charAt(sIdx)== ' ')
+            sIdx++;
+        
+        if(sIdx == s.length())
             return 0;
         
-        // remove white spaces from the string
-        while(index < str.length() && str.charAt(index) == ' ')
-            index++;
+        //check sign - or +
+        if(s.charAt(sIdx) == '+')
+            sIdx ++;
+        else if(s.charAt(sIdx) =='-'){
+            sIdx++;
+            sign = -1;
+        }
         
-        if (index == str.length()) return 0;
-
-        //2. Remove Spaces
-        while(str.charAt(index) == ' ' && index < str.length())
-            index ++;
-
-        //3. Handle signs
-        if(str.charAt(index) == '+' || str.charAt(index) == '-'){
-            sign = str.charAt(index) == '+' ? 1 : -1;
-            index ++;
+        while(sIdx< s.length()){
+            int digit = s.charAt(sIdx)-'0';
+            if(digit>9 || digit<0)break;
+                
+            
+            if(Integer.MAX_VALUE /10 < integer || (Integer.MAX_VALUE/10 == integer && Integer.MAX_VALUE %10 < digit))
+                return sign==1? Integer.MAX_VALUE: Integer.MIN_VALUE;
+            
+            integer = 10*integer + digit;
+            sIdx++;
         }
-
-        //4. Convert number and avoid overflow
-        while(index < str.length()){
-            int digit = str.charAt(index) - '0';
-            if(digit < 0 || digit > 9) break;
-
-            //check if total will be overflow after 10 times and add digit
-            if(Integer.MAX_VALUE/10 < total || Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE %10 < digit)
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-
-            total = 10 * total + digit;
-            index ++;
-        }
-        return total * sign;
+            
+        
+        return integer * sign;
     }
 }
