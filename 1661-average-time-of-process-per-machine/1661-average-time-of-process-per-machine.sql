cfe14c17ec44451641ea77fb93b81f484049089b
templeside <47994862+templeside@users.noreply.github.com>
2023-11-26 20:01:26 -0600

    Time: 415 ms (48.82%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
-# Activity (machine_id int, process_id int, activity_type ENUM('start', 'end'), timestamp float)
-# divide up by machines.
-# tables split up in two cases - 'start', 'end'
-# In the first one, a solution based on 'join' is achieved (which is more self-explanatory)
-
-select a1.machine_id, round(avg(a2.timestamp- a1.timestamp), 3) as processing_time
+select a1.machine_id, round(avg(a2.timestamp - a1.timestamp),3) as processing_time
 from Activity a1
 join Activity a2
-on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
-and a1.activity_type='start' and a2.activity_type = 'end'
-group by a1.machine_id
+on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type='start' and a2.activity_type = 'end'
+group by a1.machine_id;
