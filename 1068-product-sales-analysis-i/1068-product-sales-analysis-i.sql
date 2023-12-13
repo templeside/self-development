e771811dc3b30e74646fa605be65df263495f02e
templeside <47994862+templeside@users.noreply.github.com>
2023-12-13 17:46:08 -0600

    Time: 1959 ms (50.63%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,17 +1,5 @@
 # Write your MySQL query statement below
-# Sales (sale_id int, product_id int, year int, quantity int, price int)
-# Product (product_id int, product_name varchar(10))
-
-SELECT Product.product_name, Sales.year, Sales.price
-from Sales
-INNER Join Product 
-on Product.product_id = Sales.product_id;
-
-# SELECT 
-#     DISTINCT p.product_name, s.year, s.price
-# FROM 
-#     Sales s
-# JOIN 
-#     Product p
-# ON
-#     s.product_id = p.product_id;
+select Product.product_name, Sales.year, Sales.price
+FROM Sales
+LEFT JOIN Product
+on Sales.product_id = Product.product_id;
