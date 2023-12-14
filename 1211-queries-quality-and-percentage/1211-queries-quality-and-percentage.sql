a388dc3fb4e56603b4f3b1f1ac11b7b7166fd83d
templeside <47994862+templeside@users.noreply.github.com>
2023-12-13 22:52:50 -0600

    Time: 970 ms (6.92%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -3,7 +3,9 @@
 # quality = avg( (rating/ position))
 # poor query = avg(case rating<3 1 else 0)*100 
 
-SELECT query_name, ROUND(SUM(rating/position)/ COUNT(rating),2) as quality, Round(avg(rating<3)*100,2) as poor_query_percentage
+SELECT query_name, 
+    ROUND(avg(rating/position),2) as quality, 
+    Round(avg(rating<3)*100,2) as poor_query_percentage
 FROM Queries
 where query_name is not null
 GROUP BY query_name;
