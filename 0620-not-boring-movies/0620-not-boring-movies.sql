b885085cbb2709e1ecba4cc3a7a7deaca3cec83e
templeside <47994862+templeside@users.noreply.github.com>
2023-12-13 19:46:02 -0600

    Time: 400 ms (55.82%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-# cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1), primary key(id))
-
-# movie, id, description
-select  id,movie, description, rating
-from Cinema
-# where description !="boring" and odd number
-Where description !="boring" and id%2= 1
-# order by rating in desc.
-ORDER BY rating desc;
+SELECT *
+FROM Cinema
+Where MOD(id,2) = 1 and description != "boring"
+ORDER BY rating DESC;
