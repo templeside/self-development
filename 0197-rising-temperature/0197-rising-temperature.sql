bd5f83c9b08a92d873109319151f7d7b61556801
templeside <47994862+templeside@users.noreply.github.com>
2023-12-13 18:09:34 -0600

    Time: 871 ms (43.40%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-# for the table, it should be joining with recordDate = recordDate-1.
-# and compare the two temperature.
 
-SELECT curr.id 
-FROM Weather curr
-LEFT JOIN Weather yester
-# ON curr.recordDate = yester.recordDate+1
-ON DATEDIFF(curr.recordDate, yester.recordDate) =1
-where curr.temperature> yester.temperature;
+select today.id
+FROM Weather today
+LEFT JOIN Weather yesterday
+on DateDiff(today.recordDate, yesterday.recordDate) = 1
+where today.temperature> yesterday.temperature;
