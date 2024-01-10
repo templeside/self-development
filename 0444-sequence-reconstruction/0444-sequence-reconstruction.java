808e95280b9d13a5cd94f00b947ade2799f22a3d
templeside <47994862+templeside@users.noreply.github.com>
2024-01-09 19:22:41 -0500

    Time: 23 ms (70.98%), Space: 48.5 MB (78.32%) - LeetHub

---
+++ 0444-sequence-reconstruction/0444-sequence-reconstruction.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean sequenceReconstruction(int[] nums, List<List<Integer>> sequences) {
+        int n = nums.length;
+
+        int[] inDegree = new int[n + 1];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int listSize = 0; listSize <= n; listSize++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(List<Integer> seq : sequences) {
+            for(int index = 1; index < seq.size(); index++) {
+                int nodeA = seq.get(index - 1);
+                int nodeB = seq.get(index);
+                
+                if(nodeA < 1 || nodeA > n || nodeB < 1 || nodeB > n) return false;
+                
+                graph.get(nodeA).add(nodeB);
+                inDegree[nodeB]++;
+            }
+        }
+
+        Queue<Integer> topSort = new LinkedList<>();
+        for(int node = 1; node <= n; node++) {
+            if(inDegree[node] == 0) {
+                topSort.offer(node);
+            }
+        }
+
+        for(int index = 0; index < n; index++) {
+            if(topSort.size() > 1 || nums[index] != topSort.peek()) return false;
+
+            int curr = topSort.poll();
+            for(int next : graph.get(curr)) {
+                inDegree[next]--;
+                if(inDegree[next] == 0) topSort.offer(next);
+            }
+        }
+
+        return true;
+    }
+}
