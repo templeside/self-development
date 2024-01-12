c0ad026b29d30f893a1e49cbd1948bb7a1185d36
templeside <47994862+templeside@users.noreply.github.com>
2024-01-11 19:39:25 -0500

    Time: 5 ms (50.19%), Space: 42.3 MB (13.00%) - LeetHub

---
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+    need to find out it is lexicographically ordered.
+    in this case, we can use topological sort.
+    1. initiate two hashmap:
+        inDegree<alphabet, prerequisite counter>
+        graph<currNode, neighborNode>
+    2. iterate through the words, and build the hashmaps
+    3. look for the non-prerequisite characters.
+    4. find the nodes.
+    
+    return in the ordered way.
+    
+    ["abc","ab"]
+    **/
+    public String alienOrder(String[] words) {
+        HashMap<Character, Integer> inDegree = new HashMap<>();
+        HashMap<Character, List<Character>> graph = new HashMap<>();
+        
+        for(String word: words){
+            for(char c: word.toCharArray()){
+                inDegree.put(c, 0);
+                graph.put(c, new ArrayList<>());
+            }
+        }
+        for(int i=0; i< words.length-1; i++){
+            String prereq = words[i];
+            String curr = words[i+1];
+            // Check that word2 is not a prefix of word1.
+            if (prereq.length() > curr.length() && prereq.startsWith(curr)) 
+                return "";
+            for(int j=0; j< Math.min(prereq.length(), curr.length()); j++){
+                if(prereq.charAt(j) != curr.charAt(j)){
+                    char prevChar = prereq.charAt(j);
+                    char currChar = curr.charAt(j);
+                    inDegree.put(currChar, inDegree.get(currChar)+1);
+                    graph.get(prevChar).add(currChar);
+                    break;
+                }
+            }
+        }
+        
+        StringBuilder currOrder = new StringBuilder();
+        Queue<Character> topoIterator = new LinkedList<>();
+        for(Map.Entry<Character, Integer> set: inDegree.entrySet()){
+            if(set.getValue() ==0)
+                topoIterator.add(set.getKey());
+        }
+        
+        while(topoIterator.size()>0){
+            char currChar = topoIterator.poll();
+            currOrder.append(currChar);
+            for(char neighbor: graph.get(currChar)){
+                inDegree.put(neighbor, inDegree.get(neighbor)-1);
+                if(inDegree.get(neighbor) ==0)
+                    topoIterator.add(neighbor);
+            }
+        }
+        return currOrder.length() == inDegree.size()? currOrder.toString() : "";
+    }
+}
