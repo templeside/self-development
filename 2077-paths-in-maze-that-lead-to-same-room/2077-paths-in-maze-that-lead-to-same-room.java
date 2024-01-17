5f15fdc441bf2069317bfe5fbc68ac54c7e6efe4
templeside <47994862+templeside@users.noreply.github.com>
2024-01-17 18:11:55 -0500

    Time: 380 ms (44.44%), Space: 71.1 MB (40.00%) - LeetHub

---
+++ 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numberOfPaths(int n, int[][] corridors) {
+        Map<Integer, Set<Integer>> neighbors= new HashMap<>();
+        int cycles=0;
+
+        for(int[] corridor: corridors){
+            int room1= corridor[0];
+            int room2= corridor[1];
+
+            neighbors.putIfAbsent(room1, new HashSet<>());
+            neighbors.putIfAbsent(room2, new HashSet<>());
+            neighbors.get(room1).add(room2);
+            neighbors.get(room2).add(room1);
+
+            cycles += intersectionLength(neighbors.get(room1), neighbors.get(room2));
+        }
+        return cycles;
+    }
+
+    public static int intersectionLength(Set<Integer> set1, Set<Integer> set2){
+        int count=0;
+        for(int element: set1){
+            if(set2.contains(element)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
