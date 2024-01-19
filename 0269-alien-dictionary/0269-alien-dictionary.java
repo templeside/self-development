ed9c0b54dea9c54e6c1750a3da0a57aadfd67497
templeside <47994862+templeside@users.noreply.github.com>
2024-01-19 18:59:01 -0500

    Time: 14 ms (5.07%), Space: 42.6 MB (9.67%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,61 +1,63 @@
 class Solution {
-    /**
-    need to find out it is lexicographically ordered.
-    in this case, we can use topological sort.
-    1. initiate two hashmap:
-        inDegree<alphabet, prerequisite counter>
-        graph<currNode, neighborNode>
-    2. iterate through the words, and build the hashmaps
-    3. look for the non-prerequisite characters.
-    4. find the nodes.
-    
-    return in the ordered way.
-    
-    ["abc","ab"]
-    **/
     public String alienOrder(String[] words) {
-        HashMap<Character, Integer> inDegree = new HashMap<>();
-        HashMap<Character, List<Character>> graph = new HashMap<>();
-        
-        for(String word: words){
-            for(char c: word.toCharArray()){
-                inDegree.put(c, 0);
-                graph.put(c, new ArrayList<>());
+      // create hashmap with values
+      Map<Character, List<Character>> graph = new HashMap<>();
+      for(String word: words){
+         for(char c: word.toCharArray())
+            graph.putIfAbsent(c, new ArrayList<>());
+      }
+
+      // add graph adjacnecies
+      for(int i=0; i < words.length-1; i++){
+         String prereq = words[i];
+         String curr = words[i+1];
+         // Check that word2 is not a prefix of word1. 
+         if (prereq.length() > curr.length() && prereq.startsWith(curr))
+            return "";
+         for(int j=0; j< Math.min(prereq.length(), curr.length()); j++){
+            if(prereq.charAt(j) != curr.charAt(j)){
+               char prevChar = prereq.charAt(j);
+               char currChar = curr.charAt(j);
+               graph.get(prevChar).add(currChar);  //graph add neighbors
+               break;
             }
-        }
-        for(int i=0; i< words.length-1; i++){
-            String prereq = words[i];
-            String curr = words[i+1];
-            // Check that word2 is not a prefix of word1.
-            if (prereq.length() > curr.length() && prereq.startsWith(curr)) 
-                return "";
-            for(int j=0; j< Math.min(prereq.length(), curr.length()); j++){
-                if(prereq.charAt(j) != curr.charAt(j)){
-                    char prevChar = prereq.charAt(j);
-                    char currChar = curr.charAt(j);
-                    inDegree.put(currChar, inDegree.get(currChar)+1);
-                    graph.get(prevChar).add(currChar);
-                    break;
-                }
-            }
-        }
-        
-        StringBuilder currOrder = new StringBuilder();
-        Queue<Character> topoIterator = new LinkedList<>();
-        for(Map.Entry<Character, Integer> set: inDegree.entrySet()){
-            if(set.getValue() ==0)
-                topoIterator.add(set.getKey());
-        }
-        
-        while(topoIterator.size()>0){
-            char currChar = topoIterator.poll();
-            currOrder.append(currChar);
-            for(char neighbor: graph.get(currChar)){
-                inDegree.put(neighbor, inDegree.get(neighbor)-1);
-                if(inDegree.get(neighbor) ==0)
-                    topoIterator.add(neighbor);
-            }
-        }
-        return currOrder.length() == inDegree.size()? currOrder.toString() : "";
-    }
+         }
+      }
+
+   return toposort(graph);
+   }
+
+   public static String toposort(Map<Character, List<Character>> graph){
+      String order = "";
+      ArrayDeque<Character> queue = new ArrayDeque<>();
+
+      //create inDegree
+      Map<Character, Integer> inDegree = new HashMap<>();
+      for(Character vertex: graph.keySet()){
+         inDegree.put(vertex, 0);
+      }
+      //find neighbor to be in 
+      for(Map.Entry<Character, List<Character>> entry: graph.entrySet()){
+         for(char neighbor: entry.getValue()){
+            inDegree.put(neighbor, inDegree.get(neighbor)+1);
+         }
+      }
+      //find 0 inDegree node
+      for(Map.Entry<Character, Integer> entry: inDegree.entrySet()){
+         if(entry.getValue() ==0)
+         queue.add(entry.getKey());
+      }
+
+      //do topological sort
+      while(!queue.isEmpty()){
+         char c = queue.poll();
+         order = order+c;
+         for(char neighbor: graph.get(c)){
+            inDegree.put(neighbor, inDegree.get(neighbor)-1);
+            if(inDegree.get(neighbor)== 0)
+               queue.add(neighbor);
+         }
+      }
+      return graph.size() == order.length()? order: "";
+   }
 }
