6877e295e87730af8af2c27267ec541dc507e705
templeside <47994862+templeside@users.noreply.github.com>
2024-01-26 14:37:33 -0500

    Time: 5 ms (94.14%), Space: 66.4 MB (86.11%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
@@ -9,40 +9,50 @@
  * }
  */
 class Solution {
+    /*
+    prev - prev notifier
+    node - iterator
+    reverse - reversed connecting lines.
+    
+    */
     public ListNode reverseEvenLengthGroups(ListNode head) {
-		ListNode prev = head;
-		ListNode node, reverse, currNext, curr, prevNext = null;
-		int groupLen = 2;
-		int numNodes = 0;
-		while(prev.next!= null)
-		{
-			node = prev;
-			numNodes = 0;
-			for (int i = 0; i < groupLen; i ++)
-			{
-				if(node.next == null)
+        ListNode prev = head;
+        ListNode node, prevNext = null;      // for connecting groups
+        ListNode curr, currNext, reversed  = null;   // for reversing
+        int groupLen = 2;
+        int numNodes = 0;
+        
+        while(prev.next !=null){
+            node = prev;
+            numNodes = 0;
+            for(int i=0; i< groupLen; i++){
+                if(node.next == null)
                     break;
-				numNodes += 1;
-				node=node.next;
-			}
+                numNodes +=1;
+                node = node.next;
+            }
+            //checking even or not.
+            if(numNodes %2 !=0){
+                prev = node;
+            }else{
+                //if even length, reverse
+                reversed = node.next;
+                curr = prev.next;
 
-			if(numNodes % 2 != 0) {
-			   prev = node; 
-			} else {
-				reverse = node.next;
-				curr = prev.next;
-                for(int j=0; j < numNodes;j++){
-                    currNext = curr.next;
-				    curr.next = reverse;
-				    reverse = curr;
-				    curr = currNext;
-                }
-                prevNext = prev.next;
-				prev.next = node;
-			    prev = prevNext;
-			}
-			groupLen += 1;
-		}
-	    return head;
+                for(int j=0; j<numNodes; j++){
+                    currNext = curr.next;       // currNext = curr.nex
+                    curr.next = reversed;       // curr.next = reversed
+                    reversed = curr;            
+                    curr = currNext;
+                }     
+                                        //                        prev |
+                prevNext = prev.next;   //reversed tail.   orig= prev->1->2->3   reversed= prev 3->2->1
+                prev.next = node;       //connecting with reversed node.  prev->3->2->1
+                prev=prevNext;          //setting new prev to 1.   ...->3->2->1
+                                        //                           new prev |
+            }
+            groupLen +=1;
+        }
+        return head;
 	}
 }
