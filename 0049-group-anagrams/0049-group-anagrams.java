c69d2f8efad99c5fb73d729428ae8d484ddd7bd6
templeside <47994862+templeside@users.noreply.github.com>
2024-07-18 16:24:20 -0500

    Time: 10 ms (28.97%), Space: 48.2 MB (19.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,51 +1,36 @@
 class Solution {
     /*
-    need to sort the strs in same place.
-    to store values in same place, we can use hashmap.
-    hashMap<hashcodeKey, anagram strings>
-    hashMap<String, List<String>>
-    
-    how to group?
-    common things are - frequency of chars.
-    we can list out in alphabetical order
-    ex) cat - a1c1t1
-    ex) tac - a1c1t1
-    
-    algorithm:
-        iterating strs:
-            convert into hashcodeKey
-            save it into hashmap
-        return the hashmap after convert to List<List<String>>
+    Anagram - same things count chars.
+    save in hashmap - for grouping
+    hashmap<
     */
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> hashmap = new HashMap<>();
-        
+        Map<String, List<String>> encodedMap = new HashMap<>();
         for(String str: strs){
-            String hashcodeKey = hash(str);
-            if(!hashmap.containsKey(hashcodeKey))
-                hashmap.put(hashcodeKey, new ArrayList<>());
-            List<String> hashcodeGroup = hashmap.get(hashcodeKey);
-            hashcodeGroup.add(str);
+            String encoded = count(str);
+            List<String> encodedList = encodedMap.getOrDefault(encoded, new ArrayList<>());
+            encodedList.add(str);
+            encodedMap.put(encoded, encodedList);
         }
         
-        List<List<String>> returnGroup = new ArrayList<>();
-        for(Map.Entry<String, List<String>> entry : hashmap.entrySet()){
-            returnGroup.add(entry.getValue());
+        List<List<String>> returnVal = new ArrayList<>();
+        for(Map.Entry<String, List<String>> entry: encodedMap.entrySet()){
+            returnVal.add(entry.getValue());
         }
-        return returnGroup;
+        return returnVal;
     }
-    
-    public static String hash(String s){
-        int[] charFreq = new int[26];
+    public String count(String s){
+        s.toLowerCase();
+        int[] arr = new int[26];
         for(char c: s.toCharArray()){
-            charFreq[c-'a']++;
+            arr[c-'a']++;
         }
         
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<26; i++){
-            if(charFreq[i] !=0){
+        for(int i=0; i< 26; i++){
+            if(arr[i] !=0){
                 sb.append((char)('a'+i));
-                sb.append(charFreq[i]);
+                sb.append(arr[i]);
             }
         }
         return sb.toString();
