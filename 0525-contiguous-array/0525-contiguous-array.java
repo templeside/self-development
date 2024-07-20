e178e50da69bf8bf6758e990d807fa3e0e9a9a32
templeside <47994862+templeside@users.noreply.github.com>
2024-07-20 16:04:30 -0500

    Time: 25 ms (22.71%), Space: 55.3 MB (17.58%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    /*
+    1. change all 0 to -1
+    2. for iteration:
+        update currSum
+        update the hashamp<sum, longestIdx>
+        calculate difference
+        if found sum to be matched to become 0, update maxLength
+    return maxLength
+    */
+    public int findMaxLength(int[] nums) {
+        int n = nums.length;
+        
+        // 1. change all 0 to -1
+        for(int i=0; i< n; i++){
+            if(nums[i] == 0){
+                nums[i] = -1;
+            }
+        }
+        int currSum = 0;
+        int maxLength = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
+        // 2. for iteration:
+        for(int i=0; i<n; i++){
+        //     update currSum
+                currSum = nums[i] + currSum;
+        //     calculate difference
+        //     if found sum to be matched to become 0, update maxLength                
+                if(map.containsKey(currSum)){
+                    maxLength = Math.max(maxLength, i - map.get(currSum));
+                }
+        //     update the hashamp<sum, longestIdx>
+                if(!map.containsKey(currSum))
+                    map.put(currSum, i);
+
+            }
+        // return maxLength
+        return maxLength;
+        
+    }
+}
