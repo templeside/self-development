c52ecf1d6411f6bc4351de41c29b544085442b4e
templeside <47994862+templeside@users.noreply.github.com>
2024-07-22 20:32:37 -0500

    Time: 1 ms, Memory: 41.2 MB - LeetHub

---
+++ decode-string/decode-string.java
@@ -0,0 +1,60 @@
+class Solution {
+    /*
+    find the digit
+        recursion with the matching brackets
+            multiply by the digits
+            add to the returning value
+    */
+    public String decodeString(String s) {
+        int n = s.length();
+        return recursion(s, 0, n-1);
+    }
+    
+    public String recursion(String s, int startIdx, int endIdx){
+        StringBuilder sb = new StringBuilder();
+        
+        // find the digit
+        for(int i=startIdx; i<= endIdx; i++){
+            char currChar = s.charAt(i);
+            if(Character.isDigit(currChar)){
+                int digitEnd = i;
+                while(Character.isDigit(s.charAt(digitEnd)))
+                    digitEnd++;
+                
+                //find left
+                int newStartIdx = digitEnd;
+                //find right
+                int newEndIdx = findMatchingEnd(newStartIdx, s);
+                
+                // recursion with the matching brackets
+                String decoded = recursion(s, newStartIdx, newEndIdx);
+                
+                // multiply by the digits
+                for(int a=0; a< Integer.valueOf(s.substring(i, digitEnd)); a++){
+                    //add to the returning value
+                    sb.append(decoded);
+                }
+                    
+                i = newEndIdx-1;
+            }else if(Character.isLetter(currChar)){
+                sb.append(currChar);
+            }
+        }
+        return sb.toString();
+    }
+    
+    public int findMatchingEnd(int startIdx, String s){
+        Stack<Character> stack =new Stack<>();
+        for(int i= startIdx; i< s.length(); i++){
+            char currChar = s.charAt(i);
+            if(currChar =='[')
+                stack.push(currChar);
+            else if(currChar ==']'){
+                stack.pop();
+                if(stack.size() ==0)
+                    return i;
+            }
+        }
+        return -1;
+    }
+}
