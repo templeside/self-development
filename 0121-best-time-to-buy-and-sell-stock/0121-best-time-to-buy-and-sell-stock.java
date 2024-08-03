efb449d128156e782e05cba7f136f360b3757ad5
templeside <47994862+templeside@users.noreply.github.com>
2024-08-03 13:47:17 -0500

    Time: 2 ms (70.21%), Space: 59.4 MB (96.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,41 +1,24 @@
 class Solution {
     /*
-    max day
-    by shifting, find a best way.
-    
-    brute force way - 
-    two pointers, left, right
-    for(int right=0; i< n; i++)
-        for(int left =0 left<right; left++)
-            find max profit, update
-    time - O(N^2)
-    space - O(1)
-    
-    better approach? 
-    
-    prices = [7,1,5,3,6,4]
-buy           ^
-sell          ^
-    
-minSell), maxBuy)
-    
-    increment: update sell
-    decrement: update buy.
-    
+    [7,1,5,3,6,4]
+buy  ^
+sell ^
+
+max 7
+min 7
+profit 0
+max date, min date update while reaching end
+why this is dynamic programming?
     */
     public int maxProfit(int[] prices) {
-        int minBuy = prices[0];
-        int maxProfit = 0;
+        int max = prices[0];
+        int min = prices[0];
+        int maxProfit = max-min;
         
-        int buy = 0;
-        for(int sell=0; sell< prices.length; sell++){
-        // increment: update sell
-            int sellPrice = prices[sell];
-        // decrement: update buy.            
-            while(minBuy> prices[sell]){
-                minBuy = Math.min(minBuy, prices[sell]);
-            }
-            maxProfit = Math.max(sellPrice-minBuy, maxProfit);
+        for(int i=0; i< prices.length; i++){
+            min = Math.min(prices[i], min);
+            int diff = prices[i]-min;
+            maxProfit = Math.max(maxProfit, diff);
         }
         return maxProfit;
     }
