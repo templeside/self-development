3a584a564b3ddc610a7546719614a31edf99777b
templeside <47994862+templeside@users.noreply.github.com>
2024-08-08 14:17:19 -0500

    Time: 4 ms (52.36%), Space: 41.4 MB (69.17%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,36 +1,61 @@
 class Solution {
     /*
-    aaabb
-    3 > 2
-    aabb
-    aaab
+    calculate all the strings
+    count the number
+    if the max is bigger than half, return ""   n+1/2 > maxcount
+    
+    while out of all chars
+        shifting index, adding chars
+        max, next max
+        
+s= aab
+a-2
+b-1
+
+max: a,2
+queue: b,1
     */
     public String reorganizeString(String s) {
-        char[] returnArr = new char[s.length()];
-        Map<Character, Integer> freq=  new HashMap<>();
+//         calculate all the strings
+        Map<Character, Integer> map = new HashMap<>();
+//         count the number
         for(char c: s.toCharArray()){
-            freq.put(c, freq.getOrDefault(c, 0)+1);
+            map.put(c, map.getOrDefault(c, 0)+1);
         }
-        PriorityQueue<Map.Entry<Character, Integer>> queue = new PriorityQueue<>((a,b)-> (b.getValue()- a.getValue()));
-        
-        for(Map.Entry<Character, Integer> entry: freq.entrySet()){
+        PriorityQueue<Map.Entry<Character, Integer>> queue= new PriorityQueue<>((a,b)-> b.getValue()-a.getValue());
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
             queue.add(entry);
         }
-        
+
+//         if the max is bigger than half, return ""   n+1/2 > maxcount
         if(queue.peek().getValue() > (s.length()+1)/2)
             return "";
         
-        int ptr = 0;
+        Map.Entry<Character, Integer> curr = queue.poll();
+        char currChar = curr.getKey();
+        int currCount = curr.getValue();
         
-        while(!queue.isEmpty()){
-            Map.Entry<Character, Integer> e = queue.poll();
-            for(int i=0; i< e.getValue(); i++){
-                returnArr[ptr] = e.getKey();
-                ptr +=2;
-                if(ptr>= s.length())
-                    ptr = 1;
+        char[] returnVal = new char[s.length()];
+        //even first
+        for(int i=0; i< s.length(); i=i+2){
+            returnVal[i] = currChar;
+            currCount--;
+            if(currCount ==0 && !queue.isEmpty()){
+                curr = queue.poll();
+                currChar = curr.getKey();
+                currCount = curr.getValue();
             }
         }
-        return new String(returnArr);
+        //odd second
+        for(int i=1; i< s.length(); i=i+2){
+            returnVal[i] = currChar;
+            currCount--;
+            if(currCount ==0 && !queue.isEmpty()){
+                curr = queue.poll();
+                currChar = curr.getKey();
+                currCount = curr.getValue();
+            }
+        }
+        return new String(returnVal);
     }
 }
