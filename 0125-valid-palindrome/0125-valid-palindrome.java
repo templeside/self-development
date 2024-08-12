972fe0fcb823cab8e528bfe9334f7ecac0fa3442
templeside <47994862+templeside@users.noreply.github.com>
2024-08-12 17:32:07 -0500

    Time: 4 ms (56.12%), Space: 42.6 MB (89.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,39 +1,29 @@
 class Solution {
     /*
-    to lower case, remove all non-alphanumeric characters
-    
-    forward and backward is same.
-    
-    two pointers left, right
-    if(left & right is characters)
-        check it is same
-        
     "race a car"
-        ^ ^
+     ^        ^
+     
     */
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length()-1;
+        s = s.toLowerCase();
+        int left=0;
+        int right =s.length()-1;
         
-        while(left< right){
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
-            if(!Character.isAlphabetic(leftChar) && !Character.isDigit(leftChar) ){
+        while(left<right){
+            if(!Character.isAlphabetic(s.charAt(left)) && !Character.isDigit(s.charAt(left))){
                 left++;
                 continue;
             }
-            else if(!Character.isAlphabetic(rightChar) && !Character.isDigit(rightChar)){
+            else if (!Character.isAlphabetic(s.charAt(right)) && !Character.isDigit(s.charAt(right))){
                 right--;
                 continue;
             }
-            else{
-                leftChar = Character.toLowerCase(leftChar);
-                rightChar = Character.toLowerCase(rightChar);
-                if(leftChar != rightChar)
-                    return false;
-                
-                left++;
-                right--;
-            }
+            
+            if(s.charAt(left)!= s.charAt(right))
+                return false;
+            
+            left++;
+            right--;
         }
         return true;
     }
