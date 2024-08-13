6f903a6ea983df50e40d2f54ca0e37f246ade89a
templeside <47994862+templeside@users.noreply.github.com>
2024-08-12 22:42:25 -0500

    Time: 3 ms (25.07%), Space: 45.9 MB (51.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,39 +1,38 @@
 class Solution {
     /*
-    using two pointer, update the values.
-    
-nums = [0,1,0,3,12]
-nums = [1,3,12,0,0]
-idx             ^
-nonzeros        ^        
+            [0,1,0,3,12]
+            [1,0,0,3,12]
+            [1,3,12,0,0]
+nonzeroIdx       ^
+currIdx               ^
 
-for(int i=0; i< n; i++)
-    if(non zero)
-        updateIdx(currIdx, nonzeroIdx)
-        currIdx++;
-        nonzeroIdx++;
-    else
-        currIdx++;
-        
-O(n)
-O(1)
-
-instead of finding 0, look for the non zero integers.
-
+if found nonzero, swap.
+    currIdx++
+    nonzeroIdx++
+else
+    currIdx++
     */
     public void moveZeroes(int[] nums) {
-        int nonZeroIdx = 0;
-        int currIdx=0;
-        int n = nums.length;
+        int nonzeroIdx = 0;
+        int currIdx = 0;
         
-        for(currIdx=0; currIdx<n; currIdx++){
-            if(nums[currIdx] !=0){
-                updateIdx(currIdx, nonZeroIdx, nums);
-                nonZeroIdx++;
+        for(; currIdx<nums.length; currIdx++){
+            int curr = nums[currIdx];
+            // if found nonzero, swap.
+            if(curr !=0){
+                swap(currIdx, nonzeroIdx, nums);
+            //     currIdx++
+            //     nonzeroIdx++
+                nonzeroIdx ++;
+                continue;
+            }else{
+            // else
+            //     currIdx++
+                continue;
             }
         }
     }
-    public void updateIdx(int i, int j, int[] nums){
+    public void swap(int i, int j, int[]nums){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
