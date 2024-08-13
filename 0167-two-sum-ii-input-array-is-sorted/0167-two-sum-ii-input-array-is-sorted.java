b44d9b2d8cdf48d885c9ef16f28517c65ffe9973
templeside <47994862+templeside@users.noreply.github.com>
2024-08-12 21:35:02 -0500

    Time: 2 ms (90.13%), Space: 47.2 MB (26.06%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,27 @@
 class Solution {
     /*
-    save prev <Value, Index> for the hashmap.
-    while iteration, add find the remainder target - currValue(numbers[i])
+    sorted array
+        [2,7,11,15], target = 9
+Output: [1,2]
     
-    if found, reutrn value with index.
+    i could use two forloop
+    two pointers:
+    left, right
+    shrink it down to find the value 
     */
     public int[] twoSum(int[] numbers, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<numbers.length; i++){
-            int diff = target-numbers[i];
-            if(map.containsKey(diff))
-                return new int[]{map.get(diff)+1, i+1};
-            
-            map.put(numbers[i], i);
+        int idx1 = 0, idx2= numbers.length-1;
+        
+        while(idx1<idx2){
+            int sum = numbers[idx1] + numbers[idx2];
+            if(target == sum)
+                return new int[]{idx1+1, idx2+1};
+            else if(target> sum){
+                idx1++;
+            }else{
+                idx2--;
+            }
         }
-        return new int[]{};
+        return null;
     }
 }
