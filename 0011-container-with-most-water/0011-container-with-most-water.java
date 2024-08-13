2f0afce47e5fa71cc8ca0a3f4c180e6a1442d0d4
templeside <47994862+templeside@users.noreply.github.com>
2024-08-12 22:54:29 -0500

    Time: 5 ms (69.21%), Space: 58.1 MB (27.69%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,60 +1,27 @@
 class Solution {
     /*
-    always would be great to have left and right height.
-    left and right wall should be the max val.
-        if left or right is updated, update the maxWater
-    idx              0 1 2 3 4 5 6 7 8
-    Input: height = [1,8,6,2,5,4,8,3,7]
-    maxLeft        0 1 8 8 8 8 8 8 8 8 8
-    left                 ^
-    maxRight       8 8 8 8 8 8 8 8 7 7 0 
-    right                            ^
-    area = (right-left)* (Min(left,right))
-    area =  8-1 (7) 49
+    area = width * height
+    width = right-left+1
+    height = min(leftHeight, rightHeight);
     
-    1. find maxLeft
-    2. find maxRight
-    3. have two pointers
-    4. update area by updating input
+    shift the bigger height
+    
     */
     public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
         int maxArea = 0;
-        int n = height.length;
-        // find maxLeft
-        int[] maxLeft = new int[n];
-        for(int i=0; i<n; i++){
-            if(i ==0){
-                maxLeft[i] = height[i];
-                continue;
-            }
-            maxLeft[i] = Math.max(maxLeft[i-1], height[i]);
-        }
         
-        // find maxRight
-        int[] maxRight= new int[n];
-        for(int i=n-1; i>=0; i--){
-            if(i ==n-1){
-                maxRight[i] = height[i];
-                continue;
-            }
-            maxRight[i] = Math.max(maxRight[i+1], height[i]);
-        }
-        
-        // have two pointers
-        int left=0, right=n-1;
-        
-        // update area by updating input
         while(left<right){
-            //check area & 
-            int area = (right-left)* Math.min(maxLeft[left], maxRight[right]);
-            //update input
-            maxArea = Math.max(area, maxArea);
+            int currArea = (right-left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, currArea);
             
-            if(maxLeft[left]< maxRight[right]){
+            if(height[left]< height[right])
                 left++;
-            }else
+            else
                 right--;
         }
+        
         return maxArea;
     }
 }
