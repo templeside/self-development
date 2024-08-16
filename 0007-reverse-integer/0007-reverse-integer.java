df54b388a41b6b3f5440a6b224faede2e51790a8
templeside <47994862+templeside@users.noreply.github.com>
2024-08-15 22:42:11 -0500

    Time: 1 ms (79.97%), Space: 41 MB (21.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,34 +1,19 @@
-// class Solution {
-//     /*
-//     123
-//     %10
-//     3
-//     */
-//     public int reverse(int x) {
-//         boolean isNegative = x<0;
-//         x= Math.abs(x);
-//         int result=  0;
-//         while(x>0){
-//             result *=10;
-//             result+= x%10;
-//             x = x/10;
-//         }
-//         return isNegative?-result:result;
-//     }
-// }
-
 class Solution {
+    /*
+    123
+    %10
+    3
+    */
     public int reverse(int x) {
-        int result =0;
-        
+        int result=  0;
         while(x!=0){
-            int temp = x%10;
-            int newResult = result *10 + temp;
-            
-            if((newResult-temp)/10 != result)
+            int tail = x%10;
+            int newResult = result *10+ tail;
+
+            if((newResult- tail)/10 !=result)
                 return 0;
             result = newResult;
-            x=x/10;
+            x = x/10;
         }
         return result;
     }
