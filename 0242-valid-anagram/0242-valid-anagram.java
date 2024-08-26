496474f9a7c5124fbfe72a3e18dceb93d4861319
templeside <47994862+templeside@users.noreply.github.com>
2024-08-25 22:56:01 -0500

    Time: 16 ms (23.03%), Space: 45.1 MB (5.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!= t.length())return false;
+        if(s.length() != t.length())
+            return false;
         
-        Map<Character ,Integer> freq = new HashMap<>();
-        
+        Map<Character, Integer> frequency = new HashMap<>();
         for(char c: s.toCharArray()){
-            freq.put(c, freq.getOrDefault(c, 0)+1);
+            frequency.put(c, frequency.getOrDefault(c,0)+1);
         }
         
         for(char c: t.toCharArray()){
-            if(!freq.containsKey(c))return false;
-            
-            freq.put(c, freq.get(c)-1);
-            if(freq.get(c) ==0)
-                freq.remove(c);
-        }       
-        return freq.size()== 0;
+            if(!frequency.containsKey(c))
+                return false;
+            frequency.put(c, frequency.get(c)-1);
+            if(frequency.get(c)==0)
+                frequency.remove(c);
+        }
+        return frequency.size() ==0;
     }
 }
