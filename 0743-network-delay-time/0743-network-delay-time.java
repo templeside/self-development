332edcb9e62d69aae7eea9e7e7662464280a7e45
templeside <47994862+templeside@users.noreply.github.com>
2024-08-31 16:52:01 -0500

    Time: 33 ms (10.57%), Space: 48.2 MB (62.77%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,43 @@
+class Solution {
+    /*
+    times - u(source), v(target), w(weight)
+    N - number of Nodes
+    K- start Node
+    
+    if not possible, return -1
+    return min time to visit all Nodes
+    */
+    public int networkDelayTime(int[][] times, int N, int K) {
+        //  <source, <target, weight>>  source -> target
+        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
+        for(int[] time : times){
+            map.putIfAbsent(time[0], new HashMap<>());
+            map.get(time[0]).put(time[1], time[2]);
+        }
+        
+        //<weight, targetNode>
+        Queue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
+        
+        pq.add(new int[]{0, K});
+        
+        boolean[] visited = new boolean[N+1];
+        int res = 0;
+        
+        while(!pq.isEmpty()){
+            int[] cur = pq.remove();
+            int curNode = cur[1];
+            int curDist = cur[0];
+            if(visited[curNode]) 
+                continue;
+            visited[curNode] = true;
+            res = curDist;
+            N--;
+            if(map.containsKey(curNode)){
+                for(int next : map.get(curNode).keySet()){
+                    pq.add(new int[]{curDist + map.get(curNode).get(next), next});
+                }
+            }
+        }
+        return N == 0 ? res : -1;
+    }
+}
