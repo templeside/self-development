7e5aa80c7185db98ed894c849e638cfd3d760a24
templeside <47994862+templeside@users.noreply.github.com>
2024-09-05 22:20:56 -0500

    Time: 0 ms (100.00%), Space: 44.1 MB (88.22%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,50 +13,26 @@
  *     }
  * }
  */
-/*
-BST means it is in order,
-so we can do in order traversal and return the kth smallest element 
-return 
-    -1: when null
-    
-recursion:
-    left
-    ---
-    check left node
-    check current node
-    
-    right
-[5,3,6,2,4,null,null,1]
-k=3
-
-       5
-     /  \
-    3    6
-    /\   /\
-   2 4  n  n
-  /
- 1
-/
-n
-*/
 class Solution {
-    public static int k;
-    public int kthSmallest(TreeNode root, int a) {
-        k=a;
-        return inorder(root);
+    /*
+    having global variable to keep in track the kth smallest element
+    */
+    public int kth = 1;
+    public int returnVal;
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return returnVal;
     }
-    public int inorder(TreeNode root){
-        if(root == null)return -1;
+    public void inorder(TreeNode root, int k){
+        if(root ==null)
+            return;
         
-        int left = inorder(root.left);
+        inorder(root.left, k);
         
-        if( left != -1)
-            return left;
-        k--;
-        if(k==0)
-            return root.val;
+        if(kth == k)
+            returnVal = root.val;
+        kth ++;
         
-        int right = inorder(root.right);
-        return right;
+        inorder(root.right, k);
     }
 }
