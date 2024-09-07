bc168eaac4c70e56ed579fb3a6cbe7f6267737c1
templeside <47994862+templeside@users.noreply.github.com>
2024-09-07 12:58:25 -0500

    Time: 4 ms (48.05%), Space: 52 MB (17.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,81 +1,42 @@
 class Solution {
-    class UnionFind {
-        int count; // # of connected components
-        int[] parent;
-        int[] rank;
-
-        public UnionFind(char[][] grid) { // for problem 200
-            count = 0;
-            int m = grid.length;
-            int n = grid[0].length;
-            parent = new int[m * n];
-            rank = new int[m * n];
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if (grid[i][j] == '1') {
-                        parent[i * n + j] = i * n + j;
-                        ++count;
-                    }
-                    rank[i * n + j] = 0;
-                }
-            }
-        }
-
-        public int find(int i) { // path compression
-            if (parent[i] != i) parent[i] = find(parent[i]);
-            return parent[i];
-        }
-
-        public void union(int x, int y) { // union with rank
-            int rootx = find(x);
-            int rooty = find(y);
-            if (rootx != rooty) {
-                if (rank[rootx] > rank[rooty]) {
-                    parent[rooty] = rootx;
-                } else if (rank[rootx] < rank[rooty]) {
-                    parent[rootx] = rooty;
-                } else {
-                    parent[rooty] = rootx;
-                    rank[rootx] += 1;
-                }
-                --count;
-            }
-        }
-
-        public int getCount() {
-            return count;
-        }
-    }
-
+    /*
+    by iterating all grids, find the land
+    
+    if found land,
+        count island
+        do dfs to check all the connected land 
+        
+    */
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-
-        int nr = grid.length;
-        int nc = grid[0].length;
-        int num_islands = 0;
-        UnionFind uf = new UnionFind(grid);
-        for (int r = 0; r < nr; ++r) {
-            for (int c = 0; c < nc; ++c) {
-                if (grid[r][c] == '1') {
-                    grid[r][c] = '0';
-                    if (r - 1 >= 0 && grid[r - 1][c] == '1') {
-                        uf.union(r * nc + c, (r - 1) * nc + c);
-                    }
-                    if (r + 1 < nr && grid[r + 1][c] == '1') {
-                        uf.union(r * nc + c, (r + 1) * nc + c);
-                    }
-                    if (c - 1 >= 0 && grid[r][c - 1] == '1') {
-                        uf.union(r * nc + c, r * nc + c - 1);
-                    }
-                    if (c + 1 < nc && grid[r][c + 1] == '1') {
-                        uf.union(r * nc + c, r * nc + c + 1);
-                    }
+        int count =0;
+        int m =grid.length;
+        int n= grid[0].length;
+        for(int r=0; r< m; r++){
+            for(int c= 0; c< n; c++){
+                if(grid[r][c] == '1'){
+                    count++;
+                    dfs(r,c,grid);
                 }
             }
         }
+        return count;
+    }
+    
+    /*
+    change coordinate element to -1
+    look for adjacencies = {{0,1},{0,-1},{1,0},{-1,0}}
+    */
+    public void dfs(int r, int c, char[][] grid){
+        int[][] dirs =new  int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        int m =grid.length;
+        int n= grid[0].length;
 
-        return uf.getCount();
+        grid[r][c] = '0';
+        for(int[] adj: dirs){
+            int newR = r+ adj[0];
+            int newC= c+ adj[1];
+            if(newR>=0 && newR<m && newC>=0 && newC<n && grid[newR][newC] == '1')
+                dfs(newR,newC, grid);
+        }
     }
 }
