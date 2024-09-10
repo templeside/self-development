ef0b7af9d900219b14c508b8ac18efcf5739973c
templeside <47994862+templeside@users.noreply.github.com>
2024-09-10 01:55:10 -0500

    Time: 37 ms (37.54%), Space: 47 MB (96.60%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,73 +1,76 @@
 class Solution {
-    class UnionFind {
-        private Map<Integer, Integer> parents;
-        public UnionFind(int n) {
-            parents = new HashMap<>();
-            for (int i = 0; i < n; i++) {
-                parents.put(i,i);
-            }
-        }
+    public class UnionFind {
+    private int[] parents;
 
-        public int find(int node) {
-            if(parents.get(node) == node){
-                return node;
-            }
-            return find(parents.get(node));
-        }
-
-        public void union(int node1, int node2) {
-            int rootNode1 = find(node1);
-            int rootNode2 = find(node2);
-            if (rootNode1 != rootNode2) {
-                parents.put(rootNode2, rootNode1);
-            }
+    public UnionFind(int n) {
+        parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
         }
     }
 
+    public int find(int node) {
+        if (parents[node] == node) {
+            return node;
+        }
+        return find(parents[node]);
+    }
+
+    public void union(int node1, int node2) {
+        int rootNode1 = find(node1);
+        int rootNode2 = find(node2);
+        if (rootNode1 != rootNode2) {
+            parents[rootNode2] = rootNode1;
+        }
+    }
+}
+
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int size = accounts.size();
+        UnionFind uf = new UnionFind(accounts.size());
+        Map<String, Integer> emailMapping = new HashMap<>();
 
-        UnionFind uf = new UnionFind(size);
-
+        
         // prepare a hash with unique email address as key and index in accouts as value
-        HashMap<String, Integer> emailToId = new  HashMap<>();
-        for(int i = 0; i < size; i++) {
-            List<String> details = accounts.get(i);
-            for(int j = 1; j < details.size(); j++) {
-                String email = details.get(j);
+        // <email address, index of the account>
+        // union inside, uf contains the parent.
+        for (int i = 0; i < accounts.size(); ++i) {
+            List<String> emails = accounts.get(i);      // ["John","johnsmith@mail.com","john_newyork@mail.com"],
+            for (int j = 1; j < emails.size(); ++j) {
                 
-				// if we have already seen this email before, merge the account  "i" with previous account
-				// else add it to hash
-                if (emailToId.containsKey(email)) {
-                    uf.union(i, emailToId.get(email));
-                } else  {
-                    emailToId.put(email, i);
+                // if we have already seen this email before, merge the account  "i" with previous account
+                // if we already found the id, union
+                // else, put
+                if (emailMapping.containsKey(emails.get(j))) {
+                    if (!accounts.get(i).get(0).equals(accounts.get(emailMapping.get(emails.get(j))).get(0))) {
+                        return new ArrayList<>(); 
+                    }
+                    uf.union(emailMapping.get(emails.get(j)), i);
                 }
+                emailMapping.put(emails.get(j), i);
             }
         }
-        
+
         // prepare a hash with index in accounts as key and list of unique email address for that account as value
-        HashMap<Integer, List<String>> idToEmails = new HashMap<>();
-        for(String key : emailToId.keySet()) {
-            int root = uf.find(emailToId.get(key));
-            
-            if (!idToEmails.containsKey(root)) {
-                idToEmails.put(root, new ArrayList<String>());
-            }
-            
-            idToEmails.get(root).add(key);
+        Map<Integer, List<String>> mergedAccounts = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : emailMapping.entrySet()) {
+            int root = uf.find(entry.getValue());
+            mergedAccounts.computeIfAbsent(root, k -> new ArrayList<>());
+            mergedAccounts.get(root).add(entry.getKey());
         }
         
         // collect the emails from idToEmails, sort it and add account name at index 0 to get the final list to add to final return List
-        List<List<String>> mergedDetails =  new ArrayList<>();
-        for(Integer id : idToEmails.keySet()) {
-            List<String> emails =  idToEmails.get(id);
-            Collections.sort(emails);
-            emails.add(0, accounts.get(id).get(0));
-            
-            mergedDetails.add(emails);
-        }
+        List<List<String>> finalMerged = new ArrayList<>();
         
-        return  mergedDetails;
+        for (Map.Entry<Integer, List<String>> entry : mergedAccounts.entrySet()) {
+            int parent = entry.getKey();
+            List<String> emails = entry.getValue();
+            Collections.sort(emails);
+            List<String> merged = new ArrayList<>();
+            merged.add(accounts.get(parent).get(0));
+            merged.addAll(emails);
+            finalMerged.add(merged);
+        }
+        return finalMerged;
     }
 }
