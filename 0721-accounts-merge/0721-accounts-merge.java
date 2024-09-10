04e7a9eb1694403863ee1c701ee352f0fa218a74
templeside <47994862+templeside@users.noreply.github.com>
2024-09-10 01:38:04 -0500

    Time: 299 ms (5.64%), Space: 51.2 MB (9.28%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,45 +1,4 @@
 class Solution {
-//     class UnionFind {
-//         int[] parent;
-//         int[] weight;
-        
-//         public UnionFind(int num) {
-//             parent = new int[num];
-//             weight = new int[num];
-            
-//             for(int i =  0; i < num; i++) {
-//                 parent[i] = i;
-//                 weight[i] = 1;
-//             }
-//         }
-        
-//         public void union(int a, int  b) {
-//             int rootA = find(a);
-//             int rootB = find(b);
-            
-//             if (rootA == rootB) {
-//                 return;
-//             }
-            
-//             if (weight[rootA] > weight[rootB]) {
-//                 parent[rootB] = rootA;
-//                 weight[rootA] += weight[rootB];
-//             } else {
-//                 parent[rootA] = rootB;
-//                 weight[rootB] += weight[rootA];
-//             }
-//         }
-        
-//         public int find(int a) {
-//             if (parent[a] == a) {
-//                 return a;
-//             }
-            
-//             parent[a] = find(parent[a]);
-//             return parent[a];
-//         }
-//     }
-
     class UnionFind {
         private Map<Integer, Integer> parents;
         public UnionFind(int n) {
