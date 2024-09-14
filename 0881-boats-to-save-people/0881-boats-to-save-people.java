88ee200afb7ce8b3c4c052d76ec8ee1bd970a9e9
templeside <47994862+templeside@users.noreply.github.com>
2024-09-14 18:12:41 -0500

    Time: 17 ms (21.41%), Space: 55.1 MB (19.18%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,30 @@
+class Solution {
+    /*
+    do greedy idea.
+    sort the people
+    matching people light and 
+    
+        [1,2]
+left     ^
+right      ^
+    */
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        int count = 0;
+        
+        while(left<=right){
+            int remainder = limit-people[right];
+            if(left<= right && remainder>= people[left]){
+                left++;
+                right--;        
+                count++;
+                continue;
+            }
+            right--;
+            count++;
+        }
+        return count;
+    }
+}
