fb5900216aec7607b9df2b0328bf902ae5541c00
templeside <47994862+templeside@users.noreply.github.com>
2024-10-20 12:47:33 -0700

    Time: 33 ms (50.90%), Space: 60.4 MB (71.68%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,24 +1,39 @@
 class Solution {
     /*
-    arrays.sort
-    dp
-    [100,4,200,1,3,2]
-    1 2 3 4 100 200
-    ^
-    1,1 
+    consecutive - nums[i]< nums[i+1]
+    lazy.
+    so if it smaller than 0, sotp and move on.
+    
+    saving all the variables in to hashset and check is it having the next incrementing values.
     */
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        Map<Integer, Integer> dp = new HashMap<>();
-        int n = nums.length;
-        int longest =0;
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums)
+            set.add(num);
         
-        for(int i=0; i<n; i++){
-            int curr = nums[i];
-            dp.put(curr, dp.getOrDefault(curr-1, 0)+1);
-            
-            longest = Math.max(longest, dp.get(curr));
+        
+        int longest = 0;
+        for(int num : nums){
+            longest = Math.max(longest, findLongest(set, num));
         }
         return longest;
+    }
+    public int findLongest(Set<Integer> set, int num){
+        int count = 1;
+        int left =num;
+        int right =num;
+        
+        while(set.contains(left-1)){
+            left--;
+            count++;
+            set.remove(left);
+        }
+        while(set.contains(right+1)){
+            right++;
+            count++;
+            set.remove(right);
+        }
+        
+        return count;
     }
 }
