dc24897a8c929ea886629b3daff010641aad5d70
templeside <47994862+templeside@users.noreply.github.com>
2024-10-20 10:05:51 -0700

    Time: 5 ms (97.05%), Space: 43.3 MB (39.85%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,26 +1,17 @@
 class Solution {
-    private class StringNumComparator implements Comparator<String>{
-        @Override
-        public int compare(String a, String b){
-            String x = a+b;
-            String y = b+a;
-            return y.compareTo(x);
-        }
-    }
     public String largestNumber(int[] nums) {
-        String[] numsStringArr = new String[nums.length];
-        for(int i=0; i< nums.length; i++){
-            numsStringArr[i] = String.valueOf(nums[i]);
+        int n = nums.length;
+        String[] strings = new String[n];
+        for(int i=0; i< n; i++){
+            strings[i] = Integer.toString(nums[i]);
         }
-        
-        Arrays.sort(numsStringArr, new StringNumComparator());
-        
-        if(numsStringArr[0].equals("0"))
-            return "0";
+        Arrays.sort(strings, (a,b)-> (b+a).compareTo(a+b));
         
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i< nums.length; i++){
-            sb.append(numsStringArr[i]);
+        if(strings[0].equals("0"))
+            return "0";
+        for(int i=0; i<n; i++){
+            sb.append(strings[i]);
         }
         return sb.toString();
     }
