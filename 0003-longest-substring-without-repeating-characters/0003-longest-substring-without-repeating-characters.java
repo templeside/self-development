45601c775a219d8ebe311c2f228d6a8825b3e12c
templeside <47994862+templeside@users.noreply.github.com>
2024-10-21 08:46:52 -0700

    Time: 6 ms (69.54%), Space: 44.8 MB (26.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,35 +1,33 @@
 class Solution {
     /*
-    slidingwindow
-    to make it sure the problem is containing.
-    windowstart, windowEnd to keep track of the size of the substring.
-    keep track of the character types and numbers.
-    int[] charfrequency = new int[26];
-    
-    while increasing window end index,
-    checking the windows start index.
-    then check the longest substring wihtout repeating characters.
+    sliding window
+        increment:
+            update without repeating chcaracters
+            
+            after removing the repeating chars
+        decrement - when having duplicated chars
+        
+        update longest
     */
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        int windowStart = 0;
-        int[] frequency = new int[256];
-        // s= s.toLowerCase();
-        int maxLength = 0;
+        int endIdx = 0;
+        int longest = 0;
+        Set<Character> visited = new HashSet<>();
         
-        for(int windowEnd=0; windowEnd< n; windowEnd++){
-            char endChar = s.charAt(windowEnd);
-            frequency[endChar] ++;
-
-            while(frequency[endChar]>1){
-                char startChar = s.charAt(windowStart);
-                frequency[startChar]--;
-                windowStart++;
+        for(int startIdx=0; startIdx<n; startIdx++){
+            char currChar = s.charAt(startIdx);
+            
+            while(endIdx<= startIdx && visited.contains(currChar)){
+                char endChar = s.charAt(endIdx);
+                visited.remove(endChar);
+                endIdx++;
             }
-
-            maxLength = Math.max(maxLength, windowEnd- windowStart+1);
+            visited.add(currChar);
+            int windowSize = startIdx-endIdx+1;
+            
+            longest = Math.max(longest, windowSize);
         }
-        
-        return maxLength;
+        return longest;
     }
 }
