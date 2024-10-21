77da0a020545c62de6a3debfb274ca48d1d728fd
templeside <47994862+templeside@users.noreply.github.com>
2024-10-21 08:36:27 -0700

    Time: 1 ms (99.56%), Space: 58.3 MB (12.75%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,29 +1,39 @@
 class Solution {
     /*
-    increment: 
-        update sum
+    sliding window is possible because it does not have the negative integers
+    if it was not possible, we can go to prefixsum.
     
-    decrement: when sum>= target
-        update windowSize
-        decrement sum
-        shift windowTail
+    increment: 
+        update values.
+        
+    decrement: when currSum>= target
+        if(currSUm == target)
+            update minimum size sub array sum - minSub
+        decrement -= endNum
+        shift end
     */
     public int minSubArrayLen(int target, int[] nums) {
-        int sum = 0;
-        int min = nums.length+1;
-        int windowStart=0;
-        int windowTail = 0;
-        
-        for(; windowStart< nums.length; windowStart++){
-            sum += nums[windowStart];
-            
-            while(sum>= target){
-                min = Math.min(windowStart-windowTail+1, min);
-                sum -= nums[windowTail];
-                windowTail ++;
+        int n = nums.length;
+        int currSum = 0;
+        int endIdx = 0;
+        int minSub = n+1;
+        for(int startIdx=0; startIdx< n; startIdx++){
+            // increment: 
+            currSum += nums[startIdx];
+
+            // update values.
+
+            // decrement: when currSum>= target
+            while(currSum >= target){            
+                int windowSize = startIdx-endIdx+1;
+                // update minimum size sub array sum - minSub
+                minSub = Math.min(minSub, windowSize);
+                
+                int endNum = nums[endIdx];
+                currSum -= endNum;
+                endIdx++;
             }
         }
-        
-        return min ==nums.length+1? 0: min;
+        return minSub == n+1? 0: minSub;
     }
 }
