4cdc2a14daa73f57fdc31c4735b1f87ed246a8da
templeside <47994862+templeside@users.noreply.github.com>
2024-10-20 19:23:46 -0700

    Time: 1353 ms (5.05%), Space: 55.1 MB (92.56%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,22 +1,14 @@
 class Solution {
     /*
-    nums[i] == nums[j]
-    
-    i-j<=k
-    
-    for loop:
-        save hashmap
-        where abs(i-j) <=k
+    3n
     */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // <num, Index>
-        Map<Integer, Integer> map = new HashMap<>(); 
-        
-        for(int i=0; i< nums.length; i++){
-            int num = nums[i];
-            if(map.containsKey(num) && Math.abs(i- map.get(num))<=k)
-                return true;
-            map.put(num, i);
+        int n =nums.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j< Math.min(i+k+1,n); j++){
+                if(nums[i] == nums[j])
+                    return true;
+            }
         }
         return false;
     }
