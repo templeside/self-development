e4bd764aab91283941a6bd72ebb968603d9157bc
templeside <47994862+templeside@users.noreply.github.com>
2024-10-22 11:42:46 -0700

    Time: 24 ms (76.36%), Space: 46.8 MB (21.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,24 +1,39 @@
 class Solution {
     /*
-    contiguous subarray.
-    hard to do sliding window
-    can't shift.
-    i would use prefixsum to keep in track of valid sums.
+    hashmap<diff, count>
+    
+    find offset = currsum -k 
+    add to the count
+    
+    update hashmap
+    
+    subarray = nums[0]+ nums[1] + nums[2] + nums[3]+...nums[n-1]
+    prefix = nums[0]+ nums[1]...nums[i]
+    subarray = nums[3]
+    
+        [1,2,3]
+         ^
+k=          3
+currSum =   1
+offset =    3-1 = 
+prefixed    0:1 1:
+count=0
     */
     public int subarraySum(int[] nums, int k) {
-        int n= nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
+        int n = nums.length;
+        Map<Integer, Integer> prefixed =new HashMap<>();
+        int count = 0;
         int currSum = 0;
-        int count=0;
-        for(int i=0; i<n ;i++){
+        prefixed.put(0,1);
+        
+        for(int i=0; i<n; i++){
             currSum += nums[i];
+            int offset = currSum-k;
             
-            int diff = currSum-k;
-            if(map.containsKey(diff))
-                count += map.get(diff);
+            if(prefixed.containsKey(offset))
+                count += prefixed.get(offset);
             
-            map.put(currSum, map.getOrDefault(currSum,0)+1);
+            prefixed.put(currSum, prefixed.getOrDefault(currSum, 0)+1);
         }
         return count;
     }
