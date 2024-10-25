a9288e285018e3cb6fbb36043ebbbf74008d2caa
templeside <47994862+templeside@users.noreply.github.com>
2024-10-25 10:27:03 -0700

    Time: 0 ms (100.00%), Space: 45.7 MB (68.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,82 +1,51 @@
 class Solution {
     /*
-    1. find out the target
-        if found,
-            find first
-            find last
-            
-[5,7,7,8,8,10]
- ^
-            ^
-       ^       
-        if not found, return[-1,-1]
+    find first
+    find last
+    
+    if found first,
+    if found target,
+        right = mid-1
         
-        for searching first,
-            if found mid
-                mostfront = mid
-                right = mid-1
-            
+        
     */
     public int[] searchRange(int[] nums, int target) {
-        //find target;
-        int idx = findMid(nums, target);
-        if(idx == -1)
+        int n = nums.length;
+        int first = findFirst(0, n-1, nums, target);
+        if(first == -1)
             return new int[]{-1,-1};
-        int left = findFront(nums, idx, target);
-        int right = findEnd(nums, idx, target);
         
-        return new int[]{left,right};
+        int last = findLast(first, n-1, nums, target);
+        return new int[] {first, last};
     }
-    
-    public int findMid(int[] nums, int target){
-        int n = nums.length;
-        int left= 0;
-        int right = n-1;
+    public int findFirst(int i, int j, int[] nums, int target){
+        int first = -1;
         
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(nums[mid] == target)
-                return mid;
-            
-            else if(nums[mid]< target){
-                left = mid+1;
-            }else
-                right = mid-1;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
+                first = mid;
+                j = mid-1;
+            }else if(nums[mid]> target)
+                j = mid-1;
+            else
+                i = mid+1;
         }
-        return -1;
+        return first;
     }
-    public int findFront(int[] nums, int right, int target){
-        int n = nums.length;
-        int left= 0;
-        int returnVal = right;
+    public int findLast(int i, int j, int[] nums, int target){
+        int last = -1;
         
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(nums[mid] == target || nums[mid]> target){
-                right = mid-1;
-                returnVal = mid;
-
-            }else{
-                left = mid+1;
-            }
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
+                last = mid;
+                i = mid+1;
+            }else if(nums[mid]> target)
+                j = mid-1;
+            else
+                i = mid+1;
         }
-        return returnVal;
-    }
-    public int findEnd(int[] nums, int left, int target){
-        int n = nums.length;
-        int right= n-1;
-        int returnVal = left;
-        
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(nums[mid] == target || nums[mid]< target){
-                left = mid+1;
-                returnVal = mid;
-
-            }else{
-                right = mid-1;
-            }
-        }
-        return returnVal;
+        return last;
     }
 }
