07221fc049d2f1ded6cbb883ce59964999c00697
templeside <47994862+templeside@users.noreply.github.com>
2024-10-25 13:18:23 -0700

    Time: 0 ms (100.00%), Space: 56.1 MB (68.78%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,29 +1,18 @@
 class Solution {
     /*
-i am only holding true statements.
-    statements: arr[i] >= arr[i+1]
-    
-              f t t       
-Input: arr = [0,2,1,0]
-              ^     ^
-              arr[i]> arr[i+1]
-Output: 1
-
+    find the condition where find first arr[i]>=arr[i+1]
     */
     public int peakIndexInMountainArray(int[] arr) {
-        int left = 0;
-        int right = arr.length-2;
+        int left =0, right = arr.length-2;
         int peak = -1;
         
         while(left<=right){
-            int mid = left +(right-left)/2;
+            int mid = left+(right-left)/2;
             if(arr[mid]>= arr[mid+1]){
                 peak = mid;
                 right = mid-1;
-            }
-            else{
+            }else
                 left = mid+1;
-            }
         }
         return peak;
     }
