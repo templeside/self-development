edfa0a03c3bf9b70a07c8b6cb6cad66270021fcf
templeside <47994862+templeside@users.noreply.github.com>
2024-10-25 12:09:25 -0700

    Time: 52 ms (88.16%), Space: 69.1 MB (30.80%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,25 +1,41 @@
 class Solution {
     /*
-    sort interval
+Output: 2
+Input: points = [[10,16],[2,8],[1,6],[7,12]]
+
+shot up - is in x direction(vertical)
+as much as shot up is important.
+
+sort the points based on x-axis
+pop out the baloon, check the xEnd
+pop out all the points
+    that the starting point of other points < xEnd
     
-    find overlapping.
-    if not finding overlapping, increment.
-    
+    Arrays.sort((a,b)-> a[0]-b[0])
+    while(pop out is done){
+        compare with curr value.
+        if(curr.start> starting point.end)
+            startingpoint = curr
+            count++;
+        else
+            pop();
+    }
+    points = [[1,6],[2,8],[7,12],[10,16]]
+
     */
     public int findMinArrowShots(int[][] points) {
         Arrays.sort(points, (a,b)-> Integer.compare(a[1],b[1]));
-        int count = 1;
-        int prevEnd = points[0][1];
-        int n = points.length;
-        
-        for(int i=1; i<n; i++){
-            int[] curr = points[i];
-            
-            System.out.println(prevEnd< curr[0]);
-                
-            if(prevEnd <curr[0]){
-                count++;
-                prevEnd = curr[1];
+        // if(points.length == 1)
+            // return 1;
+
+
+
+        int prevRange = points[0][1];
+        int count=1;
+        for(int[] point: points){
+            if(prevRange< point[0]){
+                count ++;
+                prevRange = point[1];
             }
         }
         return count;
