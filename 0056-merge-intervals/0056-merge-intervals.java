764fd5e90c42c28e199ef3db3c8386c7520d3bce
templeside <47994862+templeside@users.noreply.github.com>
2024-10-25 12:21:47 -0700

    Time: 8 ms (87.81%), Space: 46.4 MB (56.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,21 +1,26 @@
 class Solution {
-    /*
-    
-    */  
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a,b)-> a[0] - b[0]);
+        int n = intervals.length;
+        List<int[]> returnVal = new ArrayList<>();
+        returnVal.add(intervals[0]);
         
-        LinkedList<int[]> returnList = new LinkedList<>();
-        
-        int[] prev= null;
-        for(int[] interval: intervals){
-            if(prev == null || prev[1]< interval[0]){
-                returnList.add(interval);
-                prev = returnList.get(returnList.size()-1);
+        int[] prev= returnVal.get(0);
+        for(int i=1; i<n; i++){
+            int[] curr = intervals[i];
+            
+            //if overlapps,  prev[1]>= curr[0]
+            if(prev[1]>= curr[0]){
+                prev[1] = Math.max(prev[1], curr[1]);
             }else{
-                prev[1] = Math.max(prev[1], interval[1]);
+                returnVal.add(curr);
+                prev = returnVal.get(returnVal.size()-1);
             }
         }
-        return returnList.toArray(new int[returnList.size()][2]);
+        int[][] returnArr = new int[returnVal.size()][2];
+        for(int i=0; i< returnVal.size(); i++){
+            returnArr[i] = returnVal.get(i);
+        }
+        return returnArr;
     }
 }
