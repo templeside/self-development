7faa6013f594a4caf69fc8b48102866d54d97d32
templeside <47994862+templeside@users.noreply.github.com>
2024-10-27 10:54:11 -0700

    Time: 16 ms (100.00%), Space: 43.2 MB (7.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,48 +1,64 @@
 class Solution {
     /*
-    find the rotated place
-    find the target in the range
-        456123
-         fttt 
-        nums[i] <nums[i+1] true
-        if mid == true
-            assign right = mid-1
-        if mid != true
-            assing left =mid+1
-            
-    then find the target in two sub values
+    find starting point
+    closing point    
+         0 1 2 3 4 5 6
+        [4,5,6,7,0,1,2]     target=0
+left           ^
+right          ^
+mid            ^          3+(5-3)/2 = 4
+         0 1 2 3 4 5 6
+        [4,5,6,7,0,1,2]     target=0
+endIdx         ^
     */
     public int search(int[] nums, int target) {
-        //find changing index
-        int idx = findChangingIdx(nums);
-        if(nums[0] <= target && target<=nums[idx])
-            return findTarget(0, idx, nums, target);
-        else
-            return findTarget(idx+1,nums.length-1, nums, target);
+        if(nums.length == 0)
+            return -1;
+
+        if(nums.length == 1)
+            return nums[0] == target? 0: -1;
+        
+        int endIdx = findEnd(nums);
+        System.out.println("end index is "+endIdx+" "+nums[endIdx]);
+        if(nums[0]<= target && target<=nums[endIdx])
+            return find(0, endIdx, target, nums);
+        if(nums[endIdx+1]<= target && target<= nums[nums.length-1])
+            return find(endIdx+1, nums.length-1, target, nums);
+        return -1;
     }
     
-    public int findChangingIdx(int[] nums){
-        int left=0; 
-        int right=  nums.length-2;
+    public int findEnd(int[] nums){
+        int left=0;
+        int right = nums.length-2;
+        
         while(left<=right){
+            
             int mid = left+(right-left)/2;
-            if(nums[mid]> nums[mid+1])
+            System.out.println("left is:"+left+" right is:"+right+ " mid is:"+mid);
+            if(nums[mid]> nums[mid+1]){
                 return mid;
-            else if(nums[left] <= nums[mid])
-                left= mid+1;
+            }
+            else if(nums[left]<= nums[mid])
+                left = mid+1;
             else
-                right = mid-1;
+                right =mid-1;
         }
         return 0;
     }
-    public int findTarget(int left, int right, int[] nums, int target){
-        
+    
+    /*
+         0 1 2 3 4 5 6
+        [4,5,6,7,0,1,2]     target=0
+endIdx         ^
+    */
+    public int find(int left, int right, int target, int[] nums){
+        System.out.println(left+" "+right);
         while(left<=right){
-            System.out.println(left +" "+ right);
             int mid = left+(right-left)/2;
-            if (nums[mid] ==target)
+            int midVal = nums[mid];
+            if(midVal == target){
                 return mid;
-            else if(nums[mid]> target){
+            }else if(midVal>target){
                 right = mid-1;
             }else
                 left = mid+1;
