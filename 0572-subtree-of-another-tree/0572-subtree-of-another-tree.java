946e80297b2e272b9c68116f9795ad95b8450833
templeside <47994862+templeside@users.noreply.github.com>
2024-10-30 09:57:13 -0700

    Time: 3 ms (20.74%), Space: 43.9 MB (90.01%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,28 +14,27 @@
  * }
  */
 class Solution {
+    /*
+    if found same head, compare.
+    */
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root ==null)return false;
-        
-        if(root.val == subRoot.val && isSame(root, subRoot))
+        if(root ==null)
+            return false;
+        if(root.val == subRoot.val && compare(root, subRoot))
             return true;
         
-        boolean left = isSubtree(root.left, subRoot);
-        boolean right = isSubtree(root.right, subRoot);
-        
-        return left || right;
+        return isSubtree(root.left, subRoot) ||isSubtree(root.right, subRoot);
     }
-    
-    public boolean isSame(TreeNode root, TreeNode subRoot){
+    public boolean compare(TreeNode root, TreeNode subRoot){
         if(root ==null && subRoot ==null)
             return true;
-        if(root== null|| subRoot ==null)
+        
+        if(root ==null || subRoot==null)
             return false;
         
-        // if(root.val != subRoot.val)
-        //     return false;
+        if(root.val != subRoot.val)
+            return false;
         
-        return root.val == subRoot.val && isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
-        
+        return compare(root.left, subRoot.left) && compare(root.right, subRoot.right);
     }
 }
