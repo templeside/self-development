efc633d7f7da18cdfa7944edd1686ce33be28438
templeside <47994862+templeside@users.noreply.github.com>
2024-11-03 18:28:04 -0500

    Time: 4 ms (50.74%), Space: 52.2 MB (14.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,41 @@
 class Solution {
     /*
-    by iterating all grids, find the land
-    
-    if found land,
-        count island
-        do dfs to check all the connected land 
-        
+    look for the island
+
+        if found, 
+            count++
+            do the dfs to cancel out the adjacencies to 0 - becausee this is one island.
     */
     public int numIslands(char[][] grid) {
         int count =0;
-        int m =grid.length;
-        int n= grid[0].length;
-        for(int r=0; r< m; r++){
-            for(int c= 0; c< n; c++){
+        int m= grid.length;
+        int n =grid[0].length;
+        for(int r=0; r<m; r++){
+            for(int c=0; c<n; c++){
                 if(grid[r][c] == '1'){
-                    count++;
                     dfs(r,c,grid);
+                    count++;
                 }
             }
         }
         return count;
     }
-    
-    /*
-    change coordinate element to -1
-    look for adjacencies = {{0,1},{0,-1},{1,0},{-1,0}}
-    */
-    public void dfs(int r, int c, char[][] grid){
-        int[][] dirs =new  int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        int m =grid.length;
-        int n= grid[0].length;
 
+    public void dfs(int r, int c, char[][] grid){
         grid[r][c] = '0';
-        for(int[] adj: dirs){
-            int newR = r+ adj[0];
-            int newC= c+ adj[1];
-            if(newR>=0 && newR<m && newC>=0 && newC<n && grid[newR][newC] == '1')
+
+        int[][] dirs= new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
+        //adjacencies
+        for(int[] dir: dirs){
+            int newR = r+ dir[0];
+            int newC = c+ dir[1];
+
+            //if valid. in range, grid[newR][newC] ==1
+            
+            if(newR >= 0 && newR< grid.length && newC>=0 && newC < grid[0].length && grid[newR][newC] == '1'){
                 dfs(newR,newC, grid);
+            }
         }
+        return;
     }
 }
