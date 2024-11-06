0b8210d657217a1413558253df5ac4cf0bcf33cd
templeside <47994862+templeside@users.noreply.github.com>
2024-11-05 23:57:47 -0500

    Time: 36 ms (44.46%), Space: 48.4 MB (44.82%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,76 +1,64 @@
 class Solution {
-    public class UnionFind {
-    private int[] parents;
+    /*
+    i'm basically having the number of accounts.
 
-    public UnionFind(int n) {
-        parents = new int[n];
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-        }
-    }
+    what i need to do is when 
+                     name           emails,      emails....
+Input: accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
 
-    public int find(int node) {
-        if (parents[node] == node) {
-            return node;
-        }
-        return find(parents[node]);
-    }
+find the representative email to connect all the related emails with name.
 
-    public void union(int node1, int node2) {
-        int rootNode1 = find(node1);
-        int rootNode2 = find(node2);
-        if (rootNode1 != rootNode2) {
-            parents[rootNode2] = rootNode1;
-        }
-    }
-}
+hahsmap<email, representative email> 
 
+    */
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        UnionFind uf = new UnionFind(accounts.size());
-        Map<String, Integer> emailMapping = new HashMap<>();
+        HashMap<String, List<String>> graph = new HashMap<>();
+        HashSet<String> visited = new HashSet<>();
 
-        
-        // prepare a hash with unique email address as key and index in accouts as value
-        // <email address, index of the account>
-        // union inside, uf contains the parent.
-        for (int i = 0; i < accounts.size(); ++i) {
-            List<String> emails = accounts.get(i);      // ["John","johnsmith@mail.com","john_newyork@mail.com"],
-            for (int j = 1; j < emails.size(); ++j) {
-                
-                // if we have already seen this email before, merge the account  "i" with previous account
-                // if we already found the id, union
-                // else, put
-                if (emailMapping.containsKey(emails.get(j))) {
-                    if (!accounts.get(i).get(0).equals(accounts.get(emailMapping.get(emails.get(j))).get(0))) {
-                        return new ArrayList<>(); 
-                    }
-                    uf.union(emailMapping.get(emails.get(j)), i);
-                }
-                emailMapping.put(emails.get(j), i);
+        for(List<String> account : accounts){
+            String accountFirstEmail = account.get(1);
+
+            for(int i=2; i< account.size(); i++){
+                String accountCurrEmail = account.get(i);
+
+                graph.putIfAbsent(accountFirstEmail, new ArrayList<>());
+                graph.get(accountFirstEmail).add(accountCurrEmail);
+
+                graph.putIfAbsent(accountCurrEmail, new ArrayList<>());
+                graph.get(accountCurrEmail).add(accountFirstEmail);
             }
         }
 
-        // prepare a hash with index in accounts as key and list of unique email address for that account as value
-        Map<Integer, List<String>> mergedAccounts = new HashMap<>();
+        List<List<String>> mergedAccounts = new ArrayList<>();
 
-        for (Map.Entry<String, Integer> entry : emailMapping.entrySet()) {
-            int root = uf.find(entry.getValue());
-            mergedAccounts.computeIfAbsent(root, k -> new ArrayList<>());
-            mergedAccounts.get(root).add(entry.getKey());
+        for(List<String> account: accounts){
+            String accountName= account.get(0);
+            String accountFirstEmail = account.get(1);
+
+            if(!visited.contains(accountFirstEmail)){
+                List<String> mergedAccount = new ArrayList<>();
+                mergedAccount.add(accountName);
+                DFS(visited, graph, mergedAccount, accountFirstEmail);
+                Collections.sort(mergedAccount.subList(1, mergedAccount.size()));
+                mergedAccounts.add(mergedAccount);
+            }
         }
-        
-        // collect the emails from idToEmails, sort it and add account name at index 0 to get the final list to add to final return List
-        List<List<String>> finalMerged = new ArrayList<>();
-        
-        for (Map.Entry<Integer, List<String>> entry : mergedAccounts.entrySet()) {
-            int parent = entry.getKey();
-            List<String> emails = entry.getValue();
-            Collections.sort(emails);
-            List<String> merged = new ArrayList<>();
-            merged.add(accounts.get(parent).get(0));
-            merged.addAll(emails);
-            finalMerged.add(merged);
+        return mergedAccounts;
+    }
+    public void DFS(HashSet<String> visited, HashMap<String, List<String>> graph, List<String> mergedAccount, String accountCurrEmail){
+        if(visited.contains(accountCurrEmail))
+        return;
+
+        visited.add(accountCurrEmail);
+
+        mergedAccount.add(accountCurrEmail);
+
+        if(!graph.containsKey(accountCurrEmail)){
+            return;
         }
-        return finalMerged;
+
+        for(String adjEmail: graph.get(accountCurrEmail)){
+            DFS(visited, graph, mergedAccount, adjEmail);
+        }
     }
 }
