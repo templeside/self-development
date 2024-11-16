8f5833d4ca46ea9f0bf8da50d77c6eed9c5819e3
templeside <47994862+templeside@users.noreply.github.com>
2024-11-16 11:08:38 -0500

    Time: 2 ms (56.60%), Space: 44.9 MB (46.00%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,21 +1,33 @@
 class Solution {
+    /**
+    nums = [2, 3, -2, 4]
+    pointer       ^
+    dpMax    2   6   -2  4
+    dpMin    2   3  -12 -48
+
+    ans = 6
+
+    By swapping dpMin and dpMax when nums[i] is negative, we ensure that the maximum and minimum products are correctly updated.
+    **/
     public int maxProduct(int[] nums) {
         int ans = nums[0];
-        int dpMin = nums[0];
-        int dpMax = nums[0];
+        int dpMin = nums[0]; // Tracks the minimum product up to the current position
+        int dpMax = nums[0]; // Tracks the maximum product up to the current position
 
         for (int i = 1; i < nums.length; ++i) {
             int num = nums[i];
-            int prevMin = dpMin;
-            int prevMax = dpMax;
+
             if (num < 0) {
-                dpMin = Math.min(prevMax * num, num);
-                dpMax = Math.max(prevMin * num, num);
-                } else {
-                dpMin = Math.min(prevMin * num, num);
-                dpMax = Math.max(prevMax * num, num);
+                // Swap dpMin and dpMax because multiplying by a negative flips signs
+                int temp = dpMax;
+                dpMax = dpMin;
+                dpMin = temp;
             }
-            ans = Math.max(ans, dpMax);
+
+            dpMax = Math.max(num, dpMax * num);
+            dpMin = Math.min(num, dpMin * num);
+
+            ans = Math.max(ans, dpMax); // Update the answer with the maximum product found so far
         }
         return ans;
     }
