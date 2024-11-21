55fe5cbdd12a5115f75d1c7c0566cfac6135e216
templeside <47994862+templeside@users.noreply.github.com>
2024-11-20 23:28:53 -0500

    Time: 17 ms (94.28%), Space: 57.9 MB (43.87%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,14 +1,15 @@
 class Solution {
     /*
-    3n
+    hashMap 으로 같은 값을 찾으면 time complexity가 준다.
     */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         int n =nums.length;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j< Math.min(i+k+1,n); j++){
-                if(nums[i] == nums[j])
-                    return true;
-            }
+        Map<Integer, Integer> map = new HashMap<>();// nums[i] i-j<=k
+        for(int j=0; j<n; j++){
+            int curr = nums[j];
+            if(map.containsKey(curr) && Math.abs(map.get(curr) - j)<=k)
+                return true;
+            map.put(curr, j);
         }
         return false;
     }
