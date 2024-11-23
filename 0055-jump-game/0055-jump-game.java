7058f96651d4576a24b5d5156ca0af47552eb5f7
templeside <47994862+templeside@users.noreply.github.com>
2024-11-23 14:35:10 -0500

    Time: 1221 ms (5.01%), Space: 45.5 MB (73.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,25 @@
 class Solution {
     /*
-    starting from backwards, check the valid hardware, do the items.
-    it is called 
-                        
-        nums = [2,3,1,1,4]
-jumpable     = [          T]
+    by approaching from back, check available to reach.
+                    t
+    nums = [2,3,1,1,4]
+    starting from last index checking vailable to reach out.
+    
+    for(int i, i < i+ nums[i]; i++)
+        dp[i] = possible values
     */
     public boolean canJump(int[] nums) {
         int n = nums.length;
         boolean[] dp = new boolean[n];
         dp[n-1] = true;
-        for(int i= n-1; i>=0 ; i--){
-            int jump = nums[i];
-            //  if(dp[i+jump] == true)
-                // update it
-            for(int j=1; j<= jump; j++){
-                int newIdx = i+j;
-                if(newIdx< n &&  dp[newIdx] ==true){
-                    dp[i] = true;
+        
+        for(int i=n-1; i>=0; i--){
+            int jumpby = i+nums[i];
+            for(int nextIdx = 1; nextIdx<= jumpby && nextIdx< n; nextIdx++){
+                if(dp[i] == true)
                     break;
+                if(dp[nextIdx] == true){
+                    dp[i] = true;
                 }
             }
         }
