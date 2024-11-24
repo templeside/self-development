7bf20da689aa0fb9d9a62ff4fc1bdc3ee77a9313
templeside <47994862+templeside@users.noreply.github.com>
2024-11-24 13:13:55 -0500

    Time: 78 ms (66.40%), Space: 47.8 MB (28.47%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,57 +1,55 @@
 class Solution {
+    /*
+    bfs로 일을 한다.
+    find the possibilities
+    
+    */
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
-        // Since all words are of same length.
-        int L = beginWord.length();
-
-        // Dictionary to hold combination of words that can be formed,
-        // from any given word. By changing one letter at a time.
-        Map<String, List<String>> allComboDict = new HashMap<>();
-
-        wordList.forEach( word -> {
-            for (int i = 0; i < L; i++) {
-                // Key is the generic word
-                // Value is a list of words which have the same intermediate generic word.
-                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
-                List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
-                transformations.add(word);
-                allComboDict.put(newWord, transformations);
-            }
-        });
-
-        // Queue for BFS
-        Queue<Pair<String, Integer>> Q = new LinkedList<>();
-        Q.add(new Pair(beginWord, 1));
-
-        // Visited to make sure we don't repeat processing same word.
-        Map<String, Boolean> visited = new HashMap<>();
-        visited.put(beginWord, true);
-
-        while (!Q.isEmpty()) {
-            Pair<String, Integer> node = Q.remove();
-            String word = node.getKey();
-            int level = node.getValue();
-            for (int i = 0; i < L; i++) {
-
-                // Intermediate words for current word
-                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
-
-                // Next states are all the words which share the same intermediate state.
-                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
-                    // If at any point if we find what we are looking for
-                    // i.e. the end word - we can return with the answer.
-                    if (adjacentWord.equals(endWord)) {
-                        return level + 1;
+        Set<String> wordSet = new HashSet<>(wordList);
+        
+        //checking the endWord is in wordSet
+        if(!wordSet.contains(endWord))
+            return 0;
+        
+        //initialize BFS
+        Queue<String> queue= new LinkedList<>();
+        queue.add(beginWord);
+        
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        
+        int level = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0; i< size; i++){
+                String currentWord = queue.poll();
+                
+                //if currWord == endWord
+                if(currentWord.equals(endWord)){
+                    return level;
+                }
+                
+                //try changing each letter with wordset
+                char[] wordChars = currentWord.toCharArray();
+                for(int j=0; j< wordChars.length; j++){
+                    char originalChar = wordChars[j];
+                    
+                    for(char c = 'a'; c<='z'; c++){
+                        if(c==originalChar)
+                            continue;
+                        wordChars[j] = c;
+                        String newWord = new String(wordChars);
+                        // check isValid - in wordset,      not in visited
+                        if(wordSet.contains(newWord) && !visited.contains(newWord)){
+                            queue.add(newWord);
+                            visited.add(newWord);
+                        }
                     }
-                    // Otherwise, add it to the BFS Queue. Also mark it visited
-                    if (!visited.containsKey(adjacentWord)) {
-                        visited.put(adjacentWord, true);
-                        Q.add(new Pair(adjacentWord, level + 1));
-                    }
+                    wordChars[j] = originalChar;
                 }
             }
+            level++;
         }
-
         return 0;
     }
 }
