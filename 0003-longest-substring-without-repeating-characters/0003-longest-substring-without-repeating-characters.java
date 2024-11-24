9b3103fb6f2d80dd1fb768a3c6915d5cf05d6644
templeside <47994862+templeside@users.noreply.github.com>
2024-11-24 01:09:35 -0500

    Time: 6 ms (70.16%), Space: 44.6 MB (57.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,31 +1,26 @@
 class Solution {
     /*
-    sliding window
-        increment:
-            update without repeating chcaracters
-            
-            after removing the repeating chars
-        decrement - when having duplicated chars
+    increment - 
+        decrement - when found the visited already
         
-        update longest
+        update length
+        add visited
     */
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int endIdx = 0;
-        int longest = 0;
         Set<Character> visited = new HashSet<>();
+        int endIdx=0;
+        int n = s.length();
+        int longest = 0;
         
-        for(int startIdx=0; startIdx<n; startIdx++){
+        for(int startIdx=0; startIdx< n; startIdx++){
             char currChar = s.charAt(startIdx);
-            
-            while(endIdx<= startIdx && visited.contains(currChar)){
+            while(visited.contains(currChar)){
                 char endChar = s.charAt(endIdx);
                 visited.remove(endChar);
                 endIdx++;
             }
             visited.add(currChar);
             int windowSize = startIdx-endIdx+1;
-            
             longest = Math.max(longest, windowSize);
         }
         return longest;
