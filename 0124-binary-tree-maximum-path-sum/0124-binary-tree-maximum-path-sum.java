c88827cce0ffc0eecca6c09e41312ef0ab2bcf21
templeside <47994862+templeside@users.noreply.github.com>
2024-11-24 12:42:24 -0500

    Time: 0 ms (100.00%), Space: 44.4 MB (40.69%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,27 +15,39 @@
  */
 class Solution {
     /*
-    updating the maxPath through global variable
-        maxPath = Math.max(maxPath, root.left + root.right + root)
-    return max path 
+    from this experience, i can have 
+    1,2,3
+    get th max path global variable to iterate over
+    
+    have recursion:
+        recursion, could have the edges
+        left = recursion(root.left)
+        right = recursion(root.right)
+        currPathSum = left+ right + root.val
+        return Math.max(left,right)+root.val
+        
+        [1,-2,3]
+        1
+    
     */
     public int maxSum;
     public int maxPathSum(TreeNode root) {
-        if(root ==null)
-            return 0;
-        maxSum = Integer.MIN_VALUE;
-        dfs(root);
+        maxSum = root.val;
+        helper(root);
         return maxSum;
     }
-    public int dfs(TreeNode root){
-        if(root == null)
+    public int helper(TreeNode root){
+        if(root ==null){
             return 0;
-        
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        
-        maxSum = Math.max(maxSum, left+right+root.val);
-        
-        return Math.max(Math.max(left,right)+root.val, 0);
+        }
+    //     recursion, could have the edges
+    //     left = recursion(root.left)
+        int left = Math.max(helper(root.left),0);
+    //     right = recursion(root.right)
+        int right = Math.max(helper(root.right),0);
+    //     currPathSum = left+ right + root.val
+        int currPathSum = left+right+root.val;
+        maxSum = Math.max(maxSum, currPathSum);
+        return Math.max(left,right)+root.val;
     }
 }
