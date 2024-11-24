f8e5b2397b8013f771090c9ca82c822b94c2951d
templeside <47994862+templeside@users.noreply.github.com>
2024-11-24 13:34:21 -0500

    Time: 4 ms (51.08%), Space: 49.7 MB (40.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,17 +1,19 @@
 class Solution {
     /*
-    look for the island
-
-        if found, 
-            count++
-            do the dfs to cancel out the adjacencies to 0 - becausee this is one island.
+    look for grid
+    
+    if found land, 
+        cross out the adj land. adj lands are:
+            {0,1},{0,-1},{1,0},{-1,0}
+            if valid - in range, it is land
+                subrecursion
+    
     */
+    public int[][] dirs= new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
     public int numIslands(char[][] grid) {
-        int count =0;
-        int m= grid.length;
-        int n =grid[0].length;
-        for(int r=0; r<m; r++){
-            for(int c=0; c<n; c++){
+        int count=0;
+        for(int r=0; r< grid.length; r++){
+            for(int c=0; c< grid[0].length; c++){
                 if(grid[r][c] == '1'){
                     dfs(r,c,grid);
                     count++;
@@ -20,22 +22,17 @@ class Solution {
         }
         return count;
     }
-
     public void dfs(int r, int c, char[][] grid){
         grid[r][c] = '0';
-
-        int[][] dirs= new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
-        //adjacencies
+        
         for(int[] dir: dirs){
             int newR = r+ dir[0];
             int newC = c+ dir[1];
-
-            //if valid. in range, grid[newR][newC] ==1
             
-            if(newR >= 0 && newR< grid.length && newC>=0 && newC < grid[0].length && grid[newR][newC] == '1'){
-                dfs(newR,newC, grid);
+            //if valid, recursion
+            if(newR>=0 && newR< grid.length && newC>=0 && newC< grid[0].length && grid[newR][newC] == '1'){
+                dfs(newR, newC, grid);
             }
         }
-        return;
     }
 }
