96e121981fc3bf9580f05b1837cee16fc691c76c
templeside <47994862+templeside@users.noreply.github.com>
2024-11-25 22:16:39 -0500

    Time: 16 ms (24.16%), Space: 45.2 MB (5.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,31 @@
 class Solution {
     /*
-    check is that valid anagram
-    check counters.
-    find matched
+    those are anagram, so i would do implementation.
+    check all the frequencies.
+    go over and check comparison.
+    
+    freq
+    freq.add
+    
+    if freq.char == 0
+        remove the key
+    if not in key
+        return false;
     */
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
-            return false;
+        if(s.length() != t.length())return false;
         
-        Map<Character, Integer> sFreq = new HashMap<>();
-        for(char c: s.toCharArray()){
-            sFreq.put(c, sFreq.getOrDefault(c, 0)+1);
+        Map<Character, Integer> freq= new HashMap<>();
+        for(char sChar: s.toCharArray()){
+            freq.put(sChar, freq.getOrDefault(sChar,0)+1);
         }
         
-        for(char c: t.toCharArray()){
-            if(!sFreq.containsKey(c))
+        for(char tChar: t.toCharArray()){
+            if(!freq.containsKey(tChar))
                 return false;
-            sFreq.put(c, sFreq.get(c)-1);
-            if(sFreq.get(c) <0)
-                return false;
+            freq.put(tChar, freq.get(tChar)-1);
+            if(freq.get(tChar)== 0)
+                freq.remove(tChar);
         }
         return true;
     }
